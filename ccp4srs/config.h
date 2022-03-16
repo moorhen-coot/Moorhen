eb2886056052a14c8c6ca41e813523b7fb9be24b
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2022-03-16 14:55:36 +0000

    Remove some more stuff.

--- ccp4srs/config.h
+++
@@ -1,8 +0,0 @@
-/* Name of package */
-#define PACKAGE ccp4srs
-
-/* Version number of package */
-#define VERSION 1.0
-
-/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.  */
-#define HAVE_DIRENT_H 1
