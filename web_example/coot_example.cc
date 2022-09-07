91ce585b950afe07f2172a0e98f1312fe374171e
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2022-09-07 18:34:02 +0100

    Some beginnings of Rama API.

--- web_example/coot_example.cc
+++ web_example/coot_example.cc
@@ -25,6 +25,29 @@ using namespace emscripten;
 
 extern void clear_getopt_initialized();
 
+struct ResidueIdentifier {
+    std::string chainId;
+    int resNo;
+    std::string insCode;
+};
+
+std::vector<ResidueIdentifier> getRamachandranData(const std::string &pdbin, const std::string &chainId){
+    std::vector<ResidueIdentifier> info;
+    const char *filename_cp = pdbin.c_str();
+    mmdb::InitMatType();
+    mmdb::Manager *molHnd = new mmdb::Manager();
+
+    int RC = molHnd->ReadCoorFile(filename_cp);
+    int selHnd = molHnd->NewSelection();
+    molHnd->Select(selHnd,mmdb::STYPE_RESIDUE,"/*/*/(GLY,ALA,VAL,PRO,SER,THR,LEU,ILE,CYS,ASP,GLU,ASN,GLN,ARG,LYS,MET,MSE,HIS,PHE,TYR,TRP,HCS,ALO,PDD,UNK)",mmdb::SKEY_NEW);
+    mmdb::Residue** SelRes=0;
+    int nRes;
+    molHnd->GetSelIndex(selHnd,SelRes,nRes);
+    // TODO - get the actual data ...
+
+    return info;
+}
+
 int flipPeptide(const std::string &pdbin, const std::string &chainId, const int resno, const std::string &pdbout){
     int retval = 0;
     std::cout << "In flipPeptide in C++. This does nothing useful." << std::endl;
@@ -76,7 +99,14 @@ int mini_rsr(const std::vector<std::string> &args){
 
 
 EMSCRIPTEN_BINDINGS(my_module) {
+    class_<ResidueIdentifier>("ResidueIdentifier")
+    .constructor<>()
+    .property("chainId", &ResidueIdentifier::chainId)
+    .property("resNo", &ResidueIdentifier::resNo)
+    .property("insCode", &ResidueIdentifier::insCode)
+    ;
     register_vector<std::string>("VectorString");
+    register_vector<ResidueIdentifier>("VectorResidueIdentifier");
     function("mini_rsr",&mini_rsr);
     function("flipPeptide",&flipPeptide);
 }
