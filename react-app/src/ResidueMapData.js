1f96587f924f8649269329bacd5f1484f9f39e73
Filomeno Sanchez <filo_san_rod@hotmail.com>
2022-10-11 10:30:48 +0100

    residuemapdata not to lookup dataobjects names when plotting

--- react-app/src/ResidueMapData.js
+++ react-app/src/ResidueMapData.js
@@ -317,25 +317,18 @@ class ResidueMapData extends Component {
     }    
 
     /**
-     * Update density fit plot data
+     * Update contents of plot
+     * @param {array} plotData - array with residue information
+     * @param {string} key - key for the selected pdb model
      */
-    updatePlotData(plotData){
+     updatePlotData(plotData, key){
         const self = this;
-        let key = self.state.selected;
-        const dataObjectNames = this.props.dataObjectsNames;
-        const pdbKeys = Object.keys(dataObjectNames.pdbFiles);
-        if(pdbKeys.length<1){
-            return;
-        }
-        if(key==="unk"){
-            key = pdbKeys[0];
-        }
 
         self.plotRef.current.dataKey = this.dataKey;
         self.plotRef.current.dataInfoScaling = this.dataInfoScaling;
         self.plotRef.current.customClickHandler = this.clickHandler;
 
-        if(dataObjectNames[self.infoName] && dataObjectNames[self.infoName][key]){
+        if(plotData){
             self.plotRef.current.updatePlotData({info:plotData, key:key});
             this.setState({plotInfo:plotData});
         }
@@ -347,7 +340,7 @@ class ResidueMapData extends Component {
      */
     async getData(){
         const self = this;
-        let key = self.state.selected;
+        let keyModel = self.state.selected;
         let keyMap = self.state.mapSelected;
         const dataObjectNames = this.props.dataObjectsNames;
         const pdbKeys = Object.keys(dataObjectNames.pdbFiles);
@@ -355,16 +348,16 @@ class ResidueMapData extends Component {
         if(pdbKeys.length<1){
             return;
         }
-        if(key==="unk"){
-            key = pdbKeys[0];
+        if(keyModel==="unk"){
+            keyModel = pdbKeys[0];
         }
         if(keyMap==="unk"){
             keyMap = mtzKeys[0];
         }
         const jobid = guid();
-        const inputData = {method:self.crystMethod,jobId:jobid,pdbinKey:key,hklinKey:keyMap,chainId:this.state.chainId};
+        const inputData = {method:self.crystMethod,jobId:jobid,pdbinKey:keyModel,hklinKey:keyMap,chainId:this.state.chainId};
         let response = await this.postCrystWorkerMessage(self.props.crystWorker, inputData);
-        this.updatePlotData(response.data.result);
+        this.updatePlotData(response.data.result, keyModel);
 
     }
 
