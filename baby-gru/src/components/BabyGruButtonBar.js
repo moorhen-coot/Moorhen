83f50b58b82f3e8c4a6f32c3aaff754dfaaba768
Martin Noble <martinemnoble1@mac.com>
2022-10-19 13:56:19 +0100

    With prompt overlay when activaed a button bar button

--- baby-gru/src/components/BabyGruButtonBar.js
+++ baby-gru/src/components/BabyGruButtonBar.js
@@ -1,5 +1,5 @@
-import { createRef, useState} from "react";
-import { ButtonGroup, Button } from "react-bootstrap"
+import { createRef, useCallback, useRef, useState } from "react";
+import { ButtonGroup, Button, Popover, Overlay } from "react-bootstrap"
 import { cootCommand, postCootMessage } from "../BabyGruUtils"
 import { circles_fragment_shader_source } from "../WebGL/circle-fragment-shader";
 
@@ -16,7 +16,7 @@ export const BabyGruButtonBar = (props) => {
 
             <BabyGruSimpleEditButton {...props}
                 buttonIndex={"0"}
-                selectedbuttonIndex = {selectedbuttonIndex}
+                selectedbuttonIndex={selectedbuttonIndex}
                 setSelectedbuttonIndex={setSelectedbuttonIndex}
                 cootCommand="auto_fit_rotamer"
                 icon={<img className="baby-gru-button-icon" src="pixmaps/auto-fit-rotamer.svg" />}
@@ -32,7 +32,7 @@ export const BabyGruButtonBar = (props) => {
 
             <BabyGruSimpleEditButton {...props}
                 buttonIndex={"1"}
-                selectedbuttonIndex = {selectedbuttonIndex}
+                selectedbuttonIndex={selectedbuttonIndex}
                 setSelectedbuttonIndex={setSelectedbuttonIndex}
                 cootCommand="flipPeptide_cid"
                 icon={<img className="baby-gru-button-icon" src="pixmaps/flip-peptide.svg" />}
@@ -48,36 +48,64 @@ export const BabyGruButtonBar = (props) => {
 }
 
 const BabyGruSimpleEditButton = (props) => {
-    const atomClickedBinding = createRef(null);
+    const [showPrompt, setShowPrompt] = useState(false)
+    const target = useRef(null);
 
-    return <Button value={props.buttonIndex} active={props.buttonIndex===props.selectedbuttonIndex} variant='light' onClick={(e) => {
-        if (props.selectedbuttonIndex === e.currentTarget.value) {
-            props.setSelectedbuttonIndex(null)
+    const atomClickedCallback = useCallback(event => {
+        props.molecules.forEach(molecule => {
+            setShowPrompt(false)
             props.setCursorStyle("default")
-            document.removeEventListener('atomClicked', atomClickedBinding.current)
-            return
-        }
-        props.setSelectedbuttonIndex(props.buttonIndex)
-        props.setCursorStyle("crosshair")
-        atomClickedBinding.current = document.addEventListener('atomClicked', (event) => {
-            props.molecules.forEach(molecule => {
-                props.setCursorStyle("default")
-                const chosenAtom = cidToSpec(event.detail)
-                cootCommand(props.cootWorker, {
-                    returnType: "status",
-                    command: props.cootCommand,
-                    commandArgs: props.formatArgs(molecule, chosenAtom)
-                }).then(_ => {
-                    molecule.setAtomsDirty(true)
-                    molecule.redraw(props.glRef)
-                    props.setSelectedbuttonIndex(null)
-                })
-
+            const chosenAtom = cidToSpec(event.detail)
+            cootCommand(props.cootWorker, {
+                returnType: "status",
+                command: props.cootCommand,
+                commandArgs: props.formatArgs(molecule, chosenAtom)
+            }).then(_ => {
+                molecule.setAtomsDirty(true)
+                molecule.redraw(props.glRef)
+                props.setSelectedbuttonIndex(null)
             })
-        }, { once: true })
-    }}>
-        {props.icon}
-    </Button>
+        })
+    })
+
+    return <>
+        <Button value={props.buttonIndex}
+            ref={target}
+            active={props.buttonIndex === props.selectedbuttonIndex}
+            variant='light'
+            onClick={(e) => {
+                if (props.selectedbuttonIndex === e.currentTarget.value) {
+                    props.setSelectedbuttonIndex(null)
+                    props.setCursorStyle("default")
+                    document.removeEventListener('atomClicked', atomClickedCallback, { once: true })
+                    setShowPrompt(false)
+                    return
+                }
+                props.setSelectedbuttonIndex(props.buttonIndex)
+                props.setCursorStyle("crosshair")
+                document.addEventListener('atomClicked', atomClickedCallback, { once: true })
+                setShowPrompt(true)
+            }}>
+            {props.icon}
+        </Button>
+
+        <Overlay target={target.current} show={showPrompt} placement="left">
+            {({ placement, arrowProps, show: _show, popper, ...props }) => (
+                <div
+                    {...props}
+                    style={{
+                        position: 'absolute',
+                        backgroundColor: 'rgba(255, 100, 100, 0.85)',
+                        padding: '2px 10px',
+                        color: 'white',
+                        borderRadius: 3,
+                        ...props.style,
+                    }}
+                >Click an atom
+                </div>
+            )}
+        </Overlay>
+    </>
 }
 
 const cidToSpec = (cid) => {
