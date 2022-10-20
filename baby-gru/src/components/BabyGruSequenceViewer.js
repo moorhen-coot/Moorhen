83aa6fe013e2de5f604489a34927e9223e498eff
FilomenoSanchez <filo_san_rod@hotmail.com>
2022-10-20 15:58:52 +0100

    refactor ramaplot and seqviewer to use same method to centre view

--- baby-gru/src/components/BabyGruSequenceViewer.js
+++ baby-gru/src/components/BabyGruSequenceViewer.js
@@ -35,21 +35,8 @@ export const BabyGruSequenceViewer = (props) => {
             return
         }
 
-        // WARNING: Currently we assume that selected model is always the first one...
-        let selectedModelIndex = 0
-        let selectedChainIndex = props.molecules[selectedMoleculeIndex].cachedAtoms.atoms[selectedModelIndex].chains.findIndex(chain => chain.residues[0].atoms[0]["_atom_site.auth_asym_id"] === clickedResidue.chain);
-        if (selectedChainIndex === -1) {
-            console.log(`Cannot find chain ${clickedResidue.molName}/${clickedResidue.chain}`)
-            return
-        }
+        props.molecules[selectedMoleculeIndex].centreOn(props.glRef, clickedResidue)
 
-        let selectedResidueIndex = props.molecules[selectedMoleculeIndex].cachedAtoms.atoms[selectedModelIndex].chains[selectedChainIndex].residues.findIndex(residue => residue.atoms[0]["_atom_site.label_seq_id"] == clickedResidue.seqNum);
-        if (selectedResidueIndex === -1) {
-            console.log(`Cannot find residue ${clickedResidue.molName}/${clickedResidue.chain}/${clickedResidue.seqNum}`)
-        } else {
-            let selectedResidueAtoms = props.molecules[selectedMoleculeIndex].cachedAtoms.atoms[selectedModelIndex].chains[selectedChainIndex].residues[selectedResidueIndex].atoms;
-            props.glRef.current.setOrigin(props.molecules[selectedMoleculeIndex].cachedAtoms.atoms[selectedModelIndex].centreOnAtoms(selectedResidueAtoms))
-        }
     }, [clickedResidue])
 
 
