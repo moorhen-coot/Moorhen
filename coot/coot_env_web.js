cee53300da15ef4bce38056aa311fa095ee05273
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2022-10-20 09:40:44 +0100

    Add COOT_BACKUP_DIR.

--- coot/coot_env_web.js
+++ coot/coot_env_web.js
@@ -1,3 +1,4 @@
 if(!Module["preRun"])Module["preRun"]=[];
 Module.preRun.push(function() {ENV.COOT_DATA_DIR="data/coot"});
 Module.preRun.push(function() {ENV.CCP4_LIB="data/ccp4_lib"});
+Module.preRun.push(function() {ENV.COOT_BACKUP_DIR="data/COOT_BACKUP"});
