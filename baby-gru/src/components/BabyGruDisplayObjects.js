cbd78a6e7a079d986675ceab4d9c8a25313dd62e
FilomenoSanchez <filo_san_rod@hotmail.com>
2022-10-23 18:14:48 +0100

    merge mapUI and moleculeUI

---
+++ baby-gru/src/components/BabyGruDisplayObjects.js
@@ -0,0 +1,307 @@
+import { useEffect, Fragment, useState, createRef, useCallback } from "react";
+import { Card, Form, Button, Row, Col, FormCheck } from "react-bootstrap";
+import { cootCommand, doDownload } from '../BabyGruUtils';
+import { DownloadOutlined, UndoOutlined, RedoOutlined, CenterFocusWeakOutlined, MoreVertOutlined } from '@mui/icons-material';
+import BabyGruSlider from "./BabyGruSlider";
+
+export const BabyGruMoleculeCard = (props) => {
+    const [showState, setShowState] = useState({})
+
+    useEffect(() => {
+        const initialState = {}
+        Object.keys(props.molecule.displayObjects).forEach(key => {
+            initialState[key] = props.molecule.displayObjects[key].length > 0
+                && props.molecule.displayObjects[key][0].visible
+        })
+        setShowState(initialState)
+    }, [
+        props.molecule.displayObjects.bonds.length,
+        props.molecule.displayObjects.sticks.length,
+        props.molecule.displayObjects.ribbons.length,
+        props.molecule.displayObjects.rama.length,
+        props.molecule.displayObjects.rotamer.length,
+    ])
+
+    return <Card className="px-0"  style={{marginBottom:'0.5rem', padding:'0'}} key={props.molecule.coordMolNo}>
+        <Card.Header>
+            <Row className='align-items-center'>
+                <Col>
+                    {`#${props.molecule.coordMolNo} Mol. ${props.molecule.name}`}
+                </Col>
+                <Col style={{display:'flex', justifyContent:'right'}}>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {
+                            cootCommand(props.cootWorker, {
+                                returnType: "status",
+                                command: "undo",
+                                commandArgs: [props.molecule.coordMolNo]
+                            }).then(_ => {
+                                props.molecule.setAtomsDirty(true)
+                                props.molecule.redraw(props.glRef)
+                            })
+                        }}><UndoOutlined /></Button>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {
+                            cootCommand(props.cootWorker, {
+                                returnType: "status",
+                                command: "redo",
+                                commandArgs: [props.molecule.coordMolNo]
+                            }).then(_ => {
+                                props.molecule.setAtomsDirty(true)
+                                props.molecule.redraw(props.glRef)
+                            })
+                        }}><RedoOutlined /></Button>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {props.molecule.centreOn(props.glRef)}}>
+                        <CenterFocusWeakOutlined />
+                    </Button>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {
+                            props.molecule.getAtoms()
+                                .then(reply => {
+                                    doDownload([reply.data.result.pdbData], `${props.molecule.name}`)
+                                })
+                        }}>
+                        <DownloadOutlined />
+                    </Button>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {console.log('More quick actions button not implemented yet')}}>
+                        <MoreVertOutlined />
+                    </Button>
+                </Col>
+            </Row>
+        </Card.Header>
+        <Card.Body>
+            {
+                Object.keys(props.molecule.displayObjects).map(key => {
+                    return <Form.Check
+                        inline
+                        label={`${key.substring(0, 3)}.`}
+                        feedbackTooltip={"Toggle on"}
+                        name={key}
+                        type="checkbox"
+                        variant="outline"
+                        checked={showState[key]}
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                props.molecule.show(key, props.glRef)
+                                const changedState = { ...showState }
+                                changedState[key] = true
+                                setShowState(changedState)
+                            }
+                            else {
+                                props.molecule.hide(key, props.glRef)
+                                const changedState = { ...showState }
+                                changedState[key] = false
+                                setShowState(changedState)
+                            }
+                        }}
+                    />
+                })
+            }
+        </Card.Body>
+    </Card >
+}
+
+const BabyGruMapCard = (props) => {
+    const [webMGContour, setWebMGContour] = useState(false)
+    const [cootContour, setCootContour] = useState(true)
+    const nextOrigin = createRef([])
+    const busyContouring = createRef(false)
+
+    const handleOriginCallback = useCallback(e => {
+        nextOrigin.current = [...e.detail.map(coord => -coord)]
+        if (props.map.cootContour) {
+            if (busyContouring.current) {
+                console.log('Skipping originChanged ', nextOrigin.current)
+            }
+            else {
+                busyContouring.current = true
+                props.map.doCootContour(props.glRef.current,
+                    ...nextOrigin.current,
+                    props.mapRadius,
+                    props.map.contourLevel)
+                    .then(result => {
+                        busyContouring.current = false
+                    })
+            }
+        }
+    }, [props.map.contourLevel, props.mapRadius])
+
+    const handleContourLevelCallback = useCallback(e => {
+        nextOrigin.current = [...e.detail.map(coord => -coord)]
+        if (props.map.cootContour) {
+            if (busyContouring.current) {
+                console.log('Skipping originChanged ', nextOrigin.current)
+            }
+            else {
+                busyContouring.current = true
+                props.map.doCootContour(props.glRef.current,
+                    ...nextOrigin.current,
+                    props.mapRadius,
+                    props.map.contourLevel)
+                    .then(result => {
+                        busyContouring.current = false
+                    })
+            }
+        }
+    }, [props.map.contourLevel, props.mapRadius])
+
+
+    useEffect(() => {
+        document.addEventListener("originChanged", handleOriginCallback);
+        document.addEventListener("contourLevelChanged", handleContourLevelCallback);
+        return () => {
+            document.removeEventListener("originChanged", handleOriginCallback);
+            document.removeEventListener("contourLevelChanged", handleContourLevelCallback);
+        };
+    }, [handleOriginCallback]);
+
+    useEffect(() => {
+        setWebMGContour(props.map.webMGContour)
+        setCootContour(props.map.cootContour)
+    }, [])
+
+    useEffect(() => {
+        setWebMGContour(props.map.webMGContour)
+        setCootContour(props.map.cootContour)
+        if (props.map.cootContour) {
+            busyContouring.current = true
+            props.map.doCootContour(props.glRef.current,
+                ...props.glRef.current.origin.map(coord => -coord),
+                props.mapRadius,
+                props.map.contourLevel)
+                .then(result => {
+                    busyContouring.current = false
+                })
+        }
+    }, [props.mapRadius, props.map.contourLevel])
+
+    return <Card className="px-0"  style={{marginBottom:'0.5rem', padding:'0'}} key={props.map.mapMolNo}>
+        <Card.Header>
+            <Row className='align-items-center'>
+                <Col>
+                    {`#${props.map.mapMolNo} Map ${props.map.name}`}
+                </Col>
+                <Col style={{display:'flex', justifyContent:'right'}}>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {
+                            props.map.getMap()
+                                .then(reply => {
+                                    doDownload([reply.data.result.mapData],
+                                        `${props.map.name.replace('.mtz', '.map')}`
+                                    )
+                                })
+                        }}>
+                        <DownloadOutlined />
+                    </Button>
+                    <Button size="sm" variant="outlined"
+                        onClick={() => {console.log('More quick actions button not implemented yet')}}>
+                        <MoreVertOutlined />
+                    </Button>
+                </Col>
+            </Row>
+        </Card.Header>
+        <Card.Body>
+            <Form.Check checked={props.map === props.activeMap}
+                        inline
+                        label={'Active'}
+                        name={`setActiveMap ${props.map.mapMolNo}`}
+                        type="checkbox"
+                        variant="outline"
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                props.setActiveMap(props.map)
+                            }
+                        }}
+            />
+            <FormCheck  checked={webMGContour}
+                        inline
+                        label={'WC'}
+                        name={`setWC ${props.map.mapMolNo}`}
+                        type="checkbox"
+                        variant="outline"
+                        onChange={(newState) => {
+                            if (newState.target.checked && !webMGContour) {
+                                props.map.makeWebMGLive(props.glRef.current)
+                                setWebMGContour(true)
+                            }
+                            else if (!newState.target.checked && webMGContour) {
+                                props.map.makeWebMGUnlive(props.glRef.current)
+                                setWebMGContour(false)
+                            }
+                }}
+            />
+            <FormCheck  checked={cootContour}
+                        inline
+                        label={'CC'}
+                        name={`setCC ${props.map.mapMolNo}`}
+                        type="checkbox"
+                        variant="outline"
+                        onChange={(newState) => {
+                            if (newState.target.checked && !cootContour) {
+                                props.map.makeCootLive(props.glRef.current, props.mapRadius)
+                                setCootContour(true)
+                            } 
+                            else if (!newState.target.checked && cootContour) {
+                                props.map.makeCootUnlive(props.glRef.current)
+                                setCootContour(false)
+                            }
+                }} 
+            />
+        </Card.Body>
+    </Card >
+    }
+
+
+export const BabyGruDisplayObjects = (props) => {
+    const [mapRadius, setMapRadius] = useState(15.)
+    const busyContouring = createRef(false)
+
+    const recontourActiveMap = (newLevel) => {
+        if (props.activeMap) {
+            props.activeMap.contourLevel = newLevel
+            if (props.activeMap.cootContour) {
+                if (busyContouring.current) {
+                }
+                else {
+                    busyContouring.current = true
+                    props.activeMap.doCootContour(props.glRef.current,
+                        ...props.glRef.current.origin,
+                        mapRadius,
+                        props.activeMap.contourLevel)
+                        .then(result => {
+                            busyContouring.current = false
+                        })
+                }
+            }
+        }
+    }
+
+    useEffect(() => {
+    }, [])
+
+    let displayData = [];
+    // TODO: Concatenate molecules and maps, sort them by coordMolNo and then push them in that order...
+    if (props.molecules.length!=0) {
+        props.molecules.forEach(molecule => displayData.push(
+            <BabyGruMoleculeCard key={molecule.coordMolNo}
+                molecule={molecule}
+                glRef={props.glRef}
+                cootWorker={props.cootWorker}>
+            </BabyGruMoleculeCard>
+            )
+        )
+    } 
+    
+    if (props.maps.length!=0) {
+        props.maps.forEach(map => displayData.push(
+            <BabyGruMapCard {...props} mapRadius={mapRadius} map={map}/>
+        ))
+    }   
+
+    return <Fragment>
+                {displayData}
+            </Fragment>
+}
+
