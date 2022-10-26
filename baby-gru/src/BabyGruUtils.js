c42fb693681e34acd3a39302f5063f6ff815db57
Martin Noble <martinemnoble1@mac.com>
2022-10-26 18:25:00 +0100

    Adding download of session history to history menu

--- baby-gru/src/BabyGruUtils.js
+++ baby-gru/src/BabyGruUtils.js
@@ -71,3 +71,15 @@ export const doDownload = (data, targetName) => {
     link.parentNode.removeChild(link);
 }
 
+export const doDownloadText = (text, filename) => {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
--- baby-gru/src/components/BabyGruHistoryMenu.js
+++ baby-gru/src/components/BabyGruHistoryMenu.js
@@ -2,11 +2,12 @@ import { NavDropdown, Form, Button, InputGroup, NavItem } from "react-bootstrap"
 import { BabyGruMolecule } from "./BabyGruMolecule";
 import { BabyGruMap } from "./BabyGruMap";
 import { useEffect, useState } from "react";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Modal} from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Modal } from "@mui/material";
+import { doDownload, doDownloadText } from "../BabyGruUtils";
 
 export const BabyGruHistoryMenu = (props) => {
     const [showHistory, setShowHistory] = useState(false)
-    const [sessionHistory, setSessionHistory] = useState({commands:[]})
+    const [sessionHistory, setSessionHistory] = useState({ commands: [] })
 
     useEffect(() => {
         setSessionHistory(props.journalState)
@@ -25,9 +26,22 @@ export const BabyGruHistoryMenu = (props) => {
                     }}
                 />
             </Form.Group>
+            <Form.Group style={{ width: '20rem', margin: '0.5rem' }} controlId="downloadHistory" className="mb-3">
+                <Form.Label>Download history as JSON</Form.Label>
+                <Form.Control
+                    type="button"
+                    value="Download"
+                    placeholder="Download"
+                    aria-label="Download history"
+                    onClick={(e) => {
+                        const json = JSON.stringify(sessionHistory, null, 2)
+                        doDownloadText(json, "BabyGruSession.json")
+                    }}
+                />
+            </Form.Group>
         </NavDropdown>
-        <Modal open={showHistory} onClose={()=>{setShowHistory(false)}}>
-            <TableContainer component={Paper} sx={{width:"60rem"}}>
+        <Modal open={showHistory} onClose={() => { setShowHistory(false) }}>
+            <TableContainer component={Paper} sx={{ width: "60rem" }}>
                 <Table aria-label="caption table">
                     <caption>A basic table example with a caption</caption>
                     <TableHead>
