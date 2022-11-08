22ad1991d59c0645e102ae71f12d21b08d127d28
Martin Noble <martinemnoble1@mac.com>
2022-11-08 08:47:44 +0000

    Preparing to add ligand menu

---
+++ baby-gru/src/components/BabyGruLigandMenu.js
@@ -0,0 +1,23 @@
+import { NavDropdown, Form, Button, InputGroup, Modal, FormSelect, Col, Row, Overlay, Card, FormCheck, OverlayTrigger, Tooltip, Popover } from "react-bootstrap";
+import { BabyGruMolecule } from "./BabyGruMolecule";
+import { BabyGruMap } from "./BabyGruMap";
+import { useEffect, useState, useRef, createRef } from "react";
+import { BabyGruMtzWrapper, cootCommand, readTextFile } from '../BabyGruUtils';
+import { InsertDriveFile } from "@mui/icons-material";
+import { MenuItem } from "@mui/material";
+import { BabyGruMenuItem } from "./BabyGruMenuItem";
+
+export const BabyGruLigandMenu = (props) => {
+
+    return <>
+        <NavDropdown title="Ligand" id="basic-nav-dropdown">
+            <BabyGruMenuItem
+                popoverContent="Hello"
+                menuItemTitle="Hiya"
+            />
+        </NavDropdown>
+    </>
+}
+
+
+
