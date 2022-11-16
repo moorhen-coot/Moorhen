8bcc871ab1edfc683391bff25cb5e637094b85e0
FilomenoSanchez <filo_san_rod@hotmail.com>
2022-11-16 10:39:31 +0000

    add default height for selectors

--- baby-gru/src/components/BabyGruMapSelect.js
+++ baby-gru/src/components/BabyGruMapSelect.js
@@ -9,7 +9,7 @@ export const BabyGruMapSelect = forwardRef((props, selectRef) => {
         }
     }
 
-    return <Form.Group style={{ width: props.width, margin: '0.5rem' }}>
+    return <Form.Group style={{ width: props.width, margin: '0.5rem', height:props.height }}>
         <Form.Label>{props.label}</Form.Label>
         <FormSelect size="sm" ref={selectRef} defaultValue={-999999} onChange={handleChange}>
             {props.maps ? props.maps.map(map => <option key={map.molNo} value={map.molNo}>{map.mapName}</option>) : null}
@@ -17,4 +17,4 @@ export const BabyGruMapSelect = forwardRef((props, selectRef) => {
     </Form.Group>
 })
 
-BabyGruMapSelect.defaultProps = { width: '20rem', maps:null, label: "Map" }
+BabyGruMapSelect.defaultProps = { height: '4rem', width: '20rem', maps:null, label: "Map" }
