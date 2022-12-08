02064927264c2354380d76880f8337f719be7ef8
FilomenoSanchez <filo_san_rod@hotmail.com>
2022-12-08 10:34:53 +0000

    functional search bar

--- baby-gru/src/components/BabyGruSearchBar.js
+++ baby-gru/src/components/BabyGruSearchBar.js
@@ -7,30 +7,7 @@ export const BabyGruSearchBar = (props) => {
     const selectRef = useRef()
     const searchBarRef = useRef()
     const [selectedItemKey, setSelectedItemKey] = useState(null)
-
-    const handleTest = () => {
-        let element;
-        let clickEvent;
-        clickEvent = new MouseEvent("click", {
-            "view": window,
-            "bubbles": true,
-            "cancelable": false
-        })
-        element = document.getElementById('file-nav-dropdown')
-        element.dispatchEvent(clickEvent)    
-        setTimeout(() => {
-            let element;
-            let clickEvent;
-            clickEvent = new MouseEvent("click", {
-                "view": window,
-                "bubbles": true,
-                "cancelable": false
-            })
-            element  = document.getElementById('upload-coordinates-form')
-            element.dispatchEvent(clickEvent)    
-        }, 50);        
-
-    }
+    const [openPopup, setOpenPopup] = useState(null)
 
     const handleActions = (...actions) => {
         actions.forEach((action, actionIndex) => {
@@ -45,7 +22,7 @@ export const BabyGruSearchBar = (props) => {
                 let element;
                 let clickEvent;
                 if (action.type === 'click'){
-                    console.log(`CLICK ON ${action.componentId}`)
+                    console.log(`Search bar is clicking on ${action.componentId}`)
                     clickEvent = new MouseEvent("click", {
                         "view": window,
                         "bubbles": true,
@@ -54,7 +31,12 @@ export const BabyGruSearchBar = (props) => {
                     element = document.getElementById(action.componentId)
                     element.dispatchEvent(clickEvent)    
                 } else if (action.type === 'setValue') {
+                    console.log(`Search bar is setting a new value ${action.newValue}`)
                     action.valueSetter(action.newValue)
+                } else if (action.type === 'setFocus') {
+                    console.log(`Search bar is setting focus on component ${action.componentId}`)
+                    element = document.getElementById(action.componentId)
+                    element.focus()
                 }
             }, parseInt(50 * actionIndex));
         })
@@ -62,25 +44,30 @@ export const BabyGruSearchBar = (props) => {
 
     const searchOptions = [
         {label: "Difference Map Peaks", actions: [
-            {type: 'click', condition: true , componentId: 'show-sidebar-button'}, 
-            {type: 'click', condition: true, componentId: 'tools-accordion-button'},
+            {type: 'click', condition: !props.showSideBar , componentId: 'show-sidebar-button'}, 
+            {type: 'click', condition: props.toolAccordionBodyHeight == 0, componentId: 'tools-accordion-button'},
             {type: 'setValue', newValue: 0, condition: true, valueSetter: props.setSelectedToolKey}
         ]},
         {label: "Fetch from PDBe", actions: [
-            {type: 'click', componentId: 'file-nav-dropdown', condition: true}, {type: 'click', componentId: 'fetch-pdbe-form', condition: true}
+            {type: 'click', componentId: 'file-nav-dropdown', condition: props.currentDropdownId !== "File"}, 
+            {type: 'setFocus', componentId: 'fetch-pdbe-form', condition: true}
         ]},
         {label: "Flip Peptide", actions: [
             {type: 'click', componentId: 'flip-peptide-edit-button', condition: true}
         ]},
         {label: "Load coordinates", actions: [
-            {type: 'click', componentId: 'file-nav-dropdown', condition: true}, {type: 'click', componentId: 'upload-coordinates-form', condition: true}
+            {type: 'click', componentId: 'file-nav-dropdown', condition: props.currentDropdownId !== "File"},
+            {type: 'setValue', newValue:'File', valueSetter: props.setCurrentDropdownId, condition: true},
+            {type: 'click', componentId: 'upload-coordinates-form', condition: true}
         ]},
         {label: "Load tutorial data", actions: [
-            {type: 'click', componentId: 'file-nav-dropdown', condition: true}, {type: 'click', componentId: 'load-tutorial-data-menu-item', condition: true}
+            {type: 'click', componentId: 'file-nav-dropdown', condition: props.currentDropdownId !== "File"},
+            {type: 'setValue', newValue:'File', valueSetter: props.setCurrentDropdownId, condition: true},
+            {type: 'click', componentId: 'load-tutorial-data-menu-item', condition: true}
         ]},
         {label: "Ramachandran Plot", actions: [
-            {type: 'click', condition: true , componentId: 'show-sidebar-button'}, 
-            {type: 'click', condition: true, componentId: 'tools-accordion-button'},
+            {type: 'click', condition: !props.showSideBar , componentId: 'show-sidebar-button'}, 
+            {type: 'click', condition: props.toolAccordionBodyHeight == 0, componentId: 'tools-accordion-button'},
             {type: 'setValue', newValue: 1, condition: true, valueSetter: props.setSelectedToolKey}
         ]},
     ]
@@ -111,7 +98,23 @@ export const BabyGruSearchBar = (props) => {
     return <Fragment> 
         <Row style={{padding: '0.5rem', width: '20rem'}}>
             <Autocomplete 
+                    ref={selectRef}
                     disablePortal
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    freeSolo
+                    includeInputInList
+                    filterSelectedOptions
+                    open={openPopup}
+                    onInputChange={(_, value) => {
+                        if (value.length === 0) {
+                            if (openPopup) setOpenPopup(false);
+                        } else {
+                              if (!openPopup) setOpenPopup(true);
+                        }
+                    }}
+                    onClose={() => setOpenPopup(false)}
                     sx={{
                         '& .MuiInputBase-root': {
                             backgroundColor:  props.darkMode ? '#222' : 'white',
@@ -126,8 +129,7 @@ export const BabyGruSearchBar = (props) => {
                         '& .MuiFormLabel-root': {
                             color: props.darkMode ? 'white' : '#222',
                         },
-                        }}                        
-                    ref={selectRef}
+                    }}               
                     onChange={handleChange}
                     size='small'
                     options={searchOptions.map(item => item.label)}
