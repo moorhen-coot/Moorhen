8b02c56674ad86fe4b9e3e814cf07174952556c9
FilomenoSanchez <filo_san_rod@hotmail.com>
2022-12-15 17:33:23 +0000

    enable session download and upload

--- baby-gru/src/components/BabyGruFileMenu.js
+++ baby-gru/src/components/BabyGruFileMenu.js
@@ -3,6 +3,8 @@ import { BabyGruMolecule } from "../utils/BabyGruMolecule";
 import { BabyGruMap } from "../utils/BabyGruMap";
 import { useState, useRef } from "react";
 import { BabyGruImportDictionaryMenuItem, BabyGruImportMapCoefficientsMenuItem, BabyGruDeleteEverythingMenuItem, BabyGruLoadTutorialDataMenuItem, BabyGruImportMapMenuItem, BabyGruImportFSigFMenuItem } from "./BabyGruMenuItem";
+import { MenuItem } from "@mui/material";
+import { doDownload, readTextFile } from "../utils/BabyGruUtils";
 
 export const BabyGruFileMenu = (props) => {
 
@@ -100,6 +102,87 @@ export const BabyGruFileMenu = (props) => {
         }
     }
 
+    const loadSession = async (file) => {
+        // Load session data
+        let sessionData = await readTextFile(file)
+        sessionData = JSON.parse(sessionData)
+        console.log(sessionData)
+        
+        // Delete current scene
+        props.molecules.forEach(molecule => {
+            molecule.delete(props.glRef)
+        })
+        changeMolecules({ action: "Empty" })
+
+        // Load molecules stored in session from pdb string
+        let newMoleculePromises = [];
+        let newMolecule;
+        sessionData.moleculesPdbData.forEach((pdbData, index) => {
+            newMolecule = new BabyGruMolecule(commandCentre)
+            newMoleculePromises.push(
+                newMolecule.loadToCootFromString(pdbData, sessionData.moleculesNames[index])
+            )
+        })
+        let newMolecules = await Promise.all(newMoleculePromises)
+
+        // Draw the molecules with the styles stored in session
+        let drawPromises = []
+        newMolecules.forEach((molecule, moleculeIndex) => {
+            molecule.cootBondsOptions = sessionData.moleculesCootBondsOptions[moleculeIndex]
+            const styles = sessionData.moleculesDisplayObjectsKeys[moleculeIndex]
+            styles.forEach(style => drawPromises.push(molecule.fetchIfDirtyAndDraw(style, glRef, true)))
+        })
+        await Promise.all(drawPromises)
+        
+        // Change props.molecules
+        newMolecules.forEach(molecule => {
+            changeMolecules({ action: "Add", item: molecule })
+        })
+
+        // Set camera details
+        glRef.current.setAmbientLightNoUpdate(...Object.values(sessionData.ambientLight))
+        glRef.current.setSpecularLightNoUpdate(...Object.values(sessionData.specularLight))
+        glRef.current.setDiffuseLightNoUpdate(...Object.values(sessionData.diffuseLight))
+        glRef.current.setLightPositionNoUpdate(...Object.values(sessionData.lightPosition))
+        glRef.current.setZoom(sessionData.zoom, false)
+        glRef.current.set_fog_range(sessionData.fogStart, sessionData.fogEnd, false)
+        glRef.current.set_clip_range(sessionData.clipStart, sessionData.clipEnd, false)
+        glRef.current.doDrawClickedAtomLines = sessionData.doDrawClickedAtomLines
+        glRef.current.atomLabelDepthMode = sessionData.atomLabelDepthMode
+        glRef.current.background_colour = sessionData.backgroundColor
+        glRef.current.setOrigin(sessionData.origin, false)
+        glRef.current.setQuat(sessionData.quat4)
+
+    }
+
+    const downloadSession = async () => {
+        let promises = props.molecules.map(molecule => {return molecule.getAtoms()})
+        let response = await Promise.all(promises)
+
+        let session = {
+            moleculesNames: props.molecules.map(molecule => molecule.name),
+            moleculesPdbData: response.map(item => item.data.result.pdbData),
+            moleculesDisplayObjectsKeys: props.molecules.map(molecule => Object.keys(molecule.displayObjects).filter(key => molecule.displayObjects[key].length > 0)),
+            moleculesCootBondsOptions: props.molecules.map(molecule => molecule.cootBondsOptions),
+            origin: props.glRef.current.origin,
+            backgroundColor: props.backgroundColor,
+            atomLabelDepthMode: props.atomLabelDepthMode,
+            ambientLight: glRef.current.light_colours_ambient,
+            diffuseLight: glRef.current.light_colours_diffuse,
+            lightPosition: glRef.current.light_positions,
+            specularLight: glRef.current.light_colours_specular,
+            fogStart: glRef.current.gl_fog_start,
+            fogEnd: glRef.current.gl_fog_end,
+            zoom: glRef.current.zoom,
+            doDrawClickedAtomLines: glRef.current.doDrawClickedAtomLines,
+            clipStart: (glRef.current.gl_clipPlane0[3] + 500) * -1,
+            clipEnd: glRef.current.gl_clipPlane1[3] - 500,
+            quat4: glRef.current.myQuat
+        }
+
+        doDownload([JSON.stringify(session)], `session.json`)
+    }
+
     return <>
         <NavDropdown
             title="File"
@@ -134,6 +217,11 @@ export const BabyGruFileMenu = (props) => {
                 </InputGroup>
             </Form.Group>
 
+            <Form.Group style={{ width: '20rem', margin: '0.5rem' }} controlId="upload-session-form" className="mb-3">
+                <Form.Label>Load from stored session</Form.Label>
+                <Form.Control type="file" accept=".json" multiple={false} onChange={(e) => { loadSession(e.target.files[0]) }}/>
+            </Form.Group>
+
             <BabyGruImportMapCoefficientsMenuItem {...menuItemProps} />
 
             <BabyGruImportFSigFMenuItem {...menuItemProps} />
@@ -143,6 +231,10 @@ export const BabyGruFileMenu = (props) => {
             <BabyGruImportDictionaryMenuItem {...menuItemProps} />
 
             <BabyGruLoadTutorialDataMenuItem {...menuItemProps} />
+
+            <MenuItem id='download-session-menu-item' variant="success" onClick={downloadSession}>
+                Download session
+            </MenuItem>
 
             <BabyGruDeleteEverythingMenuItem {...menuItemProps} />
 
