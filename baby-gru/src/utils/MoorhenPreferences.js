f6e418f190d71290093d8ce2c8d378a2abe1d973
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-01-30 19:44:45 +0000

    update preferences version number

--- baby-gru/src/utils/MoorhenPreferences.js
+++ baby-gru/src/utils/MoorhenPreferences.js
@@ -12,7 +12,7 @@ const updateStoredPreferences = async (key, value) => {
 
 const getDefaultValues = () => {
     return {
-        version: '0.0.7',
+        version: '0.0.8',
         darkMode: false, 
         atomLabelDepthMode: true, 
         defaultExpandDisplayCards: true,
