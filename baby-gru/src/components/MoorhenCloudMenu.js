49b0f7ec826fe37b0d8c7d4b565808233c5b61dd
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-02-12 19:10:38 +0000

    update moorhen cloud wrapper as requested

--- baby-gru/src/components/MoorhenCloudMenu.js
+++ baby-gru/src/components/MoorhenCloudMenu.js
@@ -8,23 +8,7 @@ export const MoorhenCloudMenu = (props) => {
     const exportToCloud = async () => {
         let moleculePromises = props.molecules.map(molecule => {return molecule.getAtoms()})
         let moleculeAtoms = await Promise.all(moleculePromises)
-        let mapPromises = props.maps.map(map => {return map.getMap()})
-        let mapData = await Promise.all(mapPromises)
-
-        const result = {
-            moleculesNames: props.molecules.map(molecule => molecule.name),
-            moleculesPdbData: moleculeAtoms.map(item => item.data.result.pdbData),
-            mapsNames: props.maps.map(map => map.name),
-            mapsMapData: mapData.map(item => new Uint8Array(item.data.result.mapData)),
-        }
-        
-        let exportEvent = new CustomEvent("moorhenDataExportEvent", {
-            "detail": {
-                result
-            }
-        })
-        
-        document.dispatchEvent(exportEvent)
+        props.molecules.forEach((molecule, index) => props.exportToCloudCallback(molecule.name, moleculeAtoms[index].data.result.pdbData))
     }
 
     return <>
