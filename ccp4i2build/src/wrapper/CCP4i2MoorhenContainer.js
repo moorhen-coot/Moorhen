ea24152a1190e26a948c4c6cd927e4b05ed530d3
Martin Noble <martinemnoble1@mac.com>
2023-03-15 18:46:01 +0000

    Near complete database interaction in ccp4i2build

--- ccp4i2build/src/wrapper/CCP4i2MoorhenContainer.js
+++ ccp4i2build/src/wrapper/CCP4i2MoorhenContainer.js
@@ -4,10 +4,11 @@ import { MoorhenContainer } from '../components/MoorhenContainer';
 import { MoorhenMolecule } from '../utils/MoorhenMolecule';
 import { MoorhenMap } from '../utils/MoorhenMap';
 import { PreferencesContextProvider } from '../utils/MoorhenPreferences';
-import { NavDropdown } from 'react-bootstrap';
-import { MenuItem } from '@mui/material';
+import { Modal, NavDropdown, Table } from 'react-bootstrap';
+import { Button, MenuItem, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import { MoorhenMoleculeSelect } from '../components/MoorhenMoleculeSelect';
 import $ from 'jquery';
+import { ArrowBack } from '@mui/icons-material';
 
 export const CCP4i2MoorhenContainer = (props) => {
     const controls = useRef(null);
@@ -201,6 +202,7 @@ export const CCP4i2MoorhenContainer = (props) => {
             cootJobId={props.cootJob}
             controls={controls}
             urlRoot={urlRoot}
+            makeFilePromise={makeFilePromise}
         />]}
         controls={controls}
         lookup={props.lookup}
@@ -214,6 +216,9 @@ const MoorhenCCP4i2Menu = (props) => {
     const [popoverIsShown, setPopoverIsShown] = useState(false)
     const [showMenu, setShowMenu] = useState(false)
     const [iSave, setISave] = useState(0)
+    const [showModal, setShowModal] = useState(false)
+    const [showMoleculeSelectorModal, setShowMoleculeSelectorModal] = useState(false)
+    const selectorRef = useRef(null)
 
     useEffect(() => {
         console.log('menuProps', props)
@@ -222,6 +227,32 @@ const MoorhenCCP4i2Menu = (props) => {
     useEffect(() => {
         console.log('menuProps.currentDr', props.currentDropdownId)
     }, [props.currentDropdownId])
+
+    const saveMolNo = async (molNo) => {
+        const formData = new FormData()
+        formData.append('jobId', props.cootJobId)
+        formData.append('fileRoot', `COOT_FILE_DROP/output_${iSave.toString().padStart(3, '0')}`)
+        formData.append('fileExtension', ".pdb")
+        const molZeros = props.controls.current.moleculesRef.current.filter(molecule => molecule.molNo === molNo)
+        if (molZeros.length === 1) {
+            console.log('molecules', props.controls.current)
+            let response = await molZeros[0].getAtoms()
+            const atomsBlob = new Blob([response.data.result.pdbData])
+            formData.append('file', atomsBlob)
+            fetch(`${props.urlRoot}/uploadFileToJob`, {
+                method: "POST",
+                body: formData
+            })
+                .then(response => response.json())
+                .then(result => {
+                    setISave(iSave + 1)
+                    props.controls.current.setToastContent("File saved")
+                })
+        }
+        else {
+            props.controls.current.setToastContent(`No mols with index ${molNo}`)
+        }
+    }
 
     return <NavDropdown
         title="CCP4i2"
@@ -241,31 +272,21 @@ const MoorhenCCP4i2Menu = (props) => {
             }
         }}>
 
+        <MenuItem key="Find files" onClick={() => {
+            setShowModal(true)
+        }}>Add files
+        </MenuItem>
+
         <MenuItem key="Save to CCP4i2" onClick={async () => {
-            const formData = new FormData()
-            formData.append('jobId', props.cootJobId)
-            formData.append('fileRoot', `COOT_FILE_DROP/output_${iSave.toString().padStart(3, '0')}`)
-            formData.append('fileExtension', ".pdb")
-            const molZeros = props.controls.current.moleculesRef.current.filter(molecule => molecule.molNo == 0)
-            if (molZeros.length === 1) {
-                console.log('molecules', props.controls.current)
-                let response = await molZeros[0].getAtoms()
-                const atomsBlob = new Blob([response.data.result.pdbData])
-                formData.append('file', atomsBlob)
-                fetch(`${props.urlRoot}/uploadFileToJob`, {
-                    method: "POST",
-                    body: formData
-                })
-                    .then(response => response.json())
-                    .then(result => {
-                        setISave(iSave + 1)
-                        props.controls.current.setToastContent("File saved")
-                    })
-            }
-            else {
-                props.controls.current.setToastContent("No molZeros")
-            }
-        }}>Save mol 0 to ccp4i2</MenuItem>
+            saveMolNo(0)
+        }}>Save mol 0 to ccp4i2
+        </MenuItem>
+
+        <MenuItem key="Select molecule to save" onClick={() => {
+            setShowMoleculeSelectorModal(true)
+        }}>Select molecule to save
+        </MenuItem>
+
         <MenuItem key="End session" onClick={() => {
             fetch(`${props.urlRoot}/makeTerminateFile?jobId=${props.cootJobId}`)
                 .then(response => response.json())
@@ -274,7 +295,172 @@ const MoorhenCCP4i2Menu = (props) => {
                         window.location.href = "about:blank"
                     }
                 })
-        }}>End job</MenuItem>
+        }}>End job
+        </MenuItem>
+
+        <Modal key="Data browser modal" show={showModal} onHide={() => {
+            setShowModal(false)
+        }}>
+            <Modal.Body>
+                <CCP4i2DataBrowser urlRoot={props.urlRoot} updated={showModal} makeFilePromise={props.makeFilePromise} />
+            </Modal.Body>
+        </Modal>
+
+        <Modal key="Save selector modal" show={showMoleculeSelectorModal} onHide={() => {
+            setShowMoleculeSelectorModal(false)
+        }}><Modal.Body>
+                {props.controls.current &&
+                    <MoorhenMoleculeSelect ref={selectorRef} molecules={props.controls.current.moleculesRef.current} allowAny={false} />
+                }
+                <Button onClick={() => {
+                    saveMolNo(parseInt(selectorRef.current.value))
+                    setShowMoleculeSelectorModal(false)
+                }}>OK</Button>
+                <Button onClick={() => { setShowMoleculeSelectorModal(false) }}>Cancel</Button>
+            </Modal.Body>
+        </Modal>
     </NavDropdown>
 }
 
+const CCP4i2DataBrowser = (props) => {
+    const [level, setLevel] = useState("Projects")
+    const [project, setProject] = useState({})
+    const [job, setJob] = useState({})
+    const projectsPanel = <CCP4i2ProjectsPanel {...props} setProject={setProject} setLevel={setLevel} />
+    const jobsPanel = <CCP4i2JobsPanel {...props} project={project} setLevel={setLevel} setJob={setJob} />
+    const filesPanel = <CCP4i2FilesPanel {...props} job={job} setLevel={setLevel} />
+
+    return level === "Projects" ? projectsPanel : level === "Jobs" ? jobsPanel : level === "Files" ? filesPanel : "No data"
+}
+
+const CCP4i2ProjectsPanel = (props) => {
+    const [projects, setProjects] = useState([])
+    useEffect(() => {
+        if (props.updated) {
+            fetch(`${props.urlRoot}/ModelValues?${$.param({
+                __type__: "Projects"
+            })}`)
+                .then(response => response.json())
+                .then(result => { setProjects(result.results) })
+        }
+    }, [props.updated])
+
+    return projects.length > 0 ? <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell>Projects</TableCell>
+            </TableRow>
+        </TableHead>
+
+        <TableBody> {projects.map(project => {
+            return <TableRow key={project.projectid}>
+                <TableCell
+                    onClick={() => {
+                        props.setLevel("Jobs")
+                        props.setProject(project)
+                    }}
+                >{project.projectname}
+                </TableCell>
+            </TableRow>
+        })}
+        </TableBody>
+
+    </Table> : <span>No data</span>
+}
+
+const CCP4i2JobsPanel = (props) => {
+    const [jobs, setJobs] = useState([])
+    useEffect(() => {
+        if (props.project.projectid) {
+            fetch(`${props.urlRoot}/ModelValues?${$.param({
+                __type__: "Jobs",
+                projectid__projectid: props.project.projectid,
+                status__statustext: "Finished",
+                parentjobid__isnull: true,
+            })}`)
+                .then(response => response.json())
+                .then(result => {
+                    setJobs(result.results.sort((a, b) => {
+                        const aN = parseInt(a.jobnumber)
+                        const bN = parseInt(b.jobnumber)
+                        return aN < bN ? 1 : bN < aN ? -1 : 0
+                    }))
+                })
+
+        }
+    }, [props.project])
+    return jobs.length > 0 ? <Table>
+        <TableHead>
+            <TableRow><Button onClick={() => { props.setLevel("Projects") }}><ArrowBack /></Button>
+                Jobs of project {props.project.projectname}</TableRow>
+        </TableHead>
+        <TableBody>
+            {jobs.map(job => <TableRow key={job.jobid}>
+                <TableCell onClick={() => {
+                    props.setJob(job)
+                    props.setLevel("Files")
+                }}><img
+                        style={{ width: "2.0rem", height: "2.0rem", marginRight: "1rem" }}
+                        title={job.taskname}
+                        src={`${props.urlRoot}/svgicons/${job.taskname}`} alt={`${job.taskname} `} />
+                    {job.jobnumber}: {job.jobtitle}
+                </TableCell>
+            </TableRow>)}
+        </TableBody>
+    </Table> :
+        <span><Button onClick={() => { props.setLevel("Projects") }}><ArrowBack /></Button>No jobs in project {props.project.projectname}</span>
+}
+
+const CCP4i2FilesPanel = (props) => {
+    const [files, setFiles] = useState([])
+    const fileTypeMapping = {
+        "application/CCP4-mtz-observed": "ObsDataFile",
+        "application/CCP4-mtz-freerflag": "FreeRDataFile",
+        "application/CCP4-mtz-map": "MapCoeffsDataFile",
+        "application/CCP4-mtz-phases": "PhsDataFile",
+        "application/refmac-dictionary": "DictDataFile",
+        "application/coot-script": "CootHistoryDataFile",
+        "application/CCP4-unmerged-experimental": "UnmergedDataFile",
+        "chemical/x-pdb": "PdbDataFile",
+        "application/CCP4-asu-content": "AsuDataFile"
+    }
+    useEffect(() => {
+        if (props.job.jobid) {
+            const predicate = {
+                __type__: "Files",
+                __values__: ["filetypeid__filetypename", "annotation", "fileid", "jobid__jobnumber", "filesubtype"],
+                jobid__jobid: props.job.jobid,
+                filetypeid__filetypename__in: ["application/CCP4-mtz-map", "application/refmac-dictionary", "chemical/x-pdb"]
+            }
+            console.log({ predicate })
+            fetch(`${props.urlRoot}/ModelValues?${$.param(predicate)}`)
+                .then(response => response.json())
+                .then(result => {
+                    setFiles(result.results)
+                })
+        }
+    }, [props.job])
+    return files.length > 0 ? <Table>
+        <TableHead>
+            <TableRow><Button onClick={() => { props.setLevel("Jobs") }}><ArrowBack /></Button>
+                Files of job {props.job.jobnumber}</TableRow>
+        </TableHead>
+        <TableBody>
+            {files.map(file => <TableRow key={file.fileid}>
+                <TableCell onClick={() => {
+                    const fileUrl = `${props.urlRoot}/getFileWithPredicate?fileid=${file.fileid}`
+                    const mimeType = file.filetypeid__filetypename
+                    const annotation = `${file.jobid__jobnumber} : ${file.annotation}`
+                    const subType = file.filesubtype
+                    props.makeFilePromise(fileUrl, mimeType, annotation, subType)
+                }}><img
+                        style={{ width: "2.0rem", height: "2.0rem", marginRight: "1rem" }}
+                        title={file.filetypeid__filetypename}
+                        src={`${props.urlRoot}/qticons/${fileTypeMapping[file.filetypeid__filetypename]}`} alt={`${fileTypeMapping[file.filetypeid__filetypename]} `} />
+                    {file.jobid__jobnumber}: {file.annotation}
+                </TableCell>
+            </TableRow>)}
+        </TableBody>
+    </Table> :
+        <span><Button onClick={() => { props.setLevel("Jobs") }}><ArrowBack /></Button>No files in job {props.job.jobnumber}</span>
+}
