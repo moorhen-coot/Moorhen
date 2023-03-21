dbe668cfc1845617688b2d65e2f81995daeee7cd
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-03-21 18:23:46 +0000

    add fixme tag

--- baby-gru/src/components/MoorhenSimpleEditButton.js
+++ baby-gru/src/components/MoorhenSimpleEditButton.js
@@ -47,6 +47,7 @@ const MoorhenSimpleEditButton = forwardRef((props, buttonRef) => {
                 }
             }
             molecule.setAtomsDirty(true)
+            // FIXME: await here is only necessary to show timings
             await molecule.redraw(props.glRef)
             const scoresUpdateEvent = new CustomEvent("scoresUpdate", { detail: { origin: props.glRef.current.origin, modifiedMolecule: molecule.molNo } })
             document.dispatchEvent(scoresUpdateEvent)
