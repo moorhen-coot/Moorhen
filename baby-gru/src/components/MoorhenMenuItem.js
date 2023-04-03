770c28d2b5010078641b873f20ddea36f7df74b1
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-04-03 11:39:21 +0100

    move colour change button somewhere more obvious

--- baby-gru/src/components/MoorhenMenuItem.js
+++ baby-gru/src/components/MoorhenMenuItem.js
@@ -594,26 +594,6 @@ export const MoorhenScoresToastPreferencesMenuItem = (props) => {
 }
 
 export const MoorhenMapSettingsMenuItem = (props) => {
-    const [mapColour, setMapColour] = useState(null)
-
-    useEffect(() => {
-        setMapColour({
-            r: 255 * props.map.rgba.r,
-            g: 255 * props.map.rgba.g,
-            b: 255 * props.map.rgba.b,
-            a: props.map.rgba.a
-        })
-    }, [props.map.rgba])
-
-    const handleColorChange = (color) => {
-        try {
-            props.map.setColour(color.rgb.r / 255., color.rgb.g / 255., color.rgb.b / 255., props.glRef)
-            setMapColour(color)
-        }
-        catch (err) {
-            console.log('err', err)
-        }
-    }
 
     const panelContent =
         <>
@@ -632,14 +612,6 @@ export const MoorhenMapSettingsMenuItem = (props) => {
             <Form.Group style={{ width: '100%', margin: '0.1rem' }} controlId="MoorhenMapOpacitySlider">
                 <MoorhenSlider minVal={0.0} maxVal={1.0} logScale={false} sliderTitle="Opacity" initialValue={props.mapOpacity} externalValue={props.mapOpacity} setExternalValue={props.setMapOpacity} />
             </Form.Group>
-            {!props.map.isDifference &&
-                <Form.Group>
-                    <Form.Label>
-                        Map Colour
-                    </Form.Label>
-                    <SketchPicker color={mapColour} onChange={handleColorChange} disableAlpha={true} />
-                </Form.Group>
-            }
         </>
     return <MoorhenMenuItem
         popoverPlacement='left'
