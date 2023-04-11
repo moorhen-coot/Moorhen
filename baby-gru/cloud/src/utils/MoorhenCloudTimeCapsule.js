1d2ae2432901f6d0570c5d9cb6a03088f4efe459
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-04-11 15:30:26 +0100

    update cloud storage instance

--- baby-gru/cloud/src/utils/MoorhenCloudTimeCapsule.js
+++ baby-gru/cloud/src/utils/MoorhenCloudTimeCapsule.js
@@ -1,106 +1,91 @@
-import { MoorhenTimeCapsule } from "../../../src/utils/MoorhenTimeCapsule"
+import { guid } from "../../../src/utils/MoorhenUtils"
 
 function CloudStorageInstance() {
-    this.exportBackupCallback = () => {}
-    this.importBackupCallback = () => {}
-    this.removeBackupCallback = () => {}
-    this.loadBackupList = () => {}
+    this.BACKUPS = []
+    
+    this.exportBackupCallback = (obj) => {
+        return new Promise((resolve, reject) => {
+            console.log('SET ITEM')
+            console.log(obj)
+            this.BACKUPS.push(obj)
+            resolve()    
+        })
+    }
+
+    this.importBackupCallback = (serNo) => {
+        return new Promise((resolve, reject) => {
+            console.log('GET ITEM')
+            console.log(serNo)
+            resolve(this.BACKUPS.find(i => i.serNo === serNo)    )
+        })
+    }
+
+    this.removeBackupCallback = (serNo) => {
+        return new Promise((resolve, reject) => {
+            console.log('REMOVE ITEM')
+            console.log(serNo)
+            resolve(this.BACKUPS.filter(i => i.serNo !== serNo))
+        })
+    }
+
+    this.removeAllBackupsCallback = () => {
+        return new Promise((resolve, reject) => {
+            console.log('CLEAR ALL')
+            this.BACKUPS = []
+            resolve()
+        })
+    }
+
+    this.loadBackupList = () => {
+        return new Promise((resolve, reject) => {
+            console.log('GET ALL KEYS')
+            resolve(this.BACKUPS)
+        })
+    }
 }
 
-CloudStorageInstance.prototype.setItem = function (key, value) {
-    this.exportBackupCallback({
-        name: key.name,
-        serNo: key.serNo,
+CloudStorageInstance.prototype.setItem = function (keyString, valueString) {
+    const key = JSON.parse(keyString)
+    const value = JSON.parse(valueString)
+    
+    if (key.type === 'version') {
+        return this.exportBackupCallback({
+            serNo: 0,
+            ...key,
+            data: value
+        })
+    }
+
+    return this.exportBackupCallback({
+        serNo: guid(),
+        ...key,
         data: value
     })
 }
 
-CloudStorageInstance.prototype.getItem = function (key) {
+CloudStorageInstance.prototype.getItem = function (keyString) {
+    const key = JSON.parse(keyString)
+    if (key.type === 'version') {
+        return this.importBackupCallback(0)
+    }
     return this.importBackupCallback(key.serNo)
 }
 
-CloudStorageInstance.prototype.keys = function () {
-    return this.loadBackupList()
+CloudStorageInstance.prototype.keys = async function () {
+    const allKeys = await this.loadBackupList()
+    return allKeys.map(key => JSON.stringify(key))
 }
 
 CloudStorageInstance.prototype.clear = async function () {
-    const backupList = this.loadBackupList()
-    return await Promise.all(backupList.map(key => this.removeItem(key.serNo)))
+    return this.removeAllBackupsCallback()
 }
 
-CloudStorageInstance.prototype.removeItem = function (key) {
+CloudStorageInstance.prototype.removeItem = function (keyString) {
+    const key = JSON.parse(keyString)
     return this.removeBackupCallback(key.serNo)
 }
 
-export function MoorhenCloudTimeCapsule(moleculesRef, mapsRef, activeMapRef, glRef, preferences) {
-    this.moleculesRef = moleculesRef
-    this.mapsRef = mapsRef
-    this.glRef = glRef
-    this.activeMapRef = activeMapRef
-    this.preferences = preferences
-    this.busy = false
-    this.modificationCount = 0
-    this.modificationCountBackupThreshold = 5
-    this.maxBackupCount = 10
-    this.version = 'v7'
-    this.disableBackups = false
-    this.storageInstance = null
+export const createLocalStorageInstance = () => {
+    console.log('CREATE CLOUD INSTANCEEEEE')
+    return new CloudStorageInstance()
 }
-  
-MoorhenCloudTimeCapsule.prototype = new MoorhenTimeCapsule()
-
-
-MoorhenCloudTimeCapsule.prototype.init = function () {
-    this.storageInstance = new CloudStorageInstance()
-    this.checkVersion()
-}
-
-MoorhenCloudTimeCapsule.prototype.checkVersion = async function () {
-    
-}
-
-MoorhenCloudTimeCapsule.prototype.updateDataFiles = async function () {
-    //pass
-}
-
-MoorhenCloudTimeCapsule.prototype.fetchSession = async function (includeAdditionalMapData=true) {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.addModification = async function() {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.cleanupIfFull = async function() {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.cleanupUnusedDataFiles = async function() {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.createBackup = async function(key, value) {
-
-
-}
-
-MoorhenCloudTimeCapsule.prototype.retrieveBackup = async function(key) {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.retrieveLastBackup = async function() {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.removeBackup = async function(key) {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.dropAllBackups = async function() {
-
-}
-
-MoorhenCloudTimeCapsule.prototype.getSortedKeys = async function() {
-
-}
-
