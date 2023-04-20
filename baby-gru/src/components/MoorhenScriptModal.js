61f6c310541a5084f0deef5087fd2364fabee9e6
Martin Noble <martinemnoble1@mac.com>
2023-04-20 11:20:42 +0100

    Making MoorhenScriptModal draggable

--- baby-gru/src/components/MoorhenScriptModal.js
+++ baby-gru/src/components/MoorhenScriptModal.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import { Modal, Button } from "react-bootstrap";
+import Draggable, {DraggableCore} from "react-draggable";
+import { Dialog, Button, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import Paper, { PaperProps } from '@mui/material/Paper';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
@@ -7,8 +9,18 @@ import 'prismjs/components/prism-javascript';
 import { PlayArrowOutlined } from "@mui/icons-material";
 import 'prismjs/themes/prism.css'; //Example style, you can use another
 
+const makeDraggablePaper = (paperProps) => {
+    //return <Paper {...paperProps} />
+    //Draggable not working for some reason !!!
+    return <Draggable>
+        <Paper {...paperProps} />
+    </Draggable>
+}
+
+
 export const MoorhenScriptModal = (props) => {
     const [code, setCode] = useState("//No code provided")
+    const [opacity, setOpacity] = useState(0.5)
 
     const { maps, changeMaps, molecules, changeMolecules, commandCentre, glRef, setToastContent } = props;
 
@@ -16,25 +28,44 @@ export const MoorhenScriptModal = (props) => {
         if (props.code) setCode(props.code)
     }, [])
 
-    return <Modal show={props.show} size="xl" onHide={() => props.setShow(false)}>
-        <Modal.Header closeButton>
-            <Modal.Title>Script preview</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div style={{ overflowY: "auto", maxHeight: "50vh" }}>
-                <Editor
-                    value={code}
-                    onValueChange={code => setCode(code)}
-                    highlight={code => highlight(code, languages.js)}
-                    padding={10}
-                    style={{
-                        fontFamily: '"Fira code", "Fira Mono", monospace',
-                        fontSize: 16
-                    }}
-                />
-            </div>
-        </Modal.Body>
-        <Modal.Footer>
+    return <Dialog
+        disableEnforceFocus // Allows other things to take focus
+        hideBackdrop  // Hides the shaded backdrop
+        open={props.show}
+        sx={{ opacity: opacity}}
+        fullWidth={true}
+        maxWidth="md"
+        style={{
+            top: '30%', // Position however you like
+            height: 'fit-content',  // Ensures that the dialog is 
+            width: 'fit-content',   // exactly the same size as its contents
+        }}
+        PaperComponent={makeDraggablePaper}
+        scroll="paper"
+        onClose={(event, reason) => {
+            if (reason !== 'backdropClick') {
+                props.setShow(false)
+            }
+        }}
+    >
+        <DialogTitle style={{ cursor: 'move', minWidth:"120rem" }} id="draggable-dialog-title">
+            Script
+        </DialogTitle>
+        <DialogContent style={{ overflowY: "auto", maxHeight: "50vh"}}
+            onMouseOver={() => { setOpacity(1.) }}
+            onMouseOut={() => { setOpacity(0.5) }}>
+            <Editor
+                value={code}
+                onValueChange={code => setCode(code)}
+                highlight={code => highlight(code, languages.js)}
+                padding={10}
+                style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    fontSize: 16
+                }}
+            />
+        </DialogContent>
+        <DialogActions>
             <Button onClick={async () => {
                 try {
                     eval(code)
@@ -44,6 +75,10 @@ export const MoorhenScriptModal = (props) => {
                 }
             }}><PlayArrowOutlined />
             </Button>
-        </Modal.Footer>
-    </Modal >
+            <Button onClick={() => {
+                props.setShow(false)
+            }}>Close
+            </Button>
+        </DialogActions>
+    </Dialog>
 }
