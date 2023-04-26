bf6996c4653fb001119ba3735b814fa105651db9
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2023-04-26 11:21:18 +0100

    Draw all HBonds checkbox (without labels).

--- baby-gru/src/components/MoorhenContextMenu.js
+++ baby-gru/src/components/MoorhenContextMenu.js
@@ -228,7 +228,7 @@ export const MoorhenContextMenu = (props) => {
     await newMolecule.updateAtoms()
     
     Object.keys(molecule.displayObjects)
-      .filter(style => { return ['CRs', 'CBs', 'ligands', 'gaussian', 'MolecularSurface', 'VdWSurface', 'DishyBases','VdwSpheres'].includes(style) })
+      .filter(style => { return ['CRs', 'CBs', 'ligands', 'gaussian', 'MolecularSurface', 'VdWSurface', 'DishyBases','VdwSpheres','allHBonds'].includes(style) })
       .forEach(async style => {
           if (molecule.displayObjects[style].length > 0 &&
               molecule.displayObjects[style][0].visible) {
@@ -307,7 +307,7 @@ export const MoorhenContextMenu = (props) => {
     newMolecule.drawSelection(props.glRef, fragmentCid)
     await newMolecule.updateAtoms()
     Object.keys(molecule.displayObjects)
-        .filter(style => { return ['CRs', 'CBs', 'ligands', 'gaussian', 'MolecularSurface', 'VdWSurface', 'DishyBases','VdwSpheres'].includes(style) })
+        .filter(style => { return ['CRs', 'CBs', 'ligands', 'gaussian', 'MolecularSurface', 'VdWSurface', 'DishyBases','VdwSpheres','allHBonds'].includes(style) })
         .forEach(async style => {
             if (molecule.displayObjects[style].length > 0 &&
                 molecule.displayObjects[style][0].visible) {
