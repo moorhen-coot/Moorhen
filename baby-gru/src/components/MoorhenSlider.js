afa55e42f4093dd41c299d9b8e41aa15694ea003
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-05-02 16:40:09 +0100

    fix map slider bug

--- baby-gru/src/components/MoorhenSlider.js
+++ baby-gru/src/components/MoorhenSlider.js
@@ -1,29 +1,39 @@
-import * as React from 'react';
+import { useCallback, useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Slider from '@mui/material/Slider';
-import { AddOutlined, RemoveOutlined } from '@mui/icons-material';
+
+const convertValueToScale = (logScale, minVal, maxVal, value) => {
+    if (logScale) {
+        return 100 * ((Math.log10(value) - Math.log10(minVal)) / ((Math.log10(maxVal) - Math.log10(minVal))));
+    } else {
+        return (100 * (value - minVal)) / (maxVal - minVal);
+    }
+}
 
 export default function MoorhenSlider(props) {
-    
-    const convertInitValueToScale = (value) => {
-        if (props.logScale) {
-            return 100 * ((Math.log10(value) - Math.log10(props.minVal)) / ((Math.log10(props.maxVal) - Math.log10(props.minVal))));
-        } else {
-            return (100 * (value - props.minVal)) / (props.maxVal - props.minVal);
-        }
-    }
+    const initValue = convertValueToScale(props.logScale, props.minVal, props.maxVal, props.initialValue)
+    const [value, setValue] = useState(initValue);
+    const [externalValue, setExternalValue] = useState(props.externalValue)
 
-    const [value, setValue] = React.useState(convertInitValueToScale(props.initialValue));
-    const [externalValue, setExternalValue] = React.useState(props.externalValue)
-
-    React.useEffect(() => {
+    useEffect(() => {
         if (props.externalValue !== parseFloat(externalValue)) {
             props.setExternalValue(parseFloat(externalValue))
         }
     }, [externalValue])
 
-    const handleChange = (event, newValue) => {
+    /** 
+    * This hook is necessary if we want the slider position to update when the value changes due to external
+    * components. However this can slow down things so better not to use it unless this is expected.
+    */
+    useEffect(() => {
+        if (props.allowExternalFeedback) {
+            setValue(convertValueToScale(props.logScale, props.minVal, props.maxVal, props.externalValue))
+        }
+
+    }, [props.externalValue])
+
+    const handleChange = useCallback((evt, newValue) => {
         setValue(props.allowFloats ? newValue : Math.round(newValue))
         if (props.logScale) {
             const log10MaxVal = Math.log10(props.maxVal)
@@ -40,7 +50,7 @@ export default function MoorhenSlider(props) {
         } else {
             setExternalValue(Math.round(props.minVal + (newValue/100.)*(props.maxVal-props.minVal)))
         }
-    };
+    }, [props.allowFloats, props.logScale, props.minVal, props.maxVal])
 
     return (
         <Box sx={{ width: '100%' }}>
@@ -60,5 +70,5 @@ export default function MoorhenSlider(props) {
 
 MoorhenSlider.defaultProps={
     minVal: 0, maxVal: 100, setExternalValue: ()=>{}, logScale: false, showMinMaxVal: true, incrementButton: null,
-    isDisabled: false, allowFloats: true, showSliderTitle: true, decimalPlaces: 3,  decrementButton: null
+    isDisabled: false, allowFloats: true, showSliderTitle: true, decimalPlaces: 3,  decrementButton: null, allowExternalFeedback: true,
 }
