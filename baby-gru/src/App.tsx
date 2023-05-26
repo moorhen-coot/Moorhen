c90895ca0119a0f3e94b20a1aa2e6e4b59116f35
Filomeno Sanchez <filo_san_rod@hotmail.com>
2023-05-26 08:26:16 +0000

    fix simulated error

--- baby-gru/src/App.tsx
+++ baby-gru/src/App.tsx
@@ -2,9 +2,6 @@ import './App.css';
 import { MoorhenApp } from './components/MoorhenApp';
 import { PreferencesContextProvider } from "./utils/MoorhenPreferences";
 
-let test: string
-test = 5
-
 function App() {
   return (
     <div className="App">
