b8ac319b0eafcb956704d70d9063e030a6dadb11
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-06-27 11:04:41 +0100

    add main moorhen.ts

---
+++ baby-gru/src/moorhen.ts
@@ -0,0 +1,13 @@
+import { ErrorBoundary } from "./ErrorBoundary";
+import { MoorhenApp } from './components/MoorhenApp';
+import { MoorhenContainer } from './components/MoorhenContainer';
+import { MoorhenContextProvider, MoorhenContext } from "./utils/MoorhenContext";
+import { MoorhenMolecule } from './utils/MoorhenMolecule'
+import { MoorhenMap } from './utils/MoorhenMap'
+import { MoorhenCommandCentre } from './utils/MoorhenCommandCentre'
+import { MoorhenTimeCapsule } from './utils/MoorhenTimeCapsule'
+
+export {
+    ErrorBoundary, MoorhenApp, MoorhenContainer, MoorhenContextProvider, MoorhenContext,
+    MoorhenMolecule, MoorhenMap, MoorhenCommandCentre, MoorhenTimeCapsule
+};
