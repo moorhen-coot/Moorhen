d059e3638323af26291d686aebfd784fa99e8221
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-06-27 11:07:18 +0100

    format

--- baby-gru/src/moorhen.ts
+++ baby-gru/src/moorhen.ts
@@ -2,10 +2,10 @@ import { ErrorBoundary } from "./ErrorBoundary";
 import { MoorhenApp } from './components/MoorhenApp';
 import { MoorhenContainer } from './components/MoorhenContainer';
 import { MoorhenContextProvider, MoorhenContext } from "./utils/MoorhenContext";
-import { MoorhenMolecule } from './utils/MoorhenMolecule'
-import { MoorhenMap } from './utils/MoorhenMap'
-import { MoorhenCommandCentre } from './utils/MoorhenCommandCentre'
-import { MoorhenTimeCapsule } from './utils/MoorhenTimeCapsule'
+import { MoorhenMolecule } from './utils/MoorhenMolecule';
+import { MoorhenMap } from './utils/MoorhenMap';
+import { MoorhenCommandCentre } from './utils/MoorhenCommandCentre';
+import { MoorhenTimeCapsule } from './utils/MoorhenTimeCapsule';
 import { MoorhenMoleculeSelect } from "./components/select/MoorhenMoleculeSelect";
 import { MoorhenMapSelect } from "./components/select/MoorhenMapSelect";
 
