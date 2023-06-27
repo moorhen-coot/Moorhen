37cdfc38ea9aed889c9b074b9c53f69f77583091
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-06-27 11:06:58 +0100

    add molecule and map select as requested

--- baby-gru/src/moorhen.ts
+++ baby-gru/src/moorhen.ts
@@ -6,8 +6,11 @@ import { MoorhenMolecule } from './utils/MoorhenMolecule'
 import { MoorhenMap } from './utils/MoorhenMap'
 import { MoorhenCommandCentre } from './utils/MoorhenCommandCentre'
 import { MoorhenTimeCapsule } from './utils/MoorhenTimeCapsule'
+import { MoorhenMoleculeSelect } from "./components/select/MoorhenMoleculeSelect";
+import { MoorhenMapSelect } from "./components/select/MoorhenMapSelect";
 
 export {
     ErrorBoundary, MoorhenApp, MoorhenContainer, MoorhenContextProvider, MoorhenContext,
-    MoorhenMolecule, MoorhenMap, MoorhenCommandCentre, MoorhenTimeCapsule
+    MoorhenMolecule, MoorhenMap, MoorhenCommandCentre, MoorhenTimeCapsule, MoorhenMoleculeSelect,
+    MoorhenMapSelect
 };
