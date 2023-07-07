2953e33993a741782c3637af68410c9553ccc755
FilomenoSanchez <FilomenoSanchez@users.noreply.github.com>
2023-07-07 16:50:27 +0000

    deploy: 13230eb421d05f9661b95114a3f1eb0703a09624

--- coot/smilestopdb.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MOORHEN_SMILSTOPDB_H
-#define _MOORHEN_SMILSTOPDB_H
-#include <string>
-
-std::pair<std::string, std::string> SmilesToPDB(const std::string &smile_cpp, const std::string &TLC, int nconf, int maxIters);
-#endif /* _MOORHEN_SMILSTOPDB_H */
