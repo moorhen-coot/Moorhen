f2d7cda5c90094e16bfa1a25f1d5afd1bd3e4642
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-07-18 17:54:37 +0100

    add types to moorhen npm package

---
+++ baby-gru/src/types/main.d.ts
@@ -0,0 +1,7 @@
+import { moorhen } from "./moorhen";
+import { webGL } from "./mgWebGL";
+import { libcootApi } from "./libcoot";
+import { gemmi } from "./gemmi";
+import { emscriptem } from "./emscriptem";
+
+export { moorhen, webGL, libcootApi, gemmi, emscriptem };
