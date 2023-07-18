8bd7ada5888fe1aaee060a614aa5ece5bdfa9910
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-07-18 11:58:14 +0100

    fix jsx syntax mistake after merge

--- baby-gru/src/components/navbar-menus/MoorhenCryoMenu.tsx
+++ baby-gru/src/components/navbar-menus/MoorhenCryoMenu.tsx
@@ -14,6 +14,5 @@ export const MoorhenCryoMenu = (props: MoorhenNavBarExtendedControlsInterface) =
             <MoorhenMapMaskingMenuItem  {...menuItemProps} />
             <MoorhenFlipMapHandMenuItem  {...menuItemProps} />
             <MoorhenMakeMaskedMapsSplitByChainMenuItem  {...menuItemProps} />
-        </NavDropdown>
     </>
 }
