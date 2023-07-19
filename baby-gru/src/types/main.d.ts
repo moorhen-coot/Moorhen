33e8dd4283f0f629777394e604bdf0f5d1c4be21
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-07-19 08:27:05 +0100

    declare a module in main.d.ts

--- baby-gru/src/types/main.d.ts
+++ baby-gru/src/types/main.d.ts
@@ -1,7 +1,13 @@
-import { moorhen } from "./moorhen";
+import { moorhen as MOORHEN } from "./moorhen"
 import { webGL } from "./mgWebGL";
 import { libcootApi } from "./libcoot";
 import { gemmi } from "./gemmi";
 import { emscriptem } from "./emscriptem";
 
-export { moorhen, webGL, libcootApi, gemmi, emscriptem };
+declare module 'moorhen' {
+    class MoorhenMolecule implements MOORHEN.Molecule { }
+    module.exports.MoorhenMolecule = MoorhenMolecule
+    
+    class MoorhenMap implements MOORHEN.Map { }
+    module.exports.MoorhenMap = MoorhenMap
+}
