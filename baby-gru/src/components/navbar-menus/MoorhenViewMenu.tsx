842d3b9b88f84ee2dd1de470de4d187e65645175
FilomenoSanchez <filo_san_rod@hotmail.com>
2023-08-18 11:52:22 +0100

    add scene presets

--- baby-gru/src/components/navbar-menus/MoorhenViewMenu.tsx
+++ baby-gru/src/components/navbar-menus/MoorhenViewMenu.tsx
@@ -5,6 +5,7 @@ import { MoorhenLightingMenuItem } from "../menu-item/MoorhenLightingMenuItem"
 import { MoorhenBlurMenuItem } from "../menu-item/MoorhenBlurMenuItem"
 import { MoorhenBackgroundColorMenuItem } from "../menu-item/MoorhenBackgroundColorMenuItem"
 import { MoorhenNavBarExtendedControlsInterface } from "./MoorhenNavBar";
+import { MoorhenScenePresetMenuItem } from "../menu-item/MoorhenScenePresetMenuItem"
 
 export const MoorhenViewMenu = (props: MoorhenNavBarExtendedControlsInterface) => {
     const [popoverIsShown, setPopoverIsShown] = useState(false)
@@ -12,51 +13,52 @@ export const MoorhenViewMenu = (props: MoorhenNavBarExtendedControlsInterface) =
 
     return <>
                 <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.drawFPS}
-                            onChange={() => { props.setDrawFPS(!props.drawFPS) }}
-                            label="Show frames per second counter"/>
-                    </InputGroup>
-                    <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.drawCrosshairs}
-                            onChange={() => { props.setDrawCrosshairs(!props.drawCrosshairs) }}
-                            label="Show crosshairs"/>
-                    </InputGroup>
-                    <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.drawAxes}
-                            onChange={() => { props.setDrawAxes(!props.drawAxes) }}
-                            label="Show axes"/>
-                    </InputGroup>
-                    <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.drawMissingLoops}
-                            onChange={() => { props.setDrawMissingLoops(!props.drawMissingLoops) }}
-                            label="Show missing loops"/>
-                    </InputGroup>
-                    <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.drawInteractions}
-                            onChange={() => { props.setDrawInteractions(!props.drawInteractions) }}
-                            label="Show Environment Distances"/>
-                    </InputGroup>
-                    <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
-                        <Form.Check 
-                            type="switch"
-                            checked={props.doPerspectiveProjection}
-                            onChange={() => { props.setDoPerspectiveProjection(!props.doPerspectiveProjection) }}
-                            label="Perspective projection"/>
-                    </InputGroup>
-                    <hr></hr>
-                    <MoorhenBackgroundColorMenuItem {...menuItemProps} />
-                    <MoorhenClipFogMenuItem {...menuItemProps} />
-                    <MoorhenLightingMenuItem {...menuItemProps} />
-                    {props.glRef.current.isWebGL2 () && <MoorhenBlurMenuItem {...menuItemProps} />}
-        </>
-    }
+                    <Form.Check 
+                        type="switch"
+                        checked={props.drawFPS}
+                        onChange={() => { props.setDrawFPS(!props.drawFPS) }}
+                        label="Show frames per second counter"/>
+                </InputGroup>
+                <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
+                    <Form.Check 
+                        type="switch"
+                        checked={props.drawCrosshairs}
+                        onChange={() => { props.setDrawCrosshairs(!props.drawCrosshairs) }}
+                        label="Show crosshairs"/>
+                </InputGroup>
+                <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
+                    <Form.Check 
+                        type="switch"
+                        checked={props.drawAxes}
+                        onChange={() => { props.setDrawAxes(!props.drawAxes) }}
+                        label="Show axes"/>
+                </InputGroup>
+                <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
+                    <Form.Check 
+                        type="switch"
+                        checked={props.drawMissingLoops}
+                        onChange={() => { props.setDrawMissingLoops(!props.drawMissingLoops) }}
+                        label="Show missing loops"/>
+                </InputGroup>
+                <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
+                    <Form.Check 
+                        type="switch"
+                        checked={props.drawInteractions}
+                        onChange={() => { props.setDrawInteractions(!props.drawInteractions) }}
+                        label="Show Environment Distances"/>
+                </InputGroup>
+                <InputGroup style={{ padding:'0.5rem', width: '25rem'}}>
+                    <Form.Check 
+                        type="switch"
+                        checked={props.doPerspectiveProjection}
+                        onChange={() => { props.setDoPerspectiveProjection(!props.doPerspectiveProjection) }}
+                        label="Perspective projection"/>
+                </InputGroup>
+                <hr></hr>
+                <MoorhenScenePresetMenuItem {...menuItemProps} />
+                <MoorhenBackgroundColorMenuItem {...menuItemProps} />
+                <MoorhenClipFogMenuItem {...menuItemProps} />
+                <MoorhenLightingMenuItem {...menuItemProps} />
+                {props.glRef.current.isWebGL2 () && <MoorhenBlurMenuItem {...menuItemProps} />}
+    </>
+}
