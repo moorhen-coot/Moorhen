0b209c8e5ea911cba881a0186e8b92b769240034
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2023-10-27 14:01:28 +0100

    add docstring to moorhen redux provider

--- baby-gru/src/components/misc/MoorhenReduxProvider.tsx
+++ baby-gru/src/components/misc/MoorhenReduxProvider.tsx
@@ -1,6 +1,20 @@
 import { Provider } from 'react-redux';
 import MoorhenStore from '../../store/MoorhenReduxStore';
 
+/**
+ * Redux provider for the Moorhen app. MoorhenContainer needs to be rendered inside this component.
+ * @param children - The Moorhen container
+ * @example
+ * import { MoorhenReduxProvider, MoorhenContainer } from "moorhen";
+ * 
+ * const exampleApp = () => {
+ * 
+ *    return <MoorhenReduxProvider>
+ *              <MoorhenContainer/>
+ *           <\MoorhenReduxProvider>
+ * }
+ * 
+ */
 export const MoorhenReduxProvider = ({ children }) => {
     return  <Provider store={MoorhenStore}> 
                 {children}
