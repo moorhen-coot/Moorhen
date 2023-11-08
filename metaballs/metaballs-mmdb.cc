4238525953039b37e3412d6b488b57432aa71197
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2023-11-08 16:18:42 +0000

    Consider vdW radii in meta-balls. This may or may not be a good idea.

--- metaballs/metaballs-mmdb.cc
+++ metaballs/metaballs-mmdb.cc
@@ -24,6 +24,7 @@
 #endif
 
 #include <iostream>
+#include <algorithm>
 #include <set>
 #include <stdlib.h>
 #include "Cartesian.h"
@@ -44,6 +45,160 @@
 
 #include "MC.h"
 #include "metaballs.h"
+
+std::map<std::string,float> multLookup = { 
+{"AC", 1.95},
+{"AG", 1.72},
+{"AL", 1.25},
+{"AM", 1.75},
+{"AR", 1.88},
+{"AS", 1.85},
+{"AT", 1.80},
+{"AU", 1.66},
+{"B", 0.85},
+{"BA", 2.53},
+{"BE", 1.12},
+{"BH", 2.0},//UNK
+{"BI", 1.8},
+{"BK", 2.0},//UNK
+{"BR", 1.85},
+{"C", 1.700},
+{"CA", 1.94},
+{"CD", 1.58},
+{"CE", 1.85},
+{"CF", 2.0},//UNK
+{"CL", 1.75},
+{"CM", 2.0},
+{"CO", 1.35},
+{"CR", 1.40},
+{"CS", 2.98},
+{"CU", 1.40},
+{"DB", 2.0},//UNK
+{"DY", 1.75},
+{"ER", 1.75},
+{"ES", 2.0},//UNK
+{"EU", 1.85},
+{"F", 1.47},
+{"FE", 1.40},
+{"FM", 2.0},//UNK
+{"FR", 2.0},//UNK
+{"GA", 1.87},
+{"GD", 1.80},
+{"GE", 2.10},
+{"H", 1.200},
+{"HE", 1.40},
+{"HF", 1.55},
+{"HG", 1.55},
+{"HO", 1.75},
+{"HS", 2.0},//UNK
+{"I", 1.98},
+{"IN", 1.93},
+{"IR", 1.35},
+{"K", 2.75},
+{"KR", 2.02},
+{"LA", 1.95},
+{"LI", 1.82},
+{"LR", 2.0},//UNK
+{"LU", 1.75},
+{"MD", 2.0},//UNK
+{"MG", 1.73},
+{"MN", 1.40},
+{"MO", 1.45},
+{"MT", 2.0},//UNK
+{"N", 1.550},
+{"NA", 2.27},
+{"NB", 1.45},
+{"ND", 1.85},
+{"ND", 1.85},
+{"NE", 1.54},
+{"NI", 1.63},
+{"NO", 2.0},//UNK
+{"NP", 1.75},
+{"O", 1.520},
+{"OSE", 1.30},
+{"P", 1.800},
+{"PA", 1.80},
+{"PB", 2.02},
+{"PD", 1.63},
+{"PM", 2.0},//UNK
+{"PO", 2.0},
+{"PR", 1.85},
+{"PT", 1.75},
+{"PU", 1.75},
+{"PU", 1.75},
+{"RA", 2.15},
+{"RB", 2.00},
+{"RB", 2.00},
+{"RB", 2.00},
+{"RE", 1.35},
+{"RF", 2.0},//UNK
+{"RH", 1.35},
+{"RN", 2.20},
+{"RU", 1.30},
+{"S", 1.800},
+{"SB", 1.8},
+{"SC", 1.60},
+{"SE", 1.90},
+{"SG", 2.0},//UNK
+{"SI", 2.10},
+{"SM", 1.85},
+{"SN", 2.17},
+{"SR", 2.19},
+{"TA", 1.45},
+{"TB", 1.75},
+{"TC", 1.35},
+{"TE", 2.06},
+{"TH", 1.80},
+{"TI", 1.40},
+{"TL", 1.96},
+{"TM", 1.75},
+{"U", 1.86},
+{"V", 1.35},
+{"W", 1.35},
+{"XE", 2.16},
+{"Y", 1.80},
+{"YB", 1.75},
+{"ZN", 1.39},
+{"ZR", 1.55},
+};
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    rtrim(s);
+    ltrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
+    return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+}
 
 void mergeLastTwoGroupsIfNecessary(std::vector<std::vector<std::array<float,4>>> &all_points){
 
@@ -94,16 +249,16 @@ coot::simple_mesh_t GenerateMoorhenMetaBalls(mmdb::Manager *molHnd, const std::s
     std::vector<std::array<float,4>> points;
 
     //TODO - double?
-    //TODO - Be cleverer about box size/orientation.
 
-    //TODO - Be more clever with no-contiguous sets of atoms, i.e. two separate ligands.
-    //      - This requires making multiple grids and then combining into one big mesh.
-    //      - But how do we cluster them in the first place?
     mmdb::Residue *currentResidue = 0;
     for(int i=0;i<nHetAtoms;i++){
         if(HetAtoms[i]->Het){
+            std::string element = std::string(HetAtoms[i]->element);
+            trim(element);
+            float atomMult = multLookup[element];
             if(strncmp(HetAtoms[i]->residue->name,"HOH",3)!=0){
-                std::array<float,4> point{float(HetAtoms[i]->x),float(HetAtoms[i]->y),float(HetAtoms[i]->z),r};
+                std::cout << element << " " << atomMult << std::endl;
+                std::array<float,4> point{float(HetAtoms[i]->x),float(HetAtoms[i]->y),float(HetAtoms[i]->z),r*atomMult/1.7f};
                 if(HetAtoms[i]->residue!=currentResidue){
                     //std::cout << "New residue!" << std::endl;
                     currentResidue = HetAtoms[i]->residue;
