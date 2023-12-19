5959ffb51d658d8cc902cf703c6eca5a40cd13cc
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2023-12-19 12:25:00 +0000

    add parse_multi_cids to type definitions

--- baby-gru/src/types/libcoot.d.ts
+++ baby-gru/src/types/libcoot.d.ts
@@ -12,6 +12,7 @@ declare global {
 
 export namespace libcootApi {
     type CCP4ModuleType = {
+        parse_multi_cids(gemmiStructure: gemmi.Structure, cid: string): emscriptem.vector<string>;
         parse_ligand_dict_info(fileContent: string): emscriptem.vector<{ comp_id: string; dict_contents: string; }>;
         get_ligand_info_for_structure(gemmiStructure: gemmi.Structure): emscriptem.vector<{
             resName: string;
