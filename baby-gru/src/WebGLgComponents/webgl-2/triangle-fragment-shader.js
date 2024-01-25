4f53b8df9ffcdf9eacdf4a10cab9cf54d7d59c8b
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-01-25 09:29:08 +0000

    Make shader colouring a bit brighter again.

--- baby-gru/src/WebGLgComponents/webgl-2/triangle-fragment-shader.js
+++ baby-gru/src/WebGLgComponents/webgl-2/triangle-fragment-shader.js
@@ -116,7 +116,7 @@ var triangle_fragment_shader_source = `#version 300 es\n
 
       vec4 theColor = vec4(vColor);
 
-      vec4 color = (theColor*Iamb + theColor* Idiff);
+      vec4 color = (1.5*theColor*Iamb + 1.2*theColor* Idiff);
 
       if(shad<0.5) {
           shad += .5;
