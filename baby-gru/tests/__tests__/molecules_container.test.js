15f67abdcd3c4beda0e71cc8869a922b1b093e18
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-01-30 16:20:18 +0000

    fix privateer test

--- baby-gru/tests/__tests__/molecules_container.test.js
+++ baby-gru/tests/__tests__/molecules_container.test.js
@@ -1168,7 +1168,7 @@ describe('Testing molecules_container_js', () => {
         const first = results.get(0)
         expect(first.wurcs).toBe("WURCS=2.0/2,3,2/[a2122h-1b_1-5_2*NCC/3=O][a1122h-1b_1-5]/1-1-2/a4-b1_b4-c1")
         expect(results.size()).toBe(38)
-        cleanUpVariables.push(first, results)
+        cleanUpVariables.push(results)
     });
 
 })
