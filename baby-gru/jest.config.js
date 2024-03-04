acafdf8a9b3d7d9d52147019944d950866d11fe8
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-03-04 17:22:24 +0000

    add config for react testing

--- baby-gru/jest.config.js
+++ baby-gru/jest.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-    "testMatch": [
-        "<rootDir>/tests/__tests__/*.test.js"
-    ],
     // "globalSetup":  "<rootDir>/tests/setup.js",
-    "globalTeardown": "<rootDir>/tests/teardown.js",
+    globalTeardown: "<rootDir>/tests/teardown.js",
+    projects: [
+        {
+            displayName: 'utils',
+            testMatch: ["<rootDir>/tests/__tests__/*.test.js"]
+        },
+        {
+            displayName: 'jsx',
+            testMatch: ['<rootDir>/tests/__tests__/*.test.jsx'],
+            testEnvironment: "jsdom",
+            preset: 'ts-jest',
+            transform: {
+                '^.+\\.(ts|tsx)?$': 'ts-jest',
+                '^.+\\.(js|jsx)$': 'babel-jest'
+            },
+        }
+    ],
   }
