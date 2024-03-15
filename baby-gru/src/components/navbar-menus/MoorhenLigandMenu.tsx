e434048c6c0ce29f37656b7faa520f581b4f07d8
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-03-15 16:13:06 +0000

    add ligand matching

--- baby-gru/src/components/navbar-menus/MoorhenLigandMenu.tsx
+++ baby-gru/src/components/navbar-menus/MoorhenLigandMenu.tsx
@@ -3,6 +3,7 @@ import { MoorhenCentreOnLigandMenuItem } from "../menu-item/MoorhenCentreOnLigan
 import { MoorhenSMILESToLigandMenuItem, MoorhenImportDictionaryMenuItem } from "../menu-item/MoorhenImportLigandDictionary";
 import { MoorhenGetMonomerMenuItem } from "../menu-item/MoorhenGetMonomerMenuItem";
 import { MoorhenMinimizeEnergyMenuItem } from "../menu-item/MoorhenMinimizeEnergyMenuItem";
+import { MoorhenMatchLigandsMenuItem } from "../menu-item/MoorhenMatchLigandsMenuItem"
 import { MoorhenNavBarExtendedControlsInterface } from "./MoorhenNavBar";
 import { MenuItem } from "@mui/material";
 import { useDispatch } from "react-redux";
@@ -21,6 +22,7 @@ export const MoorhenLigandMenu = (props: MoorhenNavBarExtendedControlsInterface)
             <MoorhenSMILESToLigandMenuItem {...menuItemProps} />
             <MoorhenCentreOnLigandMenuItem {...menuItemProps} />
             <MoorhenMinimizeEnergyMenuItem {...menuItemProps} />
+            <MoorhenMatchLigandsMenuItem {...menuItemProps} />
             <MenuItem onClick={() => {
                 dispatch(setShowFitLigandModal(true))
                 document.body.click()
