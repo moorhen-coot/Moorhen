8f0677cbc0626a8b48f57ba5b6429ae13c600cfc
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-03-21 14:28:07 +0000

    Build igraph, jsoncpp and new SliceNDice method. (The new method is not available in Moorhen yet).

--- initial_build.sh
+++ initial_build.sh
@@ -75,6 +75,22 @@ emmake make -j ${NUMPROCS}
 emmake make install
 cd ${BUILD_DIR}
 
+#jsoncpp
+mkdir -p ${BUILD_DIR}/jsoncpp_build
+cd ${BUILD_DIR}/jsoncpp_build
+emcmake cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}/checkout/jsoncpp -DJSONCPP_WITH_TESTS=OFF
+emmake make -j ${NUMPROCS}
+emmake make install
+cd ${BUILD_DIR}
+
+#igraph
+mkdir -p ${BUILD_DIR}/igraph_build
+cd ${BUILD_DIR}/igraph_build
+emcmake cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}/checkout/igraph
+emmake make -j ${NUMPROCS}
+emmake make install
+cd ${BUILD_DIR}
+
 #Moorhen
 mkdir -p ${BUILD_DIR}/moorhen_build
 cd ${BUILD_DIR}/moorhen_build
