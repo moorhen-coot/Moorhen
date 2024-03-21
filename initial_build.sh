02fd4d64782e9202eba78c1dec5cc81ffc528693
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-03-21 17:11:35 +0000

    Ug. Compiling igraph with -sMEMORY64=0 changes the integer type to 64 bit. This is not expected. So not passing this flag. This is kinda moot as igraph won't compile with actual 64-bit emscripten.

--- initial_build.sh
+++ initial_build.sh
@@ -105,7 +105,7 @@ else
 #igraph
 mkdir -p ${BUILD_DIR}/igraph_build
 cd ${BUILD_DIR}/igraph_build
-emcmake cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}/checkout/igraph -DCMAKE_C_FLAGS="-sMEMORY64=${MEMORY64} -pthread" -DCMAKE_CXX_FLAGS="-sMEMORY64=${MEMORY64} -pthread"
+emcmake cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ${SOURCE_DIR}/checkout/igraph -DCMAKE_C_FLAGS="-pthread" -DCMAKE_CXX_FLAGS="-pthread"
 emmake make -j ${NUMPROCS}
 emmake make install
 cd ${BUILD_DIR}
