a5149e4d6fa1a7596a2f22347b2b4643c21f2915
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-04-19 14:53:19 +0100

    update tomogram manager to buffer frames while user browses around

--- baby-gru/src/components/toasts/MoorhenTomogramManager.tsx
+++ baby-gru/src/components/toasts/MoorhenTomogramManager.tsx
@@ -1,4 +1,4 @@
-import { ArrowBackIos, ArrowForwardIos, KeyboardArrowLeftOutlined, KeyboardArrowRightOutlined, KeyboardDoubleArrowDownOutlined, KeyboardDoubleArrowLeftOutlined, KeyboardDoubleArrowRightOutlined } from "@mui/icons-material"
+import { KeyboardArrowLeftOutlined, KeyboardArrowRightOutlined, KeyboardDoubleArrowLeftOutlined, KeyboardDoubleArrowRightOutlined } from "@mui/icons-material"
 import { IconButton, LinearProgress, Slider } from "@mui/material"
 import { useCallback, useEffect, useRef, useState } from "react"
 import { Stack } from "react-bootstrap"
@@ -25,9 +25,11 @@ export const MoorhenTomogramManager = (props: {
     
     const mapMolNo = useSelector((state: moorhen.State) => state.activePopUps.tomogramPopUp.mapMolNo)
 
-    const framesRef = useRef<null | moorhen.DisplayObject[][]>([])
+    const frameDataRef = useRef(null)
+    const framesRef = useRef([])
     const iFrameRef = useRef<number>(0)
 
+    const [progressBufferedFrames, setProgressBufferedFrames] = useState<{value: number; label: string}[]>([])
     const [busyComputingFrames, setBusyComputingFrames] = useState<boolean>(false)
     const [nFrames, setNFrames] = useState<number>(0)
     const [progress, setProgress] = useState<number>(0)
@@ -46,19 +48,51 @@ export const MoorhenTomogramManager = (props: {
             }, false) as moorhen.WorkerResponse<number>
             
             framesRef.current = Array(nFrames.data.result.result)
-            setNFrames(nFrames.data.result.result)           
-            setBusyComputingFrames(false)
+            setNFrames(nFrames.data.result.result)
+
+            const initNFrames = nFrames.data.result.result < 50 ? nFrames.data.result.result : 50
+            const stepPercent = initNFrames / 50
+            const singleStepPercent = 1 / stepPercent
+            for (let i = 0; i < nFrames.data.result.result; i++) {
+                if (!framesRef.current[i]) {
+                    const frame = await props.commandCentre.current.cootCommand({
+                        returnType: "texture_as_floats_t",
+                        command: "get_map_section_texture",
+                        commandArgs: [mapMolNo, i, 0],
+                    }, false) as moorhen.WorkerResponse<any>
+                    framesRef.current[i] = frame.data.result.result
+                }
+                
+                if (i < initNFrames) {
+                    setProgress((prev) => prev + singleStepPercent)
+                } else if (i === initNFrames) {
+                    const percentage = initNFrames / (nFrames.data.result.result / 100)
+                    setProgressBufferedFrames([{
+                        value: percentage,
+                        label: `${percentage.toFixed(0)}%`
+                    }])        
+                    setBusyComputingFrames(false)
+                } else {
+                    const percentage = i / (nFrames.data.result.result / 100)
+                    setProgressBufferedFrames([{
+                        value: percentage,
+                        label: `${percentage.toFixed(0)}%`
+                    }])
+                }
+            }
+            setProgressBufferedFrames([])
         }
         loadNFrames()
     }, [])
 
     const showFrame = useCallback(async () => {
         let frameData: any
+        if (frameDataRef.current) {
+            props.glRef.current.texturedShapes.pop()
+        }
         if (framesRef.current[iFrameRef.current]) {
-            console.log('CACHED!!!!')
             frameData = framesRef.current[iFrameRef.current]
         } else {
-            console.log('NOT CACHED!!!!')
             const frame = await props.commandCentre.current.cootCommand({
                 returnType: "texture_as_floats_t",
                 command: "get_map_section_texture",
@@ -68,6 +102,7 @@ export const MoorhenTomogramManager = (props: {
             framesRef.current[iFrameRef.current] = frameData
         }
         
+        frameDataRef.current = frameData
         const obj = props.glRef.current.appendOtherData(frameData, true)
         const shape = obj[0].texturedShapes
         shape.setColourRamp(greyScaleColourRamp, true)
@@ -117,6 +152,8 @@ export const MoorhenTomogramManager = (props: {
             </div>
             <Slider
                 size="small"
+                track={false}
+                marks={progressBufferedFrames}
                 defaultValue={0}
                 value={currentFrameIndex}
                 onChange={(evt, newVal) => {
