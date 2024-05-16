6834c7e19c6eebc45c835383c4cc6427d6f58afe
FilomenoSanchez <filo_san_rod@hotmail.com>
2024-05-16 12:04:12 +0100

    add svg and other tooltips when searching ligand by name

--- baby-gru/src/components/menu-item/MoorhenGetMonomerMenuItem.tsx
+++ baby-gru/src/components/menu-item/MoorhenGetMonomerMenuItem.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useRef, useState } from "react";
-import { Form, FormSelect } from "react-bootstrap";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Form, FormSelect, OverlayTrigger, Stack, Tooltip } from "react-bootstrap";
 import { MoorhenBaseMenuItem } from "./MoorhenBaseMenuItem"
 import { MoorhenMoleculeSelect } from "../select/MoorhenMoleculeSelect";
 import { MoorhenMolecule } from "../../utils/MoorhenMolecule";
@@ -9,8 +9,47 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addMolecule } from "../../store/moleculesSlice";
 import { ToolkitStore } from "@reduxjs/toolkit/dist/configureStore";
 import { useSnackbar } from "notistack";
-import { Autocomplete, CircularProgress, createFilterOptions, TextField } from "@mui/material";
+import { Autocomplete, CircularProgress, createFilterOptions, MenuItem, selectClasses, Skeleton, TextField } from "@mui/material";
 import { libcootApi } from "../../types/libcoot";
+import parse from 'html-react-parser';
+
+const CompoundAutoCompleteOption = (props: {
+    compoundName: string;
+    monLibListRef: React.RefObject<libcootApi.compoundInfo[]>;
+    setValue: (newVal: string) => void;
+}) => {
+
+    const [ligandSVG, setLigandSVG] = useState<string>("")
+
+    const tooltip = useMemo(() => {
+        return <Tooltip style={{ zIndex: 99999 }}>
+            {ligandSVG ? parse(ligandSVG) : <Skeleton variant="rectangular" width={100} height={100} />}
+            <br></br>
+            <strong>{props.compoundName}</strong>
+        </Tooltip>
+    }, [ligandSVG])
+
+    useEffect(() => {
+        const fetchLigandSVG = async () => {
+            const threeLetterCode = props.monLibListRef.current.find(item => item.name === props.compoundName)?.three_letter_code
+            const response = await fetch(`https://www.ebi.ac.uk/pdbe/static/files/pdbechem_v2/${threeLetterCode}_100.svg`)
+            const text = await response.text()
+            setLigandSVG(text)
+        }
+        fetchLigandSVG()
+    }, [])
+
+    return <OverlayTrigger placement="right" overlay={tooltip}>
+        <MenuItem onClick={() => props.setValue(props.compoundName)}>
+            <Stack direction="horizontal" gap={1} style={{ width: "100%" }}>
+                {ligandSVG ? parse(ligandSVG) : <Skeleton variant="rectangular" width={100} height={100} />}
+                <span style={{ width: '100%', overflow: "hidden" }}>
+                    {props.compoundName}
+                </span>
+            </Stack>
+        </MenuItem>
+    </OverlayTrigger>
+}
 
 export const MoorhenGetMonomerMenuItem = (props: {
     glRef: React.RefObject<webGL.MGWebGL>
@@ -35,13 +74,15 @@ export const MoorhenGetMonomerMenuItem = (props: {
 
     const [searchMode, setSearchMode] = useState<string>("tlc")
     const [busy, setBusy] = useState<boolean>(false)
+    const [autoCompleteValue, setAutoCompleteValue] = useState<string>("")
+    const [autocompleteOpen, setAutocompleteOpen] = useState<boolean>(false)
 
     const { enqueueSnackbar } = useSnackbar()
 
     const filterOptions = useMemo(() => createFilterOptions({
         ignoreCase: true,
         matchFrom: "start",
-        limit: 10
+        limit: 5
     }), [])
 
     const handleSearchModeChange = async (evt) => {
@@ -53,7 +94,7 @@ export const MoorhenGetMonomerMenuItem = (props: {
                 const fileContents = await response.text()
                 const table = await props.commandCentre.current.cootCommand({
                     command: 'parse_mon_lib_list_cif',
-                    commandArgs: [ fileContents ],
+                    commandArgs: [fileContents],
                     returnType: 'status'
                 }, false) as moorhen.WorkerResponse<libcootApi.compoundInfo[]>
                 monLibListRef.current = table.data.result.result
@@ -75,54 +116,69 @@ export const MoorhenGetMonomerMenuItem = (props: {
             </FormSelect>
         </Form.Group>
         {searchMode === "tlc" ?
-        <Form.Group className='moorhen-form-group' controlId="MoorhenGetMonomerMenuItem">
-            <Form.Label>Monomer identifier</Form.Label>
-            <Form.Control ref={tlcRef} type="text" size="sm"  style={{textTransform: 'uppercase'}}/>
-        </Form.Group>
-        : 
-        <Form.Group className='moorhen-form-group' controlId="MoorhenGetMonomerMenuItem">
-            <Form.Label>Compound Name</Form.Label>
-            <Autocomplete
-                disablePortal
-                selectOnFocus
-                clearOnBlur
-                handleHomeEndKeys
-                freeSolo
-                includeInputInList
-                filterSelectedOptions
-                size='small'
-                loading={busy}
-                renderInput={(params) => <TextField {...params} label="Search" InputProps={{
-                    ...params.InputProps,
-                    endAdornment: (
-                      <>
-                        {busy ? <CircularProgress color="inherit" size={20} /> : null}
-                        {params.InputProps.endAdornment}
-                      </>
-                    ),
-                }}/>}
-                options={monLibListRef.current.map(item => item.name)}
-                filterOptions={filterOptions}
-                onChange={(evt, newSelection: string) => {
-                    autoCompleteRef.current = newSelection
-                }}
-                sx={{
-                    '& .MuiInputBase-root': {
-                        backgroundColor:  isDark ? '#222' : 'white',
-                        color: isDark ? 'white' : '#222',
-                    },
-                    '& .MuiOutlinedInput-notchedOutline': {
-                        borderColor: isDark ? 'white' : 'grey',
-                    },
-                    '& .MuiButtonBase-root': {
-                        color: isDark ? 'white' : 'grey',
-                    },
-                    '& .MuiFormLabel-root': {
-                        color: isDark ? 'white' : '#222',
-                    },
-                }}
-            />
-        </Form.Group>
+            <Form.Group className='moorhen-form-group' controlId="MoorhenGetMonomerMenuItem">
+                <Form.Label>Monomer identifier</Form.Label>
+                <Form.Control ref={tlcRef} type="text" size="sm" style={{ textTransform: 'uppercase' }} />
+            </Form.Group>
+            :
+            <Form.Group className='moorhen-form-group' controlId="MoorhenGetMonomerMenuItem">
+                <Form.Label>Compound Name</Form.Label>
+                <Autocomplete
+                    disablePortal
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    freeSolo
+                    includeInputInList
+                    filterSelectedOptions
+                    size='small'
+                    loading={busy}
+                    value={autoCompleteValue}
+                    open={autocompleteOpen}
+                    onClose={() => setAutocompleteOpen(false)}
+                    onOpen={() => setAutocompleteOpen(true)}
+                    renderInput={(params) => <TextField {...params} label="Search" InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <>
+                                {busy ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </>
+                        ),
+                    }} />}
+                    renderOption={(props, option: string) => {
+                        return <CompoundAutoCompleteOption key={option} compoundName={option} monLibListRef={monLibListRef} setValue={(newVal) => {
+                            autoCompleteRef.current = newVal
+                            setAutoCompleteValue(newVal)
+                            setAutocompleteOpen(false)
+                        }}/>
+                    }}
+                    options={monLibListRef.current.map(item => item.name)}
+                    filterOptions={filterOptions}
+                    onChange={(evt, newSelection: string) => {
+                        autoCompleteRef.current = newSelection
+                        if (newSelection === null) {
+                            setAutoCompleteValue(newSelection)
+                        }
+                        setAutocompleteOpen(false)
+                    }}
+                    sx={{
+                        '& .MuiInputBase-root': {
+                            backgroundColor: isDark ? '#222' : 'white',
+                            color: isDark ? 'white' : '#222',
+                        },
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            borderColor: isDark ? 'white' : 'grey',
+                        },
+                        '& .MuiButtonBase-root': {
+                            color: isDark ? 'white' : 'grey',
+                        },
+                        '& .MuiFormLabel-root': {
+                            color: isDark ? 'white' : '#222',
+                        },
+                    }}
+                />
+            </Form.Group>
         }
     </>
 
@@ -153,12 +209,12 @@ export const MoorhenGetMonomerMenuItem = (props: {
         }
 
         let result = await getMonomer()
-        
+
         if (result.data.result.result === -1) {
             await newMolecule.loadMissingMonomer(newTlc, fromMolNo)
             result = await getMonomer()
-        } 
-        
+        }
+
         if (result.data.result.status === "Completed" && result.data.result.result !== -1) {
             newMolecule.molNo = result.data.result.result
             newMolecule.name = newTlc
@@ -173,9 +229,9 @@ export const MoorhenGetMonomerMenuItem = (props: {
                 }
             }
             await newMolecule.fetchIfDirtyAndDraw('CBs')
-            dispatch( addMolecule(newMolecule) )
+            dispatch(addMolecule(newMolecule))
         } else {
-            enqueueSnackbar("Error getting monomer. Missing dictionary?", {variant: 'warning'})
+            enqueueSnackbar("Error getting monomer. Missing dictionary?", { variant: 'warning' })
             console.log('Error getting monomer. Missing dictionary?')
         }
     }
