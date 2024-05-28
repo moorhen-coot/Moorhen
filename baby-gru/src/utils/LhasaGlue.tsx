e7d61bd818e84b41a3c213f3f057d05815750db9
Jakub Smulski <hgonomeg@gmail.com>
2024-05-28 15:41:47 +0200

    Experiment with initialization

--- baby-gru/src/utils/LhasaGlue.tsx
+++ baby-gru/src/utils/LhasaGlue.tsx
@@ -1,15 +1,22 @@
 // Import auto-generated type-definitions
 import '../LhasaReact/src/lhasa.d.ts';
 import { LhasaComponent } from '../LhasaReact/src/Lhasa';
+import { useSelector } from 'react-redux';
+import { moorhen } from '../types/moorhen.js';
 //import Module from '/baby-gru/moorhen.js?url';
 
 function LhasaWrapper() {
-    // This is 'undefined'    
-    let lh_module = window.CCP4Module;
+    const cootInitialized = useSelector((state: moorhen.State) => state.generalStates.cootInitialized)
+    let lh_module = null;
+    if(cootInitialized) {
+        lh_module = window.CCP4Module;
+    } 
     
     return (
     <>
-            {/* <LhasaComponent Lhasa={lh_module} /> */}
+            {cootInitialized &&
+                <LhasaComponent Lhasa={lh_module} />
+            }
     </>
     );
 }
