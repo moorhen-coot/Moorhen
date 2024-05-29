f472546d0870470bdcf04af19d8c1dd9db855451
Jakub Smulski <hgonomeg@gmail.com>
2024-05-29 14:28:20 +0200

    Remove the test code for now

--- baby-gru/src/components/MoorhenContainer.tsx
+++ baby-gru/src/components/MoorhenContainer.tsx
@@ -35,9 +35,6 @@ import { MoorhenRotamerChangeSnackBar } from './snack-bar/MoorhenRotamerChangeSn
 import { MoorhenScreenshotSnackBar } from './snack-bar/MoorhenScreenshotSnackBar';
 import { MoorhenSideBar } from './snack-bar/MoorhenSideBar';
 
-// FIXME: This is here for testing only
-import { LhasaWrapper } from '../utils/LhasaGlue';
-
 declare module "notistack" {
     interface VariantOverrides {
         goToResidue: {
@@ -528,11 +525,6 @@ export const MoorhenContainer = (props: moorhen.ContainerProps) => {
                         viewOnly={viewOnly}
                         videoRecorderRef={videoRecorderRef}
                     />
-                </div>
-            </Col>
-            <Col>
-                <div>
-                    <LhasaWrapper />
                 </div>
             </Col>
         </Row>
