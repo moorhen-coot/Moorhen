4b53aa5c9aaccb012bff6b581a34b28e8a8c0f7b
Jakub Smulski <hgonomeg@gmail.com>
2024-05-30 18:21:06 +0200

    Try adding Lhasa Modal window based on Filo's code

---
+++ baby-gru/src/components/modal/MoorhenLhasaModal.tsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { useSelector } from "react-redux";
+import { convertRemToPx, convertViewtoPx } from "../../utils/MoorhenUtils";
+import { MoorhenDraggableModalBase } from "./MoorhenDraggableModalBase";
+import { moorhen } from "../../types/moorhen";
+import { LhasaWrapper } from "../../utils/LhasaGlue";
+
+export const MoorhenLhasaModal = (props) => {
+    const resizeNodeRef = useRef<HTMLDivElement>();
+      
+    const width = useSelector((state: moorhen.State) => state.sceneSettings.width)
+    const height = useSelector((state: moorhen.State) => state.sceneSettings.height)
+
+    return <MoorhenDraggableModalBase
+                modalId="lhasa-modal"
+                left={width / 6}
+                top={height / 3}
+                show={props.show}
+                setShow={props.setShow}
+                defaultHeight={convertViewtoPx(70, height)}
+                defaultWidth={convertViewtoPx(37, width)}
+                minHeight={convertViewtoPx(30, height)}
+                minWidth={convertRemToPx(37)}
+                maxHeight={convertViewtoPx(90, height)}
+                maxWidth={convertViewtoPx(80, width)}
+                enforceMaxBodyDimensions={false}
+                overflowY='hidden'
+                overflowX='auto'
+                headerTitle='Lhasa'
+                // footer={null}
+                resizeNodeRef={resizeNodeRef}
+                body={
+                    
+                    <LhasaWrapper/>
+                }
+            />
+}
