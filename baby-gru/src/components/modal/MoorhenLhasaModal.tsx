67439730f60de58c5fe62b4f1e7674f3b069c603
Jakub Smulski <hgonomeg@gmail.com>
2024-06-05 18:42:23 +0200

    Try to integrate libcootApi with Lhasa

--- baby-gru/src/components/modal/MoorhenLhasaModal.tsx
+++ baby-gru/src/components/modal/MoorhenLhasaModal.tsx
@@ -1,9 +1,18 @@
-import { useRef } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { convertRemToPx, convertViewtoPx } from "../../utils/MoorhenUtils";
 import { MoorhenDraggableModalBase } from "./MoorhenDraggableModalBase";
 import { moorhen } from "../../types/moorhen";
 import { LhasaWrapper } from "../../utils/LhasaGlue";
+
+const item_reducer = (old_map: Map<string,Uint8Array>, action: any) => {
+    if(action.type === 'add') {
+        old_map[action.id] = action.value;
+    }
+    return new  Map<string,Uint8Array>(old_map);
+};
+
+const initial_value = new Map<string,Uint8Array>();
 
 export const MoorhenLhasaModal = (props) => {
     const resizeNodeRef = useRef<HTMLDivElement>();
@@ -11,7 +20,27 @@ export const MoorhenLhasaModal = (props) => {
     const width = useSelector((state: moorhen.State) => state.sceneSettings.width)
     const height = useSelector((state: moorhen.State) => state.sceneSettings.height)
 
-    let your_rdkit_mol_pickle_map: Map<string,Uint8Array> | undefined = undefined;
+    const [my_map,setMyMap] = useReducer(item_reducer, initial_value);
+    useEffect(() => {
+        props.commandCentre.current.cootCommand({
+            returnType: 'Uint8Array',
+            command: "get_rdkit_mol_pickle",
+            commandArgs: [
+                "LZA",
+                -999999
+            ]
+        },false).then((response) => {
+            const pickle = response.data.result.result;
+            setMyMap({
+                type: "add",
+                id: "LZA"+(-999999),
+                value: pickle
+            });
+        });
+
+    }, []);
+
+    // let your_rdkit_mol_pickle_map: Map<string,Uint8Array> | undefined = undefined;
 
     return <MoorhenDraggableModalBase
                 modalId="lhasa-modal"
@@ -33,7 +62,7 @@ export const MoorhenLhasaModal = (props) => {
                 resizeNodeRef={resizeNodeRef}
                 body={
                     
-                    <LhasaWrapper rdkit_molecule_pickle_map={your_rdkit_mol_pickle_map} />
+                    <LhasaWrapper rdkit_molecule_pickle_map={my_map} />
                 }
             />
 }
