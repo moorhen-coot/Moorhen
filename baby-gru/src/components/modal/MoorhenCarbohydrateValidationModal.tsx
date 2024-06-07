0d5483f83573de240cfbd22ca3cfee2d3035af42
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-06-07 12:02:43 +0100

    add carb validation side panel

--- baby-gru/src/components/modal/MoorhenCarbohydrateValidationModal.tsx
+++ baby-gru/src/components/modal/MoorhenCarbohydrateValidationModal.tsx
@@ -1,17 +1,25 @@
 import { MoorhenDraggableModalBase } from "./MoorhenDraggableModalBase"
 import { moorhen } from "../../types/moorhen";
 import { useRef } from "react";
-import { Row } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import { convertRemToPx, convertViewtoPx} from '../../utils/utils';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MoorhenCarbohydrateValidation } from "../validation-tools/MoorhenCarbohydrateValidation";
 import { modalKeys } from "../../utils/enums";
+import { Tooltip } from "@mui/material";
+import { hideModal } from "../../store/modalsSlice";
+import { useSnackbar } from "notistack";
+import { LastPageOutlined } from "@mui/icons-material";
 
 export const MoorhenCarbohydrateValidationModal = (props: moorhen.CollectedProps) => {        
     const resizeNodeRef = useRef<HTMLDivElement>();
       
     const width = useSelector((state: moorhen.State) => state.sceneSettings.width)
     const height = useSelector((state: moorhen.State) => state.sceneSettings.height)
+
+    const dispatch = useDispatch()
+
+    const { enqueueSnackbar } = useSnackbar()
 
     const collectedProps = {
         sideBarWidth: convertViewtoPx(35, width), dropdownId: 1, busy: false, 
@@ -41,6 +49,27 @@ export const MoorhenCarbohydrateValidationModal = (props: moorhen.CollectedProps
                         </Row>
                     </div>
                 }
+                additionalHeaderButtons={[
+                    <Tooltip title={"Move to side panel"}  key={1}>
+                        <Button variant='white' style={{margin: '0.1rem', padding: '0.1rem'}} onClick={() => {
+                            dispatch( hideModal(modalKeys.CARB_VALIDATION) )
+                            enqueueSnackbar(modalKeys.CARB_VALIDATION, {
+                                variant: "sideBar",
+                                persist: true,
+                                anchorOrigin: {horizontal: "right", vertical: "bottom"},
+                                modalId: modalKeys.CARB_VALIDATION,
+                                title: "Privateer",
+                                children: <div style={{ overflowY: 'scroll', overflowX: "hidden", maxHeight: '30vh' }}>
+                                            <Row className={"big-validation-tool-container-row"}>
+                                                <MoorhenCarbohydrateValidation {...collectedProps}/>
+                                            </Row>
+                                        </div>
+                            })
+                        }}>
+                            <LastPageOutlined/>
+                        </Button>
+                    </Tooltip>
+                ]}
             />
 }
 
