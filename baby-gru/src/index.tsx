b757d60d88a6fb4bd6cd18dbf78a6627698478d5
Jakub Smulski <hgonomeg@gmail.com>
2024-06-17 20:08:12 +0200

    Temporarily disable StrictMode

--- baby-gru/src/index.tsx
+++ baby-gru/src/index.tsx
@@ -6,11 +6,11 @@ import { ErrorBoundary } from './ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <ErrorBoundary >
       <App />
     </ErrorBoundary>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
