97c7071c1b03ced7e50cdf955ca37091ba5d3881
FilomenoSanchez <filo_san_rod@hotmail.com>
2024-06-20 12:04:21 +0100

    more tidying up in moorhen lhasa modal

--- baby-gru/src/components/modal/MoorhenLhasaModal.tsx
+++ baby-gru/src/components/modal/MoorhenLhasaModal.tsx
@@ -4,14 +4,60 @@ import { convertRemToPx, convertViewtoPx } from "../../utils/utils";
 import { MoorhenDraggableModalBase } from "./MoorhenDraggableModalBase";
 import { moorhen } from "../../types/moorhen";
 import { LhasaComponent } from '../../LhasaReact/src/Lhasa';
+import { modalKeys } from "../../utils/enums";
+
+type rdkitMoleculesChange = {
+    type: "add";
+    id: string;
+    value: string;
+}
+
+const rdkitMoleculeReducer = (oldMap: Map<string, string>, action: rdkitMoleculesChange) => {
+    const newMap = new Map<string, string>()
+    
+    if (action.type === 'add') {
+        newMap.set(action.id, action.value)
+        oldMap.forEach((value, key) => {
+            newMap.set(key, value);
+        })
+    } else {
+        console.error(`Unknown rdkitMoleculeReducer action type '${action.type}'... Doing nothing.`)
+    }
+
+    return newMap
+}
+
+const initialRdkitMolecules = new Map<string, string>();
 
 const LhasaWrapper = (props: {
-    rdkiMoleculePickleMap?: Map<string, string>;
-    smilesCallback?: (id: string, smiles: string) => void;
+    commandCentre: React.RefObject<moorhen.CommandCentre>;
 }) => {
 
     const [isCootAttached, setCootAttached] = useState(window.cootModule !== undefined)
     
+    const [rdkiMoleculePickleMap, setRdkiMoleculePickleMap] = useReducer(rdkitMoleculeReducer, initialRdkitMolecules)
+
+
+    useEffect(() => {
+
+        props.commandCentre.current.cootCommand({
+            returnType: 'string',
+            command: "get_rdkit_mol_pickle_base64",
+            commandArgs: [
+                "LZA",
+                -999999
+            ]
+        },false).then((response) => {
+            const pickle = response.data.result.result;
+            setRdkiMoleculePickleMap({
+                type: "add",
+                id: "LZA"+(-999999),
+                value: pickle
+            });
+        });
+
+    }, []);
+
     const handleCootAttached = useCallback(() => {
         if (window.cootModule !== undefined) {
             setCootAttached(true)
@@ -27,63 +73,30 @@ const LhasaWrapper = (props: {
         };
     }, [handleCootAttached])
 
+    const smilesCallback = useCallback((id: string, smiles: string) => {
+        console.log(`>> Received SMILES back from Lhasa: ID=${id} SMILES=${smiles}`)
+    }, [])
+
     return  isCootAttached ?
                 <LhasaComponent 
                     Lhasa={window.cootModule}
                     show_footer={false}
                     show_top_panel={false}
-                    rdkit_molecule_pickle_map={props.rdkiMoleculePickleMap}
+                    rdkit_molecule_pickle_map={rdkiMoleculePickleMap}
                     icons_path_prefix='/pixmaps/lhasa_icons'
                     name_of_host_program='Moorhen'
-                    smiles_callback={props.smilesCallback}
+                    smiles_callback={smilesCallback}
                 /> : null
 }
 
-const item_reducer = (old_map: Map<string, string>, action: any) => {
-    if(action.type === 'add') {
-        old_map.set(action.id, action.value);
-    }
-    let ret = new Map<string, string>();
-    old_map.forEach((value,key) => {
-        ret.set(key, value);
-    })
-    return ret;
-};
-
-const initial_value = new Map<string, string>();
-
-export const MoorhenLhasaModal = (props) => {
+export const MoorhenLhasaModal = (props: moorhen.CollectedProps) => {
     const resizeNodeRef = useRef<HTMLDivElement>();
       
     const width = useSelector((state: moorhen.State) => state.sceneSettings.width)
     const height = useSelector((state: moorhen.State) => state.sceneSettings.height)
 
-    const [my_map,setMyMap] = useReducer(item_reducer, initial_value);
-    useEffect(() => {
-
-        props.commandCentre.current.cootCommand({
-            returnType: 'string',
-            command: "get_rdkit_mol_pickle_base64",
-            commandArgs: [
-                "LZA",
-                -999999
-            ]
-        },false).then((response) => {
-            const pickle = response.data.result.result;
-            // console.log("Got pickle: ", pickle);
-            setMyMap({
-                type: "add",
-                id: "LZA"+(-999999),
-                value: pickle
-            });
-        });
-
-    }, []);
-
-    // let your_rdkit_mol_pickle_map: Map<string,Uint8Array> | undefined = undefined;
-
     return <MoorhenDraggableModalBase
-                modalId="lhasa-modal"
+                modalId={modalKeys.LHASA}
                 left={width / 6}
                 top={height / 3}
                 defaultHeight={convertViewtoPx(70, height)}
@@ -92,18 +105,11 @@ export const MoorhenLhasaModal = (props) => {
                 minWidth={convertRemToPx(37)}
                 maxHeight={convertViewtoPx(90, height)}
                 maxWidth={convertViewtoPx(80, width)}
-                enforceMaxBodyDimensions={false}
+                enforceMaxBodyDimensions={true}
                 overflowY='auto'
                 overflowX='auto'
                 headerTitle='Lhasa'
-                // footer={null}
                 resizeNodeRef={resizeNodeRef}
-                body={
-                    
-                    <LhasaWrapper 
-                        rdkit_molecule_pickle_map={my_map}
-                        smiles_callback={(id, smiles) => console.info("ID=", id, " SMILES=", smiles)}
-                    />
-                }
+                body={ <LhasaWrapper commandCentre={props.commandCentre}/> }
             />
 }
