843233afc63021fafa21b29e358882078c8bf627
FilomenoSanchez <filo_san_rod@hotmail.com>
2024-07-04 12:14:51 +0100

    fix public folder issue when building with webpack

--- baby-gru/webpack.config.js
+++ baby-gru/webpack.config.js
@@ -10,30 +10,13 @@ const paths = {
   types: path.resolve(__dirname, 'src', 'types'),
   dist: path.resolve(__dirname, 'dist'),
   public: path.resolve(__dirname, 'public'),
-  publicBabyGru: path.resolve(__dirname, 'public', 'baby-gru'),
-  pixmapsPath: path.resolve(__dirname, 'pixmaps'),
-  monomerLibraryPath: path.resolve(__dirname, 'public', 'baby-gru', 'monomers'),
+  monomerLibraryPath: path.resolve(__dirname, 'public', 'monomers'),
   minimalMonomerLib: [
     'ALA', 'ASP', 'ASN', 'CYS', 'GLN', 'GLY', 'GLU', 'PHE', 'HIS', 'ILE', 'LYS', 
     'LEU', 'MET', 'MSE', 'PRO', 'ARG', 'SER', 'THR', 'VAL', 'TRP', 'TYR', 'PO4',
     'SO4', 'GOL', 'CIT', 'EDO', 'A', 'C', 'G', 'U', 'DA', 'DC', 'DG', 'DT', 'HOH',
     'NA'
   ],
-  requiredCootPixmaps: [
-    'MoorhenLogo.png', 'rama2_all.png', 'rama2_gly.png',
-    'rama2_pre_pro.png', 'rama2_pro.png', 'rama2_ileval.png', 'rama2_non_gly_pro.png',
-    'rama2_non_gly_pro_pre_pro_ileval.png', 'rama-plot-gly-normal.png', 'mutate.svg', 
-    'rama-plot-pro-normal.png', 'rama-plot-pro-outlier.png', 'rama-plot-other-normal.png',
-    'rama-plot-other-outlier.png', 'auto-fit-rotamer.svg', 'flip-peptide.svg', 
-    'side-chain-180.svg', 'refine-1.svg', 'delete.svg', 'rama-plot-gly-outlier.png',
-    'add-peptide-1.svg', 'rigid-body.svg', 'spin-view.svg', 'edit-chi.svg',
-    'jed-flip-reverse.svg', 'rtz.svg', 'add-alt-conf.svg', 'cis-trans.svg',
-    'auto-fit-rotamer.svg', 'flip-peptide.svg', 'cis-trans.svg', 'side-chain-180.svg',
-    'refine-1.svg', 'add-alt-conf.svg', 'delete.svg', 'mutate.svg', 'temperature.svg',
-    'add-peptide-1.svg', 'spin-view.svg', 'edit-chi.svg', 'jed-flip-reverse.svg',
-    'rigid-body.svg', 'atom-at-pointer.svg', 'rtz.svg', 'rotamers.svg', 'drag.svg',
-    'keyboard-blank.svg', 'map-grey.svg', 'moorhen-ligand.svg', 'secondary-structure-grey.svg'
-  ]
 }
 
 module.exports = (env, argv) => {
@@ -50,11 +33,11 @@ module.exports = (env, argv) => {
       new CopyWebpackPlugin({
         patterns: [
           {
-            from: paths.publicBabyGru,
-            to: paths.dist + '/baby-gru/',
+            from: paths.public,
+            to: paths.dist + '/public/',
             toType: 'dir',
             globOptions: {
-              ignore: ['**/monomers/**', '**/pixmaps/**']
+              ignore: ['**/monomers/**']
             }
           },
           {
@@ -65,14 +48,7 @@ module.exports = (env, argv) => {
           ...paths.minimalMonomerLib.map(monomer => {
             return {
               from: path.resolve(paths.monomerLibraryPath, monomer.charAt(0).toLowerCase(), `${monomer}.cif`),
-              to: path.resolve(paths.dist, 'baby-gru', 'monomers', monomer.charAt(0).toLowerCase()),
-              toType: 'dir',  
-            }
-          }),
-          ...paths.requiredCootPixmaps.map(pixmap => {
-            return {
-              from: path.resolve(paths.pixmapsPath, pixmap),
-              to: path.resolve(paths.dist, 'pixmaps'),
+              to: path.resolve(paths.dist, 'public', 'monomers', monomer.charAt(0).toLowerCase()),
               toType: 'dir',  
             }
           }),
@@ -114,7 +90,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.js$/,
-          exclude: [/node_modules/, path.resolve(paths.src, 'index.js'), paths.publicBabyGru],
+          exclude: [/node_modules/, path.resolve(paths.src, 'index.js'), paths.public],
           loader: 'babel-loader',
         },
         {
