300cbbf8460ca981c97d43e019dc0eca8e4a8be5
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-11-07 15:42:55 +0000

    First stab at using RDKit for small molecule connectivity determination - this currently ignores disorder groups.

--- wasm_src/moorhen-wrappers.cc
+++ wasm_src/moorhen-wrappers.cc
@@ -56,10 +56,6 @@
 #include "clipper/core/ramachandran.h"
 #include "clipper/clipper-ccp4.h"
 
-#include "gemmi/small.hpp"
-#include "gemmi/smcif.hpp"
-#include "gemmi/to_mmcif.hpp"
-
 #include "cartesian.h"
 #include "geomutil.h"
 #include "matrix.h"
@@ -1012,179 +1008,6 @@ emscripten::val testFloat32Array(const emscripten::val &floatArrayObject){
     return result;
 }
 
-std::pair<std::string,std::string> SmallMoleculeCifToMMCif(const std::string &small_molecule_cif){
-
-    std::string mmcif_string;
-    gemmi::cif::Document doc = gemmi::cif::read_string(small_molecule_cif);
-    gemmi::cif::Block block = doc.sole_block();
-
-    gemmi::SmallStructure small = gemmi::make_small_structure_from_block(block);
-
-
-    gemmi::Residue r;
-    r.name = "UNK";
-    r.label_seq = 1;
-
-    std::map<std::string,int> atoms;
-    std::map<std::string,int> name_disorder_group_map;
-
-    int nAtAll = 0;
-    int nAtNoH = 0;
-
-    for (const auto& site : small.sites) {
-        gemmi::Atom atom;
-        auto orth = small.cell.orthogonalize(site.fract);
-        atom.pos = orth;
-        atom.element = site.element;
-
-        std::string elname = std::string(site.element.name());
-        std::transform(elname.begin(), elname.end(), elname.begin(), ::toupper);
-
-        if(atoms.count(elname)==0){
-            atoms[elname]  = 1;
-        } else {
-            atoms[elname] += 1;
-        }
-        atom.name = elname+std::to_string(atoms[elname]);
-
-        nAtAll++;
-        atom.serial = nAtAll;
-        r.atoms.push_back(atom);
-        if(elname!="H"){
-            nAtNoH++;
-        }
-    }
-
-    gemmi::Structure st2;
-
-    gemmi::Model m("1");
-    gemmi::Chain c("A");
-
-    c.residues.push_back(r);
-    m.chains.push_back(c);
-    st2.models.push_back(m);
-
-    st2.cell = small.cell;
-    st2.name = small.name;
-
-    st2.spacegroup_hm = small.spacegroup_hm;
-
-    gemmi::cif::Document doc2 = gemmi::make_mmcif_document(st2);
-    gemmi::cif::Table data = doc2.sole_block().find_mmcif_category("_atom_site.");
-
-    for (auto d: data) {
-        int auth_seq_id_pos = d.tab.find_column_position("auth_seq_id");
-        d.value_at_unsafe(auth_seq_id_pos) = "1";
-        int label_entity_id_pos = d.tab.find_column_position("label_entity_id");
-        d.value_at_unsafe(label_entity_id_pos) = "1";
-        int label_asym_id_pos = d.tab.find_column_position("label_asym_id");
-        d.value_at_unsafe(label_asym_id_pos) = "A";
-    }
-
-    std::ostringstream s;
-    gemmi::cif::write_cif_to_stream(s, doc2);
-
-    mmcif_string = s.str();
-
-    std::ostringstream dict_output;
-    dict_output << "global_\n";
-    dict_output << "_lib_name         ?\n";
-    dict_output << "_lib_version      ?\n";
-    dict_output << "_lib_update       ?\n";
-    dict_output << "# ------------------------------------------------\n";
-    dict_output << "#\n";
-    dict_output << "# ---   LIST OF MONOMERS ---\n";
-    dict_output << "#\n";
-    dict_output << "data_comp_list\n";
-    dict_output << "loop_\n";
-    dict_output << "_chem_comp.id\n";
-    dict_output << "_chem_comp.three_letter_code\n";
-    dict_output << "_chem_comp.name\n";
-    dict_output << "_chem_comp.group\n";
-    dict_output << "_chem_comp.number_atoms_all\n";
-    dict_output << "_chem_comp.number_atoms_nh\n";
-    dict_output << "_chem_comp.desc_level\n";
-    std::string resname = "UNK";
-    dict_output << resname << "      " << resname << " 'UNKNOWN LIGAND                      ' non-polymer        " << nAtAll << " " <<  nAtNoH << " .\n";
-    dict_output << "# ------------------------------------------------------\n";
-    dict_output << "# ------------------------------------------------------\n";
-    dict_output << "#\n";
-    dict_output << "# --- DESCRIPTION OF MONOMERS ---\n";
-    dict_output << "#\n";
-    dict_output << "data_comp_" << resname << "\n";
-    dict_output << "#\n";
-    dict_output << "loop_\n";
-    dict_output << "_chem_comp_atom.comp_id\n";
-    dict_output << "_chem_comp_atom.atom_id\n";
-    dict_output << "_chem_comp_atom.type_symbol\n";
-    dict_output << "_chem_comp_atom.type_energy\n";
-    dict_output << "_chem_comp_atom.charge\n";
-    dict_output << "_chem_comp_atom.x\n";
-    dict_output << "_chem_comp_atom.y\n";
-    dict_output << "_chem_comp_atom.z\n";
-
-    atoms.clear();
-    for (const auto& site : small.sites) {
-        gemmi::Atom atom;
-        auto orth = small.cell.orthogonalize(site.fract);
-        float x = orth.x;
-        float y = orth.y;
-        float z = orth.z;
-        atom.pos = orth;
-        atom.element = site.element;
-        float charge = 0.0;//FIXME Small molecules have a signed char charge
-
-        std::string elname = std::string(site.element.name());
-        std::transform(elname.begin(), elname.end(), elname.begin(), ::toupper);
-
-        if(atoms.count(elname)==0){
-            atoms[elname]  = 1;
-        } else {
-            atoms[elname] += 1;
-        }
-        atom.name = elname+std::to_string(atoms[elname]);
-        name_disorder_group_map[atom.name] = site.disorder_group;
-
-        dict_output << resname << "           " << atom.name << " " << elname  << " " << atom.name << " " << charge << " " << x << " " << y << " " << z << "\n";
-    }
-
-    dict_output << "loop_\n";
-    dict_output << "_chem_comp_bond.comp_id\n";
-    dict_output << "_chem_comp_bond.atom_id_1\n";
-    dict_output << "_chem_comp_bond.atom_id_2\n";
-    dict_output << "_chem_comp_bond.type\n";
-    dict_output << "_chem_comp_bond.aromatic\n";
-    dict_output << "_chem_comp_bond.value_dist\n";
-    dict_output << "_chem_comp_bond.value_dist_esd\n";
-
-    std::string aromaticFlag = "n";
-
-    for(const auto &a1: r.atoms){
-        for(const auto &a2: r.atoms){
-            if(a1.serial<a2.serial&&name_disorder_group_map[a1.name]==name_disorder_group_map[a2.name]){
-                float dsq = (a1.pos.x - a2.pos.x) * (a1.pos.x - a2.pos.x)
-                          + (a1.pos.y - a2.pos.y) * (a1.pos.y - a2.pos.y)
-                          + (a1.pos.z - a2.pos.z) * (a1.pos.z - a2.pos.z);
-                float bondLength = sqrt(dsq);
-                float radiiSum = a1.element.covalent_r() + a2.element.covalent_r();
-                std::string bondType = "SINGLE";
-                if(a1.element.is_metal()||a2.element.is_metal()){
-                    bondType = "metal";
-                }
-                if(bondLength<(radiiSum*1.08)){
-                    dict_output << resname << "  " << "    " << a1.name <<   " "  << a2.name  << " " << bondType  << " " << aromaticFlag << " " << bondLength << "  0.020\n";
-                }
-            }
-        }
-    }
-
-    dict_output << "# ------------------------------------------------------\n";
-
-    std::string dict_string = dict_output.str();
-
-    return std::pair<std::string,std::string>(mmcif_string,dict_string);
-}
-
 void unzipFileToFP(const std::string &file_name, FILE *fp){
 
     const int LENGTH = 1024;
@@ -1252,6 +1075,8 @@ void unpackCootDataFile(const std::string &fileName, bool doUnzip, const std::st
         chdir(cwd);
     }
 }
+
+std::pair<std::string,std::string> SmallMoleculeCifToMMCif(const std::string &small_molecule_cif);
 
 EMSCRIPTEN_BINDINGS(my_module) {
         // PRIVATEER
