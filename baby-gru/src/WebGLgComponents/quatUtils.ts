0faba614c21565d5df26d016d72a52ce51dc19e5
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2025-05-08 15:20:29 +0100

    Start of simplifying MgWebGL in hope to convert to functional component.

---
+++ baby-gru/src/WebGLgComponents/quatUtils.ts
@@ -0,0 +1,59 @@
+import * as quat4 from 'gl-matrix/quat';
+
+export function createQuatFromDXAngle(angle_in, axis) {
+    let angle = angle_in * Math.PI / 180.0;
+    let q = quat4.create();
+    quat4.set(q, Math.sin(angle / 2.0) * axis[0], Math.sin(angle / 2.0) * axis[1], Math.sin(angle / 2.0) * axis[2], Math.cos(angle / 2.0));
+    return q;
+}
+
+export function createQuatFromAngle(angle_in,axis) {
+    let angle = angle_in * Math.PI / 180.0;
+    let q = quat4.create();
+    quat4.set(q, Math.sin(angle / 2.0)*axis[0], Math.sin(angle / 2.0)*axis[1], Math.sin(angle / 2.0)*axis[2], Math.cos(angle / 2.0));
+    return q;
+}
+
+export function createXQuatFromDX(angle_in) {
+    let angle = angle_in * Math.PI / 180.0;
+    let q = quat4.create();
+    quat4.set(q, Math.sin(angle / 2.0), 0.0, 0.0, Math.cos(angle / 2.0));
+    return q;
+}
+
+export function createYQuatFromDY(angle_in) {
+    let angle = angle_in * Math.PI / 180.0;
+    let q = quat4.create();
+    quat4.set(q, 0.0, Math.sin(angle / 2.0), 0.0, Math.cos(angle / 2.0));
+    return q;
+}
+
+export function createZQuatFromDX(angle_in) {
+    let angle = angle_in * Math.PI / 180.0;
+    let q = quat4.create();
+    quat4.set(q, 0.0, 0.0, Math.sin(angle / 2.0), Math.cos(angle / 2.0));
+    return q;
+}
+
+export function quatDotProduct(q1,q2){
+    return q1[0] * q2[0] + q1[1] * q2[1] + q1[2] * q2[2] + q1[3] * q2[3];
+}
+
+export function quatSlerp(q1,q2,h) {
+    let cosw = quatDotProduct(q1,q2);
+    if(Math.abs(cosw-1.0)<1e-5) return q1;
+    if(cosw>1.0) cosw = 1.0;
+    if(cosw<-1.0) cosw = -1.0;
+    const omega = Math.acos(cosw);
+    const q1Mult = Math.sin((1.0-h)*omega)
+    const q2Mult = Math.sin(h*omega)
+    let newQuat = quat4.create()
+    const newQuat0 = (q1Mult * q1[0] + q2Mult * q2[0]) / Math.sin(omega)
+    const newQuat1 = (q1Mult * q1[1] + q2Mult * q2[1]) / Math.sin(omega)
+    const newQuat2 = (q1Mult * q1[2] + q2Mult * q2[2]) / Math.sin(omega)
+    const newQuat3 = (q1Mult * q1[3] + q2Mult * q2[3]) / Math.sin(omega)
+    quat4.set(newQuat,newQuat0,newQuat1,newQuat2,newQuat3)
+    return newQuat
+}
+
+
--- baby-gru/src/types/mgWebGL.d.ts
+++ baby-gru/src/types/mgWebGL.d.ts
@@ -9,6 +9,338 @@ import * as mat4 from 'gl-matrix/mat4';
 import * as mat3 from 'gl-matrix/mat3';
 
 export namespace webGL {
+interface MGWebGLRenderingContext extends WebGLRenderingContext {
+    viewportWidth: number;
+    viewportHeight: number;
+}
+
+interface MGWebGL2RenderingContext extends WebGL2RenderingContext {
+    viewportWidth: number;
+    viewportHeight: number;
+}
+
+interface clickAtom {
+    x: number;
+    y: number;
+    z: number;
+    charge: number;
+    label: string;
+    symbol: string;
+    displayBuffer: DisplayBuffer;
+    circleData?: ImageData;
+}
+
+interface Dictionary<T> {
+    [Key: string]: T;
+}
+
+
+interface MGWebGLFrameBuffer extends WebGLFramebuffer {
+    width: number;
+    height: number;
+}
+
+interface MGWebGLBuffer {
+    itemSize: number;
+    numItems: number;
+}
+
+interface MGWebGLShaderDepthPeelAccum extends WebGLProgram {
+    vertexPositionAttribute: GLint;
+    vertexTextureAttribute: GLint;
+    pMatrixUniform: WebGLUniformLocation;
+    peelNumber: WebGLUniformLocation;
+    depthPeelSamplers: WebGLUniformLocation;
+    colorPeelSamplers: WebGLUniformLocation;
+    xSSAOScaling: WebGLUniformLocation;
+    ySSAOScaling: WebGLUniformLocation;
+}
+
+interface MGWebGLTextureQuadShader extends WebGLProgram {
+    vertexPositionAttribute: GLint;
+    vertexTextureAttribute: GLint;
+    pMatrixUniform: WebGLUniformLocation;
+    mvMatrixUniform: WebGLUniformLocation;
+    mvInvMatrixUniform: WebGLUniformLocation;
+    fog_start: WebGLUniformLocation;
+    fog_end: WebGLUniformLocation;
+    fogColour: WebGLUniformLocation;
+    clipPlane0: WebGLUniformLocation;
+    clipPlane1: WebGLUniformLocation;
+    clipPlane2: WebGLUniformLocation;
+    clipPlane3: WebGLUniformLocation;
+    clipPlane4: WebGLUniformLocation;
+    clipPlane5: WebGLUniformLocation;
+    clipPlane6: WebGLUniformLocation;
+    clipPlane7: WebGLUniformLocation;
+    nClipPlanes: WebGLUniformLocation;
+    valueMap: WebGLUniformLocation;
+    colorMap: WebGLUniformLocation;
+}
+
+interface MGWebGLShader extends WebGLProgram {
+    vertexPositionAttribute: GLint;
+    vertexNormalAttribute: GLint;
+    vertexColourAttribute: GLint;
+    vertexTextureAttribute: GLint;
+    pMatrixUniform: WebGLUniformLocation;
+    mvMatrixUniform: WebGLUniformLocation;
+    mvInvMatrixUniform: WebGLUniformLocation;
+    fog_start: WebGLUniformLocation;
+    fog_end: WebGLUniformLocation;
+    fogColour: WebGLUniformLocation;
+    clipPlane0: WebGLUniformLocation;
+    clipPlane1: WebGLUniformLocation;
+    clipPlane2: WebGLUniformLocation;
+    clipPlane3: WebGLUniformLocation;
+    clipPlane4: WebGLUniformLocation;
+    clipPlane5: WebGLUniformLocation;
+    clipPlane6: WebGLUniformLocation;
+    clipPlane7: WebGLUniformLocation;
+    nClipPlanes: WebGLUniformLocation;
+    light_positions: WebGLUniformLocation;
+    light_colours_ambient: WebGLUniformLocation;
+    light_colours_specular: WebGLUniformLocation;
+    light_colours_diffuse: WebGLUniformLocation;
+    peelNumber: WebGLUniformLocation;
+    depthPeelSamplers: WebGLUniformLocation;
+}
+
+interface ShaderThickLines extends MGWebGLShader {
+    screenZ: WebGLUniformLocation;
+    offset: WebGLUniformLocation;
+    size: WebGLUniformLocation;
+    scaleMatrix: WebGLUniformLocation;
+    ShadowMap: WebGLUniformLocation;
+    SSAOMap: WebGLUniformLocation;
+    edgeDetectMap: WebGLUniformLocation;
+    doShadows: WebGLUniformLocation;
+    doSSAO: WebGLUniformLocation;
+    doEdgeDetect: WebGLUniformLocation;
+    xPixelOffset: WebGLUniformLocation;
+    yPixelOffset: WebGLUniformLocation;
+    shadowQuality: WebGLUniformLocation;
+    pixelZoom: WebGLUniformLocation;
+    specularPower: WebGLUniformLocation;
+    textureMatrixUniform: WebGLUniformLocation;
+    shinyBack: WebGLUniformLocation;
+}
+
+interface ShaderThickLinesNormal extends ShaderThickLines {
+    vertexRealNormalAttribute: GLint;
+    xSSAOScaling: WebGLUniformLocation; //Perhaps in parent? Perhaps more of parent here?
+    ySSAOScaling: WebGLUniformLocation; // ditto
+    occludeDiffuse: WebGLUniformLocation;
+    doPerspective: WebGLUniformLocation;
+}
+
+interface ShaderTwodShapes extends MGWebGLShader {
+    offset: WebGLUniformLocation;
+    size: WebGLUniformLocation;
+    scaleMatrix: WebGLUniformLocation;
+}
+
+interface ShaderTextInstanced extends MGWebGLShader {
+    offsetAttribute: GLint;
+    sizeAttribute: GLint;
+    textureOffsetAttribute: GLint;
+    pixelZoom: WebGLUniformLocation;
+    vertexTextureAttribute: GLint;
+    textureMatrixUniform: WebGLUniformLocation;
+}
+
+interface ShaderTextBackground extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    pixelZoom: WebGLUniformLocation;
+    screenZ: WebGLUniformLocation;
+    maxTextureS: WebGLUniformLocation;
+}
+
+interface ShaderFrameBuffer extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    blurredTexture: WebGLUniformLocation;
+    depthTexture: WebGLUniformLocation;
+    focussedTexture: WebGLUniformLocation;
+    blurDepth: WebGLUniformLocation;
+}
+
+interface ShaderPointSpheres extends MGWebGLShader {
+    size: WebGLUniformLocation;
+    offset: WebGLUniformLocation;
+    scaleMatrix: WebGLUniformLocation;
+    textureMatrixUniform: WebGLUniformLocation;
+}
+
+interface ShaderPerfectSpheres extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    offsetAttribute: GLint;
+    sizeAttribute: GLint;
+    invSymMatrixUniform: WebGLUniformLocation;
+    textureMatrixUniform: WebGLUniformLocation;
+    xPixelOffset: WebGLUniformLocation;
+    yPixelOffset: WebGLUniformLocation;
+    xSSAOScaling: WebGLUniformLocation;
+    ySSAOScaling: WebGLUniformLocation;
+    ShadowMap: WebGLUniformLocation;
+    SSAOMap: WebGLUniformLocation;
+    edgeDetectMap: WebGLUniformLocation;
+    outlineSize: WebGLUniformLocation;
+    shadowQuality: WebGLUniformLocation;
+    doShadows: WebGLUniformLocation;
+    doSSAO: WebGLUniformLocation;
+    doEdgeDetect: WebGLUniformLocation;
+    occludeDiffuse: WebGLUniformLocation;
+    doPerspective: WebGLUniformLocation;
+    clipCap: WebGLUniformLocation;
+    specularPower: WebGLUniformLocation;
+    scaleMatrix: WebGLUniformLocation;
+}
+
+interface ShaderTriangles extends MGWebGLShader {
+    specularPower: WebGLUniformLocation;
+    shinyBack: WebGLUniformLocation;
+    backColour: WebGLUniformLocation;
+    defaultColour: WebGLUniformLocation;
+    ShadowMap: WebGLUniformLocation;
+    SSAOMap: WebGLUniformLocation;
+    edgeDetectMap: WebGLUniformLocation;
+    shadowQuality: WebGLUniformLocation;
+    doShadows: WebGLUniformLocation;
+    doSSAO: WebGLUniformLocation;
+    doEdgeDetect: WebGLUniformLocation;
+    cursorPos: WebGLUniformLocation;
+    xPixelOffset: WebGLUniformLocation;
+    yPixelOffset: WebGLUniformLocation;
+    xSSAOScaling: WebGLUniformLocation;
+    ySSAOScaling: WebGLUniformLocation;
+    occludeDiffuse: WebGLUniformLocation;
+    doPerspective: WebGLUniformLocation;
+    textureMatrixUniform: WebGLUniformLocation;
+    screenZ: WebGLUniformLocation;
+}
+
+interface ShaderGBuffersTriangles extends MGWebGLShader {
+}
+
+interface ShaderGBuffersThickLinesNormal extends ShaderThickLinesNormal {
+}
+
+interface ShaderBlurX extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    inputTexture: WebGLUniformLocation;
+    depthTexture: WebGLUniformLocation;
+    blurSize: WebGLUniformLocation;
+    blurDepth: WebGLUniformLocation;
+    blurCoeffs: WebGLUniformLocation | null;
+}
+
+interface ShaderSimpleBlurX extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    inputTexture: WebGLUniformLocation;
+    blurSize: WebGLUniformLocation;
+    blurCoeffs: WebGLUniformLocation | null;
+}
+
+interface ShaderSSAO extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    gPositionTexture: WebGLUniformLocation;
+    gNormalTexture: WebGLUniformLocation;
+    texNoiseTexture: WebGLUniformLocation;
+    zoom: WebGLUniformLocation | null;
+    depthBufferSize: WebGLUniformLocation | null;
+    samples: WebGLUniformLocation | null;
+    radius: WebGLUniformLocation | null;
+    bias: WebGLUniformLocation | null;
+    depthFactor: WebGLUniformLocation | null;
+}
+
+interface ShaderEdgeDetect extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    gPositionTexture: WebGLUniformLocation;
+    gNormalTexture: WebGLUniformLocation;
+    depthBufferSize: WebGLUniformLocation | null;
+    depthThreshold: WebGLUniformLocation | null;
+    normalThreshold: WebGLUniformLocation | null;
+    scaleDepth: WebGLUniformLocation | null;
+    scaleNormal: WebGLUniformLocation | null;
+    xPixelOffset: WebGLUniformLocation | null;
+    yPixelOffset: WebGLUniformLocation | null;
+    depthFactor: WebGLUniformLocation | null;
+    zoom: WebGLUniformLocation | null;
+}
+
+interface ShaderBlurY extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    inputTexture: WebGLUniformLocation;
+    depthTexture: WebGLUniformLocation;
+    blurSize: WebGLUniformLocation;
+    blurDepth: WebGLUniformLocation;
+    blurCoeffs: WebGLUniformLocation | null;
+}
+
+interface ShaderSimpleBlurY extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    inputTexture: WebGLUniformLocation;
+    blurSize: WebGLUniformLocation;
+    blurCoeffs: WebGLUniformLocation | null;
+}
+
+interface ShaderCircles extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    up: WebGLUniformLocation;
+    right: WebGLUniformLocation;
+}
+
+interface ShaderImages extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    size: WebGLUniformLocation;
+    offset: WebGLUniformLocation;
+    scaleMatrix: WebGLUniformLocation;
+}
+
+interface ShaderTrianglesInstanced extends ShaderTriangles {
+    vertexInstanceOriginAttribute: GLint;
+    vertexInstanceSizeAttribute : GLint;
+    vertexInstanceOrientationAttribute  : GLint;
+    outlineSize  : WebGLUniformLocation;
+}
+
+interface ShaderGBuffersPerfectSpheres extends ShaderPerfectSpheres {
+}
+
+interface ShaderGBuffersTrianglesInstanced extends ShaderGBuffersTriangles {
+    vertexInstanceOriginAttribute: GLint;
+    vertexInstanceSizeAttribute : GLint;
+    vertexInstanceOrientationAttribute  : GLint;
+}
+
+interface ShaderOutLine extends MGWebGLShader {
+    outlineSize  : WebGLUniformLocation;
+    cursorPos  : WebGLUniformLocation;
+    textureMatrixUniform: WebGLUniformLocation;
+}
+
+interface ShaderOverlay extends MGWebGLShader {
+    vertexTextureAttribute: GLint;
+    inputTexture: WebGLUniformLocation;
+}
+
+interface MGWebGLPropsInterface {
+                    reContourMapOnlyOnMouseUp: boolean | null;
+                    onAtomHovered : (identifier: { buffer: { id: string; }; atom: moorhen.AtomInfo; }) => void;
+                    onKeyPress : (event: KeyboardEvent) =>  boolean | Promise<boolean>;
+                    messageChanged : ((d:Dictionary<string>) => void);
+                    mouseSensitivityFactor :  number | null;
+                    zoomWheelSensitivityFactor :  number | null;
+                    keyboardAccelerators : Dictionary<string>;
+                    showCrosshairs : boolean | null;
+                    showScaleBar : boolean | null;
+                    showAxes : boolean | null;
+                    showFPS : boolean | null;
+                    mapLineWidth : number;
+                    width? : number;
+                    height? : number;
+}
     interface MGWebGL extends React.Component  {
         isWebGL2() : boolean;
         lerp(a:number, b:number, f:number) : number;
@@ -83,44 +415,9 @@ export namespace webGL {
         applySymmetryMatrix(theShader: MGWebGLShader,symmetryMatrix: number[],tempMVMatrix: number[],tempMVInvMatrix: number[]) : void;
         setMatrixUniforms(program: MGWebGLShader) : void;
         setLightUniforms(program: MGWebGLShader) : void;
-        quatDotProduct(q1: quat, q2: quat) : number;
-        quatSlerp(q1: quat, q2: quat, h: number) : quat;
         centreOn(idx: number) : void;
         initTextBuffersBuffer(any) : void;
         initTextBuffers() : void;
-        initInstancedOutlineShaders(vertexShaderOutline : string, fragmentShaderOutline : string)  : void;
-        initInstancedShadowShaders(vertexShaderShadow : string, fragmentShaderShadow : string)  : void;
-        initShadowShaders(vertexShaderShadow : string, fragmentShaderShadow : string)  : void;
-        initBlurXShader(vertexShaderBlurX : string, fragmentShaderBlurX : string)  : void;
-        initSSAOShader(vertexShaderBlurX : string, ssaoFragmentShader : string)  : void;
-        initEdgeDetectShader(vertexShaderBlurX : string, edgeDetectFragmentShader : string)  : void;
-        initBlurYShader(vertexShaderBlurY : string, fragmentShaderBlurY : string)  : void;
-        initSimpleBlurXShader(vertexShaderBlurX : string, fragmentShaderBlurX : string)  : void;
-        initSimpleBlurYShader(vertexShaderBlurX : string, fragmentShaderBlurX : string)  : void;
-        initOverlayShader(vertexShaderOverlay : string, fragmentShaderOverlay : string)  : void;
-        initRenderFrameBufferShaders(vertexShaderRenderFrameBuffer : string, fragmentShaderRenderFrameBuffer : string)  : void;
-        initCirclesShaders(vertexShader : string, fragmentShader : string)  : void;
-        initTextInstancedShaders(vertexShader : string, fragmentShader : string)  : void;
-        initTextBackgroundShaders(vertexShaderTextBackground : string, fragmentShaderTextBackground : string)  : void;
-        initOutlineShaders(vertexShader : string, fragmentShader : string)  : void;
-        initShaders(vertexShader : string, fragmentShader : string)  : void;
-        initShadersTextured(vertexShader : string, fragmentShader : string)  : void;
-        initShadersDepthPeelAccum(vertexShader : string, fragmentShader : string)  : void;
-        initGBufferShaders(vertexShader : string, fragmentShader : string)  : void;
-        initGBufferShadersInstanced(vertexShader : string, fragmentShader : string)  : void;
-        initGBufferShadersPerfectSphere(vertexShader : string, fragmentShader : string)  : void;
-        initGBufferThickLineNormalShaders(vertexShader : string, fragmentShader : string)  : void;
-        initShadersInstanced(vertexShader : string, fragmentShader : string)  : void;
-        initThickLineNormalShaders(vertexShader : string, fragmentShader : string)  : void;
-        initThickLineShaders(vertexShader : string, fragmentShader : string)  : void;
-        initLineShaders(vertexShader : string, fragmentShader : string)  : void;
-        initDepthShadowPerfectSphereShaders(vertexShader : string, fragmentShader : string)  : void;
-        initPerfectSphereOutlineShaders(vertexShader : string, fragmentShader : string)  : void;
-        initPerfectSphereShaders(vertexShader : string, fragmentShader : string)  : void;
-        initImageShaders(vertexShader : string, fragmentShader : string)  : void;
-        initTwoDShapesShaders(vertexShader : string, fragmentShader : string)  : void;
-        initPointSpheresShadowShaders(vertexShader : string, fragmentShader : string)  : void;
-        initPointSpheresShaders(vertexShader : string, fragmentShader : string)  : void;
         setDraggableMolecule(molecule: moorhen.Molecule): void;
         setOrigin(o: [number, number, number], doDrawScene=true, dispatchEvent=true) : void;
         buildBuffers(): void;
