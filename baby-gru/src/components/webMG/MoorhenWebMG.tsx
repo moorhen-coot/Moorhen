4f6a853f5619ce5deb80465c17cb1cd442a4822c
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2025-07-23 17:52:02 +0100

    Factor out vector drawing into separate file.

--- baby-gru/src/components/webMG/MoorhenWebMG.tsx
+++ baby-gru/src/components/webMG/MoorhenWebMG.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useCallback, forwardRef, useState, useReducer,useRef } from 'react';
 import { MGWebGL } from '../../WebGLgComponents/mgWebGL';
 import { buildBuffers, appendOtherData,linesToThickLines } from '../../WebGLgComponents/buildBuffers'
+import { getVectorsBuffers } from '../../WebGLgComponents/vectorsDraw'
 import { Moorhen2DOverlay } from './Moorhen2DOverlay';
 import { MoorhenContextMenu } from "../context-menu/MoorhenContextMenu"
 import { cidToSpec } from '../../utils/utils';
@@ -152,191 +153,7 @@ export const MoorhenWebMG = forwardRef<webGL.MGWebGL, MoorhenWebMGPropsInterface
                 oldBuffers = oldBuffers?.filter(glBuffer => glBuffer.id !== buffer.id)
             })
 
-            const atomColours = {}
-            const colour = [1.0,0.0,0.0,1.0]
-            const dashPairs = []
-            const solidPairs = []
-            const arrowHeadPairs = []
-            const newLabelBuffers = []
-            vectorsList.forEach(vec => {
-                let xFromOrig;
-                let yFromOrig;
-                let zFromOrig;
-                let xToOrig;
-                let yToOrig;
-                let zToOrig;
-                if(vec.coordsMode==="points"){
-                    xFromOrig = vec.xFrom
-                    yFromOrig = vec.yFrom
-                    zFromOrig = vec.zFrom
-                    xToOrig = vec.xTo
-                    yToOrig = vec.yTo
-                    zToOrig = vec.zTo
-                }
-                if(vec.coordsMode==="atompoint"||vec.coordsMode==="atoms"){
-                    if(vec.coordsMode==="atompoint"){
-                        xToOrig = vec.xTo
-                        yToOrig = vec.yTo
-                        zToOrig = vec.zTo
-                    }
-                    if(vec.molNoFrom!==undefined&&vec.cidFrom.length>0){
-                        const fromMol = molecules.find(mol => mol.molNo === vec.molNoFrom)
-                        if(fromMol){
-                            const fromAtoms = window.CCP4Module.get_atom_info_for_selection(fromMol.gemmiStructure, vec.cidFrom, "" )
-                            const nFromAtoms = fromAtoms.size()
-                            if(nFromAtoms>0){
-                                let totXFrom = 0
-                                let totYFrom = 0
-                                let totZFrom = 0
-                                for(let iFrom=0;iFrom<nFromAtoms;iFrom++){
-                                    const at = fromAtoms.get(iFrom)
-                                    totXFrom += at.x
-                                    totYFrom += at.y
-                                    totZFrom += at.z
-                                }
-                                totXFrom /= nFromAtoms
-                                totYFrom /= nFromAtoms
-                                totZFrom /= nFromAtoms
-                                xFromOrig = totXFrom
-                                yFromOrig = totYFrom
-                                zFromOrig = totZFrom
-                            }
-                            fromAtoms.delete()
-                        }
-                    }
-                }
-                if(vec.coordsMode==="atoms"){
-                    if(vec.molNoTo!==undefined&&vec.cidTo.length>0){
-                        const toMol = molecules.find(mol => mol.molNo === vec.molNoTo)
-                        if(toMol){
-                            const toAtoms = window.CCP4Module.get_atom_info_for_selection(toMol.gemmiStructure, vec.cidTo, "" )
-                            const nToAtoms = toAtoms.size()
-                            if(nToAtoms>0){
-                                let totXTo = 0
-                                let totYTo = 0
-                                let totZTo = 0
-                                for(let iTo=0;iTo<nToAtoms;iTo++){
-                                    const at = toAtoms.get(iTo)
-                                    totXTo += at.x
-                                    totYTo += at.y
-                                    totZTo += at.z
-                                }
-                                totXTo /= nToAtoms
-                                totYTo /= nToAtoms
-                                totZTo /= nToAtoms
-                                xToOrig = totXTo
-                                yToOrig = totYTo
-                                zToOrig = totZTo
-                            }
-                            toAtoms.delete()
-                        }
-                    }
-                }
-                if(xFromOrig!==undefined&&xToOrig!==undefined){
-                    let xFrom = xFromOrig
-                    let yFrom = yFromOrig
-                    let zFrom = zFromOrig
-                    let xTo = xToOrig
-                    let yTo = yToOrig
-                    let zTo = zToOrig
-                    if(vec.labelMode==="start"){
-                        newLabelBuffers.push({label:{font:"24px Arial",text:vec.labelText,x:xFrom,y:yFrom,z:zFrom},uuid:guid()})
-                    }
-                    if(vec.labelMode==="end"){
-                        newLabelBuffers.push({label:{font:"24px Arial",text:vec.labelText,x:xTo,y:yTo,z:zTo},uuid:guid()})
-                    }
-                    if(vec.labelMode==="middle"){
-                        const xLabel = xFrom + 0.5 * (xToOrig - xFromOrig)
-                        const yLabel = yFrom + 0.5 * (yToOrig - yFromOrig)
-                        const zLabel = zFrom + 0.5 * (zToOrig - zFromOrig)
-                        newLabelBuffers.push({label:{font:"24px Arial",text:vec.labelText,x:xLabel,y:yLabel,z:zLabel},uuid:guid()})
-                    }
-                    if(vec.arrowMode==="end"||vec.arrowMode==="both"){
-                        xTo = xFrom + 0.8 * (xToOrig - xFromOrig)
-                        yTo = yFrom + 0.8 * (yToOrig - yFromOrig)
-                        zTo = zFrom + 0.8 * (zToOrig - zFromOrig)
-                    }
-                    if(vec.arrowMode==="start"||vec.arrowMode==="both"){
-                        xFrom = xFrom + 0.2 * (xToOrig - xFromOrig)
-                        yFrom = yFrom + 0.2 * (yToOrig - yFromOrig)
-                        zFrom = zFrom + 0.2 * (zToOrig - zFromOrig)
-                    }
-                    const firstAtomInfo = {
-                        pos: [xFrom, yFrom, zFrom],
-                        x: xFrom,
-                        y: yFrom,
-                        z: zFrom,
-                    }
-                    const secondAtomInfo = {
-                        pos: [xTo, yTo, zTo],
-                        x: xTo,
-                        y: yTo,
-                        z: zTo,
-                    }
-                    const pair = [firstAtomInfo, secondAtomInfo]
-                    if(vec.drawMode==="dashedcylinder"){
-                        dashPairs.push(pair)
-                    } else {
-                        solidPairs.push(pair)
-                    }
-                    if(vec.arrowMode==="end"||vec.arrowMode==="both"){
-                        const xFrom = xFromOrig + 0.8 * (xToOrig - xFromOrig)
-                        const yFrom = yFromOrig + 0.8 * (yToOrig - yFromOrig)
-                        const zFrom = zFromOrig + 0.8 * (zToOrig - zFromOrig)
-                        const xTo = xToOrig
-                        const yTo = yToOrig
-                        const zTo = zToOrig
-                        const firstAtomInfo = {
-                            pos: [xFrom, yFrom, zFrom],
-                            x: xFrom,
-                            y: yFrom,
-                            z: zFrom,
-                        }
-                        const secondAtomInfo = {
-                            pos: [xTo, yTo, zTo],
-                            x: xTo,
-                            y: yTo,
-                            z: zTo,
-                        }
-                        const pair = [firstAtomInfo, secondAtomInfo]
-                        arrowHeadPairs.push(pair)
-                    }
-                    if(vec.arrowMode==="start"||vec.arrowMode==="both"){
-                        const xTo = xFromOrig
-                        const yTo = yFromOrig
-                        const zTo = zFromOrig
-                        const xFrom = xFromOrig + 0.2 * (xToOrig - xFromOrig)
-                        const yFrom = yFromOrig + 0.2 * (yToOrig - yFromOrig)
-                        const zFrom = zFromOrig + 0.2 * (zToOrig - zFromOrig)
-                        const firstAtomInfo = {
-                            pos: [xFrom, yFrom, zFrom],
-                            x: xFrom,
-                            y: yFrom,
-                            z: zFrom,
-                        }
-                        const secondAtomInfo = {
-                            pos: [xTo, yTo, zTo],
-                            x: xTo,
-                            y: yTo,
-                            z: zTo,
-                        }
-                        const pair = [firstAtomInfo, secondAtomInfo]
-                        arrowHeadPairs.push(pair)
-                    }
-                }
-            })
-            dashPairs.forEach(atom => { atomColours[`${atom[0].serial}`] = colour; atomColours[`${atom[1].serial}`] = colour })
-            solidPairs.forEach(atom => { atomColours[`${atom[0].serial}`] = colour; atomColours[`${atom[1].serial}`] = colour })
-            arrowHeadPairs.forEach(atom => { atomColours[`${atom[0].serial}`] = colour; atomColours[`${atom[1].serial}`] = colour })
-            const objects = [
-                gemmiAtomPairsToCylindersInfo(dashPairs, 0.07, atomColours, false, 0.01, 1000.)
-            ]
-            objects.push(...[
-                gemmiAtomPairsToCylindersInfo(solidPairs, 0.07, atomColours, false, 0.01, 1000., false)
-            ])
-            objects.push(...[
-                gemmiAtomPairsToCylindersInfo(arrowHeadPairs, 0.15, atomColours, false, 0.01, 1000., false, "cone")
-            ])
+            const [objects,newLabelBuffers] = getVectorsBuffers()
 
             let newBuffers = []
             objects.filter(object => typeof object !== 'undefined' && object !== null).forEach(object => {
@@ -344,6 +161,7 @@ export const MoorhenWebMG = forwardRef<webGL.MGWebGL, MoorhenWebMGPropsInterface
                 newBuffers = [...newBuffers,...a]
                 buildBuffers(a)
             })
+
             setVectorBuffers(newBuffers)
             setVectorLabelBuffers(newLabelBuffers)
             dispatch(setDisplayBuffers([...newBuffers,...oldBuffers]))
