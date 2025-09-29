a7d09b39695b2cf503edd2dccf720328b2ac4e25
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2025-09-29 11:56:55 +0100

    Better PAE plot resize behaviour and some axes labels.

--- baby-gru/src/components/modal/MoorhenPAEModal.tsx
+++ baby-gru/src/components/modal/MoorhenPAEModal.tsx
@@ -1,17 +1,25 @@
-import { useRef } from "react";
 import { Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { moorhen } from "../../types/moorhen";
 import { MoorhenPAEPlot } from "../validation-tools/MoorhenPAEPlot"
 import { convertRemToPx, convertViewtoPx} from '../../utils/utils';
 import { modalKeys } from "../../utils/enums";
+import { dispatchPersistentStates, usePersistentState } from "../../store/menusSlice";
 import { MoorhenDraggableModalBase } from "./MoorhenDraggableModalBase"
 
-export const MoorhenPAEModal = () => {        
-    const resizeNodeRef = useRef<HTMLDivElement>(null);
-      
+export const MoorhenPAEModal = () => {
+    const menu = 'moorhenPAEPlotModal'
+
     const width = useSelector((state: moorhen.State) => state.sceneSettings.width)
     const height = useSelector((state: moorhen.State) => state.sceneSettings.height)
+    const dispatch = useDispatch()
+
+    const [modalSize, setModalSize] = usePersistentState<{ width: number; height: number }>(
+        menu,
+        'modalSize',{
+        width: convertRemToPx(40),
+        height: convertRemToPx(32)
+    })
 
     return <MoorhenDraggableModalBase
                 modalId={modalKeys.PAEPLOT}
@@ -24,13 +32,21 @@ export const MoorhenPAEModal = () => {
                 enforceMaxBodyDimensions={false}
                 overflowY='hidden'
                 overflowX='auto'
-                headerTitle='Alphafold PAE Plot'
+                headerTitle='Alphafold PAE Plot (example data)'
                 footer={null}
-                resizeNodeRef={resizeNodeRef}
+                onResize={(evt, ref, direction, delta, size) => {
+                   setModalSize(size)
+                }}
+                onResizeStop={() => dispatchPersistentStates(
+                    dispatch,
+                    menu,
+                    [{key: 'modalSize', value: modalSize}],
+                )}
+
                 body={
                     <div style={{height: '100%'}} >
                         <Row className={"big-validation-tool-container-row"}>
-                            <MoorhenPAEPlot />
+                            <MoorhenPAEPlot size={modalSize} resizeTrigger={false}/>
                         </Row>
                     </div>
                 }
