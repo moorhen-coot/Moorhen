65a9367497756860445adfdadd0e4330a4f101df
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2025-11-11 15:53:52 +0000

    moorhen.data is no longer used, so remove from test setup/teardown.

--- baby-gru/tests/setup.js
+++ baby-gru/tests/setup.js
@@ -1,16 +1,2 @@
-const fs = require('fs')
-const path = require('path')
-
 module.exports = () => {
-    fs.stat('moorhen.data', function(err, stat) { 
-        if (err == null) {
-            // file exists
-        } else if (err.code === 'ENOENT') {
-            fs.symlink(path.join(__dirname, '..', 'public', 'moorhen.data'), 'moorhen.data', 'file', (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            })
-        }
-    })
 }
