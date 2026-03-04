0cc876057bb016a05763a13fffeeef17cb346886
Clement Degut <c.degut@gmail.com>
2026-03-04 10:33:40 +0000

    adde proper typing to activity compat

--- baby-gru/src/components/interface-base/Compatibility/Activity.tsx
+++ baby-gru/src/components/interface-base/Compatibility/Activity.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const ActivityCompat =
+export const ActivityCompat: React.FC<{ mode: string; children: React.ReactNode }> =
     (React as any).Activity ??
     (({ mode, children }: { mode: string; children: React.ReactNode }) => (
         <div style={{ display: mode === "visible" ? "flex" : "none", flex: 1, minWidth: 0, overflow: "hidden" }}>{children}</div>
