21ed628e7bd8a32cfe1c0eec10c0bb3a914abc9a
cdegut <c.degut@gmail.com>
2026-03-06 13:27:05 +0000

    fix type issues

--- baby-gru/src/utils/utils.ts
+++ baby-gru/src/utils/utils.ts
@@ -1,15 +1,15 @@
 import { hexToRgb } from "@mui/material";
-import { Store } from "@reduxjs/toolkit";
 import * as mat3 from "gl-matrix/mat3";
 import * as vec3 from "gl-matrix/vec3";
 import JSZip from "jszip";
 import pako from "pako";
-import { RootState } from "@/store/MoorhenReduxStore";
+import { CommandCentre, WorkerResponse } from "@/InstanceManager/CommandCentre";
+import { MoorhenReduxStoreType } from "@/store/MoorhenReduxStore";
+import type { MoorhenMolecule, ResidueInfo } from "@/utils//MoorhenMolecule";
 import { Shortcut } from "../components/managers/preferences";
 import { gemmi } from "../types/gemmi";
 import { libcootApi } from "../types/libcoot";
 import { moorhen } from "../types/moorhen";
-import type { ResidueInfo } from "./MoorhenMolecule";
 
 export const make3MFZipFile = async modelFile => {
     const zip = new JSZip();
@@ -89,11 +89,11 @@ export const parseAtomInfoLabel = (atomInfo: moorhen.AtomInfo) => {
 };
 
 export const getCentreAtom = async (
-    molecules: moorhen.Molecule[],
-    commandCentre: React.RefObject<moorhen.CommandCentre>,
-    store: Store<RootState>
-): Promise<[moorhen.Molecule, string]> => {
-    const visibleMolecules: moorhen.Molecule[] = molecules.filter((molecule: moorhen.Molecule) => molecule.isVisible());
+    molecules: MoorhenMolecule[],
+    commandCentre: React.RefObject<CommandCentre>,
+    store: MoorhenReduxStoreType
+): Promise<[MoorhenMolecule, string]> => {
+    const visibleMolecules: MoorhenMolecule[] = molecules.filter((molecule: MoorhenMolecule) => molecule.isVisible());
     const originState = store.getState().glRef.origin;
     if (visibleMolecules.length === 0) {
         return [null, null];
@@ -105,10 +105,10 @@ export const getCentreAtom = async (
             commandArgs: [...originState.map(coord => coord * -1), visibleMolecules.map(molecule => molecule.molNo).join(":")],
         },
         false
-    )) as moorhen.WorkerResponse<libcootApi.PairType<number, string>>;
+    )) as WorkerResponse<libcootApi.PairType<number, string>>;
     const moleculeMolNo: number = response.data.result.result.first;
     const residueCid: string = response.data.result.result.second;
-    const selectedMolecule = visibleMolecules.find((molecule: moorhen.Molecule) => molecule.molNo === moleculeMolNo);
+    const selectedMolecule = visibleMolecules.find((molecule: MoorhenMolecule) => molecule.molNo === moleculeMolNo);
     return [selectedMolecule, residueCid];
 };
 
@@ -304,63 +304,16 @@ export function convertViewtoPx(input: number, height: number): number {
 }
 
 export const readGzippedTextFile = async (source: File): Promise<string> => {
-    const data = await source.arrayBuffer()
-    const gUnZippeData = pako.inflate(data, { to: 'string' })
-    return gUnZippeData
-}
-
-<<<<<<< HEAD
-    return new Promise((resolve, reject) => {
-        const reader: FileReader = new FileReader();
-        reader.addEventListener("load", () => resolveReader(reader, resolve));
-        reader.readAsText(source);
-    });
+    const data = await source.arrayBuffer();
+    const gUnZippeData = pako.inflate(data, { to: "string" });
+    return gUnZippeData;
 };
 
-export const readGzippedTextFile = (source: File): Promise<ArrayBuffer> => {
-    const resolveReader = (reader: FileReader, resolveCallback) => {
-        reader.removeEventListener("load", resolveCallback);
-        let res = "";
-        const chunk = 32 * 1024;
-        const gUnZippeData = pako.inflate(reader.result);
-        let i;
-        for (i = 0; i < gUnZippeData.length / chunk; i++) {
-            res += String.fromCharCode.apply(null, gUnZippeData.subarray(i * chunk, (i + 1) * chunk));
-        }
-        res += String.fromCharCode.apply(null, gUnZippeData.subarray(i * chunk));
-        resolveCallback(res);
-    };
-
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.addEventListener("load", () => resolveReader(reader, resolve));
-        reader.readAsArrayBuffer(source);
-    });
-};
-
-export const readDataFile = (source: File): Promise<ArrayBuffer> => {
-    const resolveReader = (reader: FileReader, resolveCallback) => {
-        reader.removeEventListener("load", resolveCallback);
-        if (typeof reader.result === "string") {
-            resolveCallback(JSON.parse(reader.result));
-        } else {
-            resolveCallback(reader.result);
-        }
-    };
-
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.addEventListener("load", () => resolveReader(reader, resolve));
-        reader.readAsArrayBuffer(source);
-    });
-};
-=======
 export const readGzippedDataFile = async (source: File): Promise<ArrayBuffer> => {
-    const data = await source.arrayBuffer()
-    const gUnZippeData = pako.inflate(data)
-    return gUnZippeData
-}
->>>>>>> origin/main
+    const data = await source.arrayBuffer();
+    const gUnZippeData = pako.inflate(data);
+    return gUnZippeData;
+};
 
 const downloadFile = (file: File, fileName: string) => {
     const url = window.URL.createObjectURL(file);
@@ -478,7 +431,7 @@ type ResidueInfoType = {
 };
 
 export const getResidueInfo = (
-    molecules: moorhen.Molecule[],
+    molecules: MoorhenMolecule[],
     selectedMolNo: number,
     selectedChain: string,
     selectedResidueIndex: number
@@ -630,7 +583,7 @@ const getSecondaryStructureColourRules = (secondaryStructureInfo: libcootApi.Res
     return result.join("|");
 };
 
-export const getMultiColourRuleArgs = async (molecule: moorhen.Molecule, ruleType: string): Promise<string> => {
+export const getMultiColourRuleArgs = async (molecule: MoorhenMolecule, ruleType: string): Promise<string> => {
     let multiRulesArgs: string;
     switch (ruleType) {
         case "secondary-structure":
