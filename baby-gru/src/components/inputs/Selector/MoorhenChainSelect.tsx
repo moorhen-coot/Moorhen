b2f876f46712e8a3e7a77a3d63fe64c5568e507b
Clement Degut <c.degut@gmail.com>
2026-03-18 11:24:43 +0000

    cleaned up selector compoenents fropm forward refs

--- baby-gru/src/components/inputs/Selector/MoorhenChainSelect.tsx
+++ baby-gru/src/components/inputs/Selector/MoorhenChainSelect.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from "react";
 import { moorhen } from "../../../types/moorhen";
 import { MoorhenSelect } from "./Select";
 
@@ -13,23 +12,17 @@ type MoorhenChainSelectPropsType = {
     onChange?: (arg0: React.ChangeEvent<HTMLSelectElement>) => void;
     defaultValue?: string;
     disabled?: boolean;
+    ref?: React.Ref<HTMLSelectElement>;
 };
 
-export const MoorhenChainSelect = forwardRef<HTMLSelectElement, MoorhenChainSelectPropsType>((props, selectRef) => {
+export const MoorhenChainSelect = (props: MoorhenChainSelectPropsType) => {
     // props.allowedTypes refers to gemmi::PolymerType member values -> https://project-gemmi.github.io/python-api/gemmi.html#PolymerType
     // 1: PeptideL, 2: PeptideD, 3: DNA, 4: RNA, 5: DNARNA hybrid
-    const {
-        allowedTypes = [1, 2, 3, 4, 5],
-        height = "4rem",
-        width = "20rem",
-        label = "Chain",
-        margin = "0.5rem",
-        defaultValue = -99999,
-    } = props;
+    const { allowedTypes = [1, 2, 3, 4, 5], label = "Chain", defaultValue = -99999, ref = null } = props;
 
     const handleChange = (evt: React.ChangeEvent<HTMLSelectElement>) => {
         props.onChange?.(evt);
-        if (selectRef !== null && typeof selectRef !== "function") selectRef.current.value = evt.target.value;
+        if (ref !== null && typeof ref !== "function") ref.current.value = evt.target.value;
     };
 
     const getChainOptions = (selectedCoordMolNo: number): React.JSX.Element[] => {
@@ -46,8 +39,8 @@ export const MoorhenChainSelect = forwardRef<HTMLSelectElement, MoorhenChainSele
     };
 
     return (
-        <MoorhenSelect ref={selectRef} disabled={props.disabled} defaultValue={defaultValue} onChange={handleChange} label={label}>
+        <MoorhenSelect ref={ref} disabled={props.disabled} defaultValue={defaultValue} onChange={handleChange} label={label}>
             {props.selectedCoordMolNo !== null ? getChainOptions(props.selectedCoordMolNo) : null}
         </MoorhenSelect>
     );
-});
+};
