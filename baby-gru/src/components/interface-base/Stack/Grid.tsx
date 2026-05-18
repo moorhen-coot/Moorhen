f1a5c8ae8354a220c2a06e4bea2ad37ff8df4449
Clement Degut <c.degut@gmail.com>
2026-05-18 17:58:46 +0100

    added grid and table to base component, remove grid from mapinfocard

--- baby-gru/src/components/interface-base/Stack/Grid.tsx
+++ baby-gru/src/components/interface-base/Stack/Grid.tsx
@@ -1,22 +1,43 @@
+import { Children, isValidElement } from "react";
+import "./grid.css"
 export const MoorhenGrid = (props: {
     children: React.ReactNode;
     gap?: string;
     columns: number;
+    table?: boolean;
     style?: React.CSSProperties;
+    titleRow?: boolean;
+    titleColumn?: boolean;
 }) => {
-    const { children, gap = "0px", columns, style = null } = props;
+    const { children, gap = "0px", columns, style = null, table, titleRow, titleColumn } = props;
+
+    const wrappedChildren = table
+        ? Children.toArray(children).map((child, index) => {
+              const isAlternateStripe = Math.floor(index / columns) % 2 === 1;
+              const is1stColumn = index % columns === 0;
+              const is1stRow = index < columns;
+              const cellClassName = `moorhen__grid-table-cell${isAlternateStripe ? " moorhen__grid-table-cell--alternate" : "" }${(is1stColumn && titleColumn) ? " moorhen__grid-table-cell--header" : ""}${(is1stRow && titleRow) ? " moorhen__grid-table-cell--header" : ""}`;
+              const childKey = isValidElement(child) && child.key !== null ? child.key : index;
+
+              return (
+                  <div key={childKey} className={cellClassName}>
+                      {child}
+                  </div>
+              );
+          })
+        : children;
+
 
     return (
         <div
-            className="moorhen__grid"
+            className={`moorhen__grid${table ? " moorhen__grid-table" : ""}`}
             style={{
-                display: "grid",
                 gridTemplateColumns: `repeat(${columns}, auto)`,
                 gap: gap,
                 ...style,
             }}
         >
-            {children}
+            {wrappedChildren}
         </div>
     );
 }
