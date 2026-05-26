87e0862f1814b873a47440fb14926cb065d837ea
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2026-05-26 17:11:01 +0100

    Draw a sphere instead of 2 squares.

--- baby-gru/src/components/modal/MoorhenCremerPople.tsx
+++ baby-gru/src/components/modal/MoorhenCremerPople.tsx
@@ -82,45 +82,176 @@ interface SideOnProgramInstanced extends WebGLProgram {
     vertexInstanceOrientationAttribute: GLint;
 }
 
-const totIdxs = [0,1,2,0,2,3,4,5,6,4,6,7]
-const totPos = [
-               -10.0, -10.0, -10.0,
-                10.0, -10.0, -10.0,
-                10.0,  10.0, -10.0,
-               -10.0,  10.0, -10.0,
-               -10.0, -10.0,  10.0,
-                10.0, -10.0,  10.0,
-                10.0,  10.0,  10.0,
-               -10.0,  10.0,  10.0,
-                ]
-const totNorm = [
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
-                0.0, 0.0,  1.0,
-                0.0, 0.0,  1.0,
-                0.0, 0.0,  1.0,
-                0.0, 0.0,  1.0,
-               ]
-const totCol = [
-                0.0, 0.0, 0.0, 1.0,
-                0.0, 0.0, 1.0, 1.0,
-                1.0, 0.0, 0.0, 1.0,
-                1.0, 0.0, 1.0, 1.0,
-                0.0, 1.0, 0.0, 1.0,
-                0.0, 1.0, 1.0, 1.0,
-                1.0, 1.0, 0.0, 1.0,
-                1.0, 1.0, 1.0, 1.0,
-               ]
-const primitive_json = { 
-    prim_types: [["TRIANGLES"]],
-    useIndices: [[true]],
-    idx_tri: [[totIdxs]],
-    vert_tri: [[totPos]],
-    norm_tri: [[totNorm]],
-    col_tri: [[totCol]]
+const genSphere = (radius) => {
+
+    const X_1_0 = 0.525731112119
+    const X_1_1 = 0.000000000000
+    const X_1_2 = 0.850650808352
+    const X_1_3 = 0.309016994375
+    const X_1_4 = 0.500000000000
+    const X_1_5 = 0.809016994375
+    const X_1_6 = 1.000000000000
+
+    const icosa_1_col = [
+    ]
+
+    const icosa_1_vert = [
+      -X_1_0, X_1_2, X_1_1,
+      -X_1_3, X_1_5, X_1_4,
+      X_1_1, X_1_6, X_1_1,
+      X_1_1, X_1_0, X_1_2,
+      X_1_3, X_1_5, X_1_4,
+      X_1_0, X_1_2, X_1_1,
+      -X_1_5, X_1_4, X_1_3,
+      -X_1_2, X_1_1, X_1_0,
+      -X_1_4, X_1_3, X_1_5,
+      -X_1_4, -X_1_3, X_1_5,
+      X_1_1, -X_1_0, X_1_2,
+      X_1_1, X_1_1, X_1_6,
+      X_1_4, X_1_3, X_1_5,
+      X_1_4, -X_1_3, X_1_5,
+      X_1_2, X_1_1, X_1_0,
+      X_1_5, X_1_4, X_1_3,
+      X_1_6, X_1_1, X_1_1,
+      X_1_2, X_1_1, -X_1_0,
+      X_1_5, X_1_4, -X_1_3,
+      X_1_5, -X_1_4, X_1_3,
+      X_1_0, -X_1_2, X_1_1,
+      X_1_5, -X_1_4, -X_1_3,
+      X_1_3, -X_1_5, X_1_4,
+      -X_1_3, -X_1_5, X_1_4,
+      -X_1_0, -X_1_2, X_1_1,
+      X_1_1, -X_1_6, X_1_1,
+      -X_1_3, -X_1_5, -X_1_4,
+      X_1_1, -X_1_0, -X_1_2,
+      X_1_3, -X_1_5, -X_1_4,
+      X_1_4, -X_1_3, -X_1_5,
+      X_1_1, X_1_1, -X_1_6,
+      X_1_1, X_1_0, -X_1_2,
+      X_1_4, X_1_3, -X_1_5,
+      -X_1_4, -X_1_3, -X_1_5,
+      -X_1_2, X_1_1, -X_1_0,
+      -X_1_4, X_1_3, -X_1_5,
+      -X_1_5, X_1_4, -X_1_3,
+      -X_1_3, X_1_5, -X_1_4,
+      X_1_3, X_1_5, -X_1_4,
+      -X_1_6, X_1_1, X_1_1,
+      -X_1_5, -X_1_4, X_1_3,
+      -X_1_5, -X_1_4, -X_1_3
+    ]
+
+    const icosa_1_idxs = [
+      0, 1, 2,
+      3, 4, 1,
+      5, 2, 4,
+      1, 4, 2,
+      0, 6, 1,
+      7, 8, 6,
+      3, 1, 8,
+      6, 8, 1,
+      7, 9, 8,
+      10, 11, 9,
+      3, 8, 11,
+      9, 11, 8,
+      3, 11, 12,
+      10, 13, 11,
+      14, 12, 13,
+      11, 13, 12,
+      3, 12, 4,
+      14, 15, 12,
+      5, 4, 15,
+      12, 15, 4,
+      14, 16, 15,
+      17, 18, 16,
+      5, 15, 18,
+      16, 18, 15,
+      14, 19, 16,
+      20, 21, 19,
+      17, 16, 21,
+      19, 21, 16,
+      10, 22, 13,
+      20, 19, 22,
+      14, 13, 19,
+      22, 19, 13,
+      10, 23, 22,
+      24, 25, 23,
+      20, 22, 25,
+      23, 25, 22,
+      24, 26, 25,
+      27, 28, 26,
+      20, 25, 28,
+      26, 28, 25,
+      27, 29, 28,
+      17, 21, 29,
+      20, 28, 21,
+      29, 21, 28,
+      27, 30, 29,
+      31, 32, 30,
+      17, 29, 32,
+      30, 32, 29,
+      27, 33, 30,
+      34, 35, 33,
+      31, 30, 35,
+      33, 35, 30,
+      34, 36, 35,
+      0, 37, 36,
+      31, 35, 37,
+      36, 37, 35,
+      0, 2, 37,
+      5, 38, 2,
+      31, 37, 38,
+      2, 38, 37,
+      31, 38, 32,
+      5, 18, 38,
+      17, 32, 18,
+      38, 18, 32,
+      7, 6, 39,
+      0, 36, 6,
+      34, 39, 36,
+      6, 36, 39,
+      7, 39, 40,
+      34, 41, 39,
+      24, 40, 41,
+      39, 41, 40,
+      7, 40, 9,
+      24, 23, 40,
+      10, 9, 23,
+      40, 23, 9,
+      27, 26, 33,
+      24, 41, 26,
+      34, 33, 41,
+      26, 41, 33
+    ]
+
+    const sphere_pos = []
+
+    icosa_1_vert.forEach(p => {
+        sphere_pos.push(p * radius)
+    })
+
+    for(let icol=0; icol<icosa_1_vert.length/3; icol++){
+        const r = Math.random()
+        const g = Math.random()
+        const b = Math.random()
+        icosa_1_col.push(r)
+        icosa_1_col.push(g)
+        icosa_1_col.push(b)
+        icosa_1_col.push(1.0)
     }
+
+    const sphere_json = {
+        prim_types: [["TRIANGLES"]],
+        useIndices: [[true]],
+        idx_tri: [[icosa_1_idxs]],
+        vert_tri: [[sphere_pos]],
+        norm_tri: [[icosa_1_vert]],
+        col_tri: [[icosa_1_col]]
+    }
+
+    console.log(sphere_json)
+
+    return sphere_json
+}
 
 export const MoorhenCremerPople = (props: { stackDirection: "horizontal" | "vertical", width?: number }) => {
 
@@ -198,7 +329,7 @@ export const MoorhenCremerPople = (props: { stackDirection: "horizontal" | "vert
         const canvasWebGL = canvasRefWebGL.current
         const gl = canvasWebGL.getContext("webgl2")
 
-        const theBuffers = createOtherDataOtherContext(primitive_json,gl)
+        const theBuffers = createOtherDataOtherContext(genSphere(10),gl)
         buildBuffers(theBuffers,store,gl)
         setMyBuffers(theBuffers)
 
@@ -399,7 +530,7 @@ export const MoorhenCremerPople = (props: { stackDirection: "horizontal" | "vert
         const sphereFragmentShader = getShader(gl, perfect_sphere_side_on_view_fragment_shader_source, "fragment");
         sphereProgramRef.current = initSideOnSphereShaders(sphereVertexShader,sphereFragmentShader,gl)
 
-        const theBuffers = createOtherDataOtherContext(primitive_json,gl)
+        const theBuffers = createOtherDataOtherContext(genSphere(10),gl)
         buildBuffers(theBuffers,store,gl)
         setMyBuffers(theBuffers)
 
