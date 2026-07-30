e87d0f76b20f7dbdd2727c8202fe1fdb257b636a
clement <c.degut@gmail.com>
2026-07-30 18:35:46 +0100

    extract range residue selector in its own component

--- baby-gru/src/components/card/MoleculeCard/addCustomRepresentation/components/ResidueSelectionSection.tsx
+++ baby-gru/src/components/card/MoleculeCard/addCustomRepresentation/components/ResidueSelectionSection.tsx
@@ -1,19 +1,16 @@
-import { MoorhenStack } from "@/components/interface-base/Stack/Stack";
-import { MoorhenSequenceViewer, moorhenSequenceToSeqViewer } from "@/components/sequence-viewer";
-import { moorhen } from "../../../../../types/moorhen";
-import { MoorhenNumberInput, MoorhenSelect } from "../../../../inputs";
+import { MoorhenMolecule } from "@/utils/MoorhenMolecule";
+import {  MoorhenSelect } from "../../../../inputs";
 import { MoorhenCidInputForm } from "../../../../inputs/Cid/MoorhenCidInputForm";
 import { MoorhenChainSelect } from "../../../../inputs/Selector/MoorhenChainSelect";
 import { MoorhenLigandSelect } from "../../../../inputs/Selector/MoorhenLigandSelect";
-import { MoorhenInfoCard } from "@/components/interface-base";
-import { SelectionInfoCard } from "@/components/sequence-viewer/infoCard";
+import { ResidueRangeSelector } from "@/components/inputs/ResidueRange";
 
 interface ResidueSelectionSectionProps {
     ruleType: "ligands" | "cid" | "molecule" | "chain" | "residue-range" | "neighbourhood";
     setRuleType: (val: ResidueSelectionSectionProps["ruleType"]) => void;
     representationStyle: string;
-    molecules: moorhen.Molecule[];
-    molecule: moorhen.Molecule;
+    molecules: MoorhenMolecule[];
+    molecule: MoorhenMolecule;
     selectedChain: string;
     setSelectedChain: (val: string) => void;
     sequenceResidueRange: [number, number];
@@ -45,29 +42,6 @@ export const ResidueSelectionSection = (props: ResidueSelectionSectionProps) => 
         isThereLigand,
     } = props;
 
-    const handleResiduesRangeSelection = selection => {
-        setSequenceResidueRange(selection.range[0] < selection.range[1] ? selection.range : [selection.range[1], selection.range[0]]);
-    };
-
-    const selectedSequence = props.molecule.sequences.find(sequence => sequence.chain === selectedChain);
-    const handleResidueRangeChange = (range: [number,number]) => {
-        let clampedRange: [number, number] = range;
-        if (clampedRange[0] > clampedRange[1]) {
-            clampedRange = [clampedRange[1], clampedRange[1]];
-        }
-
-        if (clampedRange[0] < selectedSequence.sequence[0].resNum) {
-            clampedRange[0] = selectedSequence.sequence[0].resNum;
-        }
-        if (clampedRange[1] > selectedSequence.sequence[selectedSequence.sequence.length - 1].resNum) {
-            clampedRange[1] = selectedSequence.sequence[selectedSequence.sequence.length - 1].resNum;
-        }
-
-        setSequenceResidueRange(clampedRange);
-    }
-    console.log(
-        `Rule type: ${ruleType}, selected chain: ${selectedChain}, sequenceResidueRange: ${sequenceResidueRange}, selectedSequence: ${selectedSequence}`
-    );
     return (
         <>
             <MoorhenSelect
@@ -132,42 +106,15 @@ export const ResidueSelectionSection = (props: ResidueSelectionSectionProps) => 
             {ruleType === "ligands" && (
                 <MoorhenLigandSelect selectedCoordMolNo={molecule.molNo} molecules={[molecule]} allowAll setValue={setLigandCid} />
             )}
-            {ruleType === "residue-range" && selectedSequence && (
-                // escape the inputGrid
-                <MoorhenStack style={{ gridColumn: "1 / -1", padding: "0.5rem", marginTop: "0.5rem" }} card direction="column" gap="0.5rem">
-                    <MoorhenStack direction="row" align="center" gap="0.5rem"  >
-                        Select a residue range: <MoorhenInfoCard infoText={SelectionInfoCard }/>
-                    </MoorhenStack>
-                    <MoorhenSequenceViewer
-                        style={{ border: "none", marginTop: "0rem" }}
-                        sequences={moorhenSequenceToSeqViewer(selectedSequence, props.molecule.name, props.molecule.molNo)}
-                        onResiduesSelect={selection => {
-                            handleResiduesRangeSelection(selection);
-                        }}
-                        showTitleBar={false}
-                        selectedResidues={
-                            sequenceResidueRange ? { molNo: props.molecule.molNo, chain: selectedChain, range: sequenceResidueRange } : null
-                        }
-                    />
-                    <MoorhenStack direction="row" align="center" justify="center" gap="2rem">
-                        <MoorhenNumberInput
-                            integer
-                            width="4rem"
-                            type="number"
-                            value={sequenceResidueRange ? sequenceResidueRange[0] : null}
-                            setValue={value => handleResidueRangeChange([value, sequenceResidueRange[1]])}
-                            label="Start"
-                        />
-                        <MoorhenNumberInput
-                            integer
-                            width="4rem"
-                            type="number"
-                            value={sequenceResidueRange ? sequenceResidueRange[1] : null}
-                            setValue={value => handleResidueRangeChange([sequenceResidueRange[0], value])}
-                            label="End"
-                        />
-                    </MoorhenStack>
-                </MoorhenStack>
+            {ruleType === "residue-range"  &&(
+                <ResidueRangeSelector
+                    molecule={molecule}
+                    selectedChain={selectedChain}
+                    sequenceResidueRange={sequenceResidueRange}
+                    setSequenceResidueRange={setSequenceResidueRange}
+                    style={{gridColumn: "1 / -1",}}
+                />
+ 
             )}
         </>
     );
---
+++ baby-gru/src/components/inputs/ResidueRange.tsx
@@ -0,0 +1,75 @@
+import { MoorhenMolecule } from "@/utils";
+import { MoorhenInfoCard, MoorhenStack } from "../interface-base";
+import { MoorhenSequenceViewer, moorhenSequenceToSeqViewer } from "../sequence-viewer";
+import { SelectionInfoCard } from "../sequence-viewer/infoCard";
+import { MoorhenNumberInput } from "./MoorhenNumberInput/NumberInput";
+
+export type ResidueRangeSelectorProps = {
+    molecule: MoorhenMolecule;
+    selectedChain: string;
+    sequenceResidueRange: [number, number] | null;
+    setSequenceResidueRange: React.Dispatch<React.SetStateAction<[number, number] | null>>;
+    style?: React.CSSProperties;
+};
+
+export const ResidueRangeSelector = (props: ResidueRangeSelectorProps) => {
+    const handleResiduesRangeSelection = selection => {
+        props.setSequenceResidueRange(selection.range[0] < selection.range[1] ? selection.range : [selection.range[1], selection.range[0]]);
+    };
+
+    const selectedSequence = props.molecule.sequences.find(sequence => sequence.chain === props.selectedChain);
+    const handleResidueRangeChange = (range: [number, number]) => {
+        let clampedRange: [number, number] = range;
+        if (clampedRange[0] > clampedRange[1]) {
+            clampedRange = [clampedRange[1], clampedRange[1]];
+        }
+
+        if (clampedRange[0] < selectedSequence.sequence[0].resNum) {
+            clampedRange[0] = selectedSequence.sequence[0].resNum;
+        }
+        if (clampedRange[1] > selectedSequence.sequence[selectedSequence.sequence.length - 1].resNum) {
+            clampedRange[1] = selectedSequence.sequence[selectedSequence.sequence.length - 1].resNum;
+        }
+
+        props.setSequenceResidueRange(clampedRange);
+    };
+
+    return (
+        <MoorhenStack style={{ padding: "0.5rem", marginTop: "0.5rem", ...props.style }} card direction="column" gap="0.5rem">
+            <MoorhenStack direction="row" align="center" gap="0.5rem">
+                Select a residue range: <MoorhenInfoCard infoText={SelectionInfoCard} />
+            </MoorhenStack>
+            <MoorhenSequenceViewer
+                style={{ border: "none", marginTop: "0rem" }}
+                sequences={moorhenSequenceToSeqViewer(selectedSequence, props.molecule.name, props.molecule.molNo)}
+                onResiduesSelect={selection => {
+                    handleResiduesRangeSelection(selection);
+                }}
+                showTitleBar={false}
+                selectedResidues={
+                    props.sequenceResidueRange
+                        ? { molNo: props.molecule.molNo, chain: props.selectedChain, range: props.sequenceResidueRange }
+                        : null
+                }
+            />
+            <MoorhenStack direction="row" align="center" justify="center" gap="2rem">
+                <MoorhenNumberInput
+                    integer
+                    width="4rem"
+                    type="number"
+                    value={props.sequenceResidueRange ? props.sequenceResidueRange[0] : null}
+                    setValue={value => handleResidueRangeChange([value, props.sequenceResidueRange ? props.sequenceResidueRange[1] : null])}
+                    label="Start"
+                />
+                <MoorhenNumberInput
+                    integer
+                    width="4rem"
+                    type="number"
+                    value={props.sequenceResidueRange ? props.sequenceResidueRange[1] : null}
+                    setValue={value => handleResidueRangeChange([props.sequenceResidueRange ? props.sequenceResidueRange[0] : null, value])}
+                    label="End"
+                />
+            </MoorhenStack>
+        </MoorhenStack>
+    );
+};
