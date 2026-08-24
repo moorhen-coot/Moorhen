2e7231b4050f929ac9c47f6f4f43e707e73bcd7d
clement <c.degut@gmail.com>
2026-08-24 12:36:45 +0100

    drop in better residue range selector

--- baby-gru/src/components/card/MoleculeCard/ColourRuleCard.tsx
+++ baby-gru/src/components/card/MoleculeCard/ColourRuleCard.tsx
@@ -8,13 +8,12 @@ import { MoorhenButton, MoorhenColourPicker, MoorhenPopoverButton, MoorhenSelect
 import { MoorhenStack } from "../../interface-base";
 
 export const NcsColourSwatch = (props: { rule: ColourRule; applyColourChange: () => void; style?: { [key: string]: string } }) => {
-    const ncsSwatchRef = useRef(null);
     const newNcsHexValueRef = useRef<string>("");
     const ncsCopySelectRef = useRef<null | HTMLSelectElement>(null);
 
     const [hex, setHex] = useState<string>("#ffffff");
     const [ncsCopyValue, setNcsCopyValue] = useState<string>("");
-    const [showColourPicker, setShowColourPicker] = useState<boolean>(false);
+
 
     const { rule, applyColourChange } = props;
 
